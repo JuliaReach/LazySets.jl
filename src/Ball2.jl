JuliaReach/LazySets.jl

commit d23ec88eb52e9e80b88607455c9651e1b4052fb2

#19 - change type unions to common supertype I also took the liberty and replaced Array{X, 1} by Vector{X}.

src/Ball2.jl
@@ -26,7 +26,7 @@ We evaluate the support vector in a given direction:
 
 ```julia
 julia> σ(ones(5), B)
-5-element Array{Float64,1}:
+5-element Vector{Float64}:
 0.06742
 0.13484
 0.20226
@@ -75,7 +75,7 @@ The support vector in the given direction.
 
 If the given direction has norm zero, the origin is returned.
 """
-function σ(d::Union{Vector{Float64}, SparseVector{Float64,Int64}}, B::Ball2)::Vector{Float64}
+function σ(d::AbstractVector{Float64}, B::Ball2)::Vector{Float64}
     dnorm = norm(d)
     if dnorm > 0
         return B.center .+ d .* (B.radius / dnorm)

src/LinearConstraints.jl
@@ -71,12 +71,12 @@ The line `y = -x + 1` intersected with `y = x`:
 
 ```julia
 julia> intersection(Line([1., 1.], 1.), Line([-1., 1.], 0.))
-2-element Array{Float64,1}:
+2-element Vector{Float64}:
  0.5
  0.5
 ```
 """
-function intersection(Δ1::Line, Δ2::Line)::Array{Float64,1}
+function intersection(Δ1::Line, Δ2::Line)::Vector{Float64}
     b = [Δ1.b, Δ2.b]
     a = [Δ1.a.' ; Δ2.a.']
     return a \ b

src/helper_functions.jl
@@ -20,7 +20,7 @@ This function can be used with vector-valued arguments via the dot operator.
 
 ```julia
 julia> unit_step([-0.6, 1.3, 0.0])
-3-element Array{Float64,1}:
+3-element Vector{Float64}:
  -1.0
  1.0
  1.0
@@ -69,7 +69,6 @@ True iff arg(u) [2π] <= arg(v) [2π]
 The argument is measured in counter-clockwise fashion, with the 0 being the
 direction (1, 0).
 """
-function <=(u::Union{Vector{Float64}, SparseVector{Float64,Int64}},
-            v::Union{Vector{Float64}, SparseVector{Float64,Int64}})
+function <=(u::AbstractVector{Float64}, v::AbstractVector{Float64})
     return jump2pi(atan2(u[2], u[1])) <= jump2pi(atan2(v[2], v[1]))
 end

src/BallInf.jl
@@ -60,7 +60,7 @@ function is such that `sign(0) = 0`, instead of 1. For this reason, we use the
 custom `unit_step` function, that allows to do: `B.center + unit_step.(d) * B.radius`
 (the dot operator performs broadcasting, to accept vector-valued entries).
 """
-function σ(d::Union{Vector{Float64}, SparseVector{Float64,Int64}}, B::BallInf)::Vector{Float64}
+function σ(d::AbstractVector{Float64}, B::BallInf)::Vector{Float64}
     return B.center .+ unit_step.(d) .* B.radius
 end
 

src/CartesianProduct.jl
@@ -122,7 +122,7 @@ Support vector of the Cartesian product of a finite number of sets.
 
 - `cp` -- cartesian product array
 """
-function σ(d::Union{Vector{Float64}, SparseVector{Float64,Int64}}, cp::CartesianProductArray)::Vector{Float64}
+function σ(d::AbstractVector{Float64}, cp::CartesianProductArray)::Vector{Float64}
     svec = Vector{Float64}(length(d))
     jinit = 1
     for sj in cp.sfarray

src/ConvexHull.jl
@@ -42,7 +42,7 @@ Return the support vector of a convex hull in a given direction.
 - `d`  -- direction
 - `ch` -- the convex hull of two sets
 """
-function σ(d::Union{Vector{Float64}, SparseVector{Float64,Int64}}, ch::ConvexHull)::Vector{Float64}
+function σ(d::AbstractVector{Float64}, ch::ConvexHull)::Vector{Float64}
     σ1 = σ(d, ch.X)
     σ2 = σ(d, ch.Y)
     ρ1 = dot(d, σ1)::Float64

src/ExponentialMap.jl
@@ -183,7 +183,7 @@ If `S = (LMR)B`, where `L` and `R` are dense matrices, `M` is a matrix
 exponential, and `B` is a set, it follows that:
 `σ(d, S) = LMR σ(R^T M^T L^T d, B)` for any direction `d`.
 """
-function σ(d::Union{Vector{Float64}, SparseVector{Float64,Int64}},
+function σ(d::AbstractVector{Float64},
            eprojmap::ExponentialProjectionMap)::Vector{Float64}
     daux = transpose(eprojmap.projspmexp.L) * d
     aux1 = expmv(1.0, eprojmap.projspmexp.spmexp.M.', daux)

src/Hyperrectangle.jl
@@ -45,8 +45,7 @@ end
 
 Return the support vector of a Hyperrectangle in a given direction.
 """
-function σ(d::Union{Vector{Float64}, SparseVector{Float64,Int64}},
-           H::Hyperrectangle)::Vector{Float64}
+function σ(d::AbstractVector{Float64}, H::Hyperrectangle)::Vector{Float64}
     return H.center .+ unit_step.(d) .* H.radius
 end
 
@@ -67,7 +66,7 @@ The list of vertices as an array of floating-point vectors.
 
 For high-dimensions, it is preferable to develop a `vertex_iterator` approach.
 """
-function vertices_list(H::Hyperrectangle)::Array{Vector{Float64}, 1}
+function vertices_list(H::Hyperrectangle)::Vector{Vector{Float64}}
     return [H.center .+ si .* H.radius for si in IterTools.product([[1, -1] for i = 1:dim(H)]...)]
 end
 

commit 6ee8211fd9d529d34201679589eba0c3e32b9a27

fix doctest errors introduced in d23ec88

src/Ball2.jl
@@ -26,7 +26,7 @@ We evaluate the support vector in a given direction:
 
 ```julia
 julia> σ(ones(5), B)
-5-element Vector{Float64}:
+5-element Array{Float64,1}:
 0.06742
 0.13484
 0.20226

src/LinearConstraints.jl
@@ -71,7 +71,7 @@ The line `y = -x + 1` intersected with `y = x`:
 
 ```julia
 julia> intersection(Line([1., 1.], 1.), Line([-1., 1.], 0.))
-2-element Vector{Float64}:
+2-element Array{Float64,1}:
  0.5
  0.5
 ```

src/helper_functions.jl
@@ -20,7 +20,7 @@ This function can be used with vector-valued arguments via the dot operator.
 
 ```julia
 julia> unit_step([-0.6, 1.3, 0.0])
-3-element Vector{Float64}:
+3-element Array{Float64,1}:
  -1.0
  1.0
  1.0