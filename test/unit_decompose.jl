db4f815c93bd20c6f0d862c98d04494e97f172dd
mforets <marcelo.forets-irurtia@univ-grenoble-alpes.fr>
2017-11-22 15:09:24 +0100

    improve decompose example:

--- test/unit_decompose.jl
+++ test/unit_decompose.jl
@@ -1,8 +1,14 @@
 # ==============================
 # Check that Issue #43 is fixed
 # ==============================
-Ω0 = LazySets.ConvexHull{LazySets.CartesianProductArray{LazySets.BallInf},LazySets.LinearMap{LazySets.CartesianProductArray{LazySets.BallInf}}}(LazySets.CartesianProductArray{LazySets.BallInf}(LazySets.BallInf[LazySets.BallInf([0.767292, 0.936613], 0.1), LazySets.BallInf([0.734104, 0.87296], 0.1)]), LazySets.LinearMap{LazySets.CartesianProductArray{LazySets.BallInf}}([1.92664 1.00674 1.0731 -0.995149; -2.05704 3.48059 0.0317863 1.83481; 0.990993 -1.97754 0.754192 -0.807085; -2.43723 0.782825 -3.99255 3.93324], LazySets.CartesianProductArray{LazySets.BallInf}(LazySets.BallInf[LazySets.BallInf([0.767292, 0.936613], 0.1), LazySets.BallInf([0.734104, 0.87296], 0.1)])))
-
+const CPA = CartesianProductArray
+X = CPA([BallInf([0.767292, 0.936613], 0.1), BallInf([0.734104, 0.87296], 0.1)])
+Y = [1.92664 1.00674 1.0731 -0.995149;
+    -2.05704 3.48059 0.0317863 1.83481;
+    0.990993 -1.97754 0.754192 -0.807085;
+    -2.43723 0.782825 -3.99255 3.93324]
+Y = Y * CPA([BallInf([0.767292, 0.936613], 0.1), BallInf([0.734104, 0.87296], 0.1)])
+Ω0 = CH(X, Y)
 dec = Approximations.decompose(Ω0)
 dec1 = dec.sfarray[1]
 
