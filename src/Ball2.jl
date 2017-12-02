f86d1a888c9b7618ade84ead0fa77efa5490b391
schillic <schillic@informatik.uni-freiburg.de>
2017-12-02 14:32:09 +0100

    docs: revised Ball2

--- src/Ball2.jl
+++ src/Ball2.jl
@@ -1,11 +1,18 @@
 export Ball2
 
 """
-    Ball2 <: LazySet
+    Ball2{N<:Real} <: LazySet
 
 Type that represents a ball in the 2-norm.
 
-It is defined as the set
+### Fields
+
+- `center` -- center of the ball as a real vector
+- `radius` -- radius of the ball as a real scalar (``≥ 0``)
+
+### Notes
+
+Mathematically, a ball in the 2-norm is defined as the set
 
 ```math
 \\mathcal{B}_2^n(c, r) = \\{ x ∈ \\mathbb{R}^n : ‖ x - c ‖_2 ≤ r \\},
@@ -15,24 +22,19 @@ Here ``‖ ⋅ ‖_2`` denotes the Euclidean norm (also known as 2-norm), define
 ``‖ x ‖_2 = \\left( \\sum\\limits_{i=1}^n |x_i|^2 \\right)^{1/2}`` for any
 ``x ∈ \\mathbb{R}^n``.
 
-### Fields
-
-- `center` -- center of the ball as a real vector
-- `radius` -- radius of the ball as a real scalar (``≥ 0``)
-
 ### Examples
 
-A five-dimensional ball in the 2-norm centered at the origin of radius 0.5:
+Create a five-dimensional ball `B` in the 2-norm centered at the origin with
+radius 0.5:
 
 ```julia
-julia> using LazySets
 julia> B = Ball2(zeros(5), 0.5)
-LazySets.Ball2([0.0, 0.0, 0.0, 0.0, 0.0], 0.5)
+LazySets.Ball2{Float64}([0.0, 0.0, 0.0, 0.0, 0.0], 0.5)
 julia> dim(B)
 5
 ```
 
-Evaluate its support vector in the direction ``[1,2,3,4,5]``:
+Evaluate `B`'s support vector in the direction ``[1,2,3,4,5]``:
 
 ```julia
 julia> σ([1.,2.,3.,4.,5.], B)
@@ -50,19 +52,19 @@ struct Ball2{N<:Real} <: LazySet
 
     # default constructor with domain constraint for radius
     Ball2{N}(center, radius) where N =
-        (radius < zero(N) ? throw(DomainError()) : new(center, radius))
+        radius < zero(N) ? throw(DomainError()) : new(center, radius)
 end
 # type-less convenience constructor
 Ball2(center::Vector{N}, radius::N) where {N<:Real} = Ball2{N}(center, radius)
 
 """
-    dim(B)
+    dim(B::Ball2)
 
 Return the dimension of a ball in the 2-norm.
 
 ### Input
 
-- `B` -- a ball in the 2-norm
+- `B` -- ball in the 2-norm
 
 ### Output
 
@@ -71,21 +73,26 @@ The ambient dimension of the ball.
 dim(B::Ball2) = length(B.center)
 
 """
-    σ(d, B)
+    σ(d::AbstractVector{N}, B::Ball2)::AbstractVector{<:AbstractFloat} where {N<:AbstractFloat}
 
-Return the support vector of a ball in the 2-norm in a given direction.
+Return the support vector of a 2-norm ball in a given direction.
 
 ### Input
 
-- `d` -- a direction
-- `B` -- a ball in the 2-norm
+- `d` -- direction
+- `B` -- ball in the 2-norm
 
 ### Output
 
-The support vector in the given direction. If the given direction has norm zero,
-the origin is returned.
+The support vector in the given direction. If the direction has norm zero, the
+origin is returned.
 
 ### Notes
+
+Let `c` and `r` be the center and radius of a ball `B` in the 2-norm,
+respectively.
+For nonzero direction d, we have
+`σ(d, B) = c + d * (r / ||d||)`.
 
 This function requires computing the 2-norm of the input direction, which is
 performed in the given precision of the numeric datatype of both the direction
