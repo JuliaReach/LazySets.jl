c15632be5774b6123c9aae387cc4c83f455f888d
schillic <schillic@informatik.uni-freiburg.de>
2017-12-02 14:32:14 +0100

    docs: revised BallInf

--- src/BallInf.jl
+++ src/BallInf.jl
@@ -1,39 +1,46 @@
-import Base.LinAlg:norm
+import Base.LinAlg:norm,
+       Base.Iterators.repeated
 
-export BallInf, vertices_list, norm, radius, diameter
+export BallInf,
+       vertices_list,
+       norm,
+       radius,
+       diameter
 
 """
-    BallInf <: LazySet
+    BallInf{N<:Real} <: LazySet
 
 Type that represents a ball in the infinity norm.
 
-It is defined as the set
+### Fields
+
+- `center` -- center of the ball as a real vector
+- `radius` -- radius of the ball as a real scalar (``≥ 0``)
+
+### Notes
+
+Mathematically, a ball in the infinity norm is defined as the set
 
 ```math
-\\mathcal{B}_∞^n(c, r) = \\{ x ∈ \\mathbb{R}^n : ‖ x - c ‖_∞ ≦ r \\},
+\\mathcal{B}_∞^n(c, r) = \\{ x ∈ \\mathbb{R}^n : ‖ x - c ‖_∞ ≤ r \\},
 ```
 where ``c ∈ \\mathbb{R}^n`` is its center and ``r ∈ \\mathbb{R}_+`` its radius.
 Here ``‖ ⋅ ‖_∞`` denotes the infinity norm, defined as
 ``‖ x ‖_∞ = \\max\\limits_{i=1,…,n} \\vert x_i \\vert`` for any
 ``x ∈ \\mathbb{R}^n``.
 
-### Fields
-
-- `center` -- center of the ball as a real vector
-- `radius` -- radius of the ball as a scalar (``≧ 0``)
-
 ### Examples
 
-We create the two-dimensional unit ball, and compute its support function
-along the positive ``x=y`` direction:
+Create the two-dimensional unit ball and compute its support function along the
+positive ``x=y`` direction:
 
 ```julia
-julia> B = BallInf(zeros(2), 0.1)
-LazySets.BallInf([0.0, 0.0], 0.1)
+julia> B = BallInf(zeros(2), 1.0)
+LazySets.BallInf{Float64}([0.0, 0.0], 1.0)
 julia> dim(B)
 2
 julia> ρ([1., 1.], B)
-0.2
+2.0
 ```
 """
 struct BallInf{N<:Real} <: LazySet
@@ -42,12 +49,11 @@ struct BallInf{N<:Real} <: LazySet
 
     # default constructor with domain constraint for radius
     BallInf{N}(center, radius) where N =
-        (radius < zero(N)
-            ? throw(DomainError())
-            : new(center, radius))
+        radius < zero(N) ? throw(DomainError()) : new(center, radius)
 end
 # type-less convenience constructor
-BallInf(center::Vector{N}, radius::N) where {N<:Real} = BallInf{N}(center, radius)
+BallInf(center::Vector{N}, radius::N) where {N<:Real} =
+    BallInf{N}(center, radius)
 
 """
     dim(B::BallInf)
@@ -56,7 +62,7 @@ Return the dimension of a ball in the infinity norm.
 
 ### Input
 
-- `B` -- a ball in the infinity norm
+- `B` -- ball in the infinity norm
 
 ### Output
 
@@ -65,41 +71,45 @@ The ambient dimension of the ball.
 dim(B::BallInf) = length(B.center)
 
 """
-    σ(d::AbstractVector{<:Real}, B::BallInf)
+    σ(d::AbstractVector{<:Real}, B::BallInf)::AbstractVector{<:Real}
 
 Return the support vector of an infinity norm ball in a given direction.
 
 ### Input
 
 - `d` -- direction
-- `B` -- unit ball in the infinity norm
+- `B` -- ball in the infinity norm
+
+### Output
+
+The support vector in the given direction. If the direction has norm zero, the
+vertex with biggest values is returned.
 """
 function σ(d::AbstractVector{<:Real}, B::BallInf)::AbstractVector{<:Real}
     #=
-    We cannot use `B.center + sign.(d) * B.radius`, since the built-in `sign`
-    function is such that `sign(0) = 0`, instead of 1 as needed.
-    The custom `unit_step` function allows to do perform broadcasting through
-    the dot operator, thus accepting vector-valued entries.
+    We cannot use `sign.(d)`, since the built-in `sign` function is such that
+    `sign(0) = 0`, instead of 1 as needed.
+    The function `unit_step.(d)` does what we want.
     =#
     return @. B.center + unit_step(d) * B.radius
 end
 
 """
-    vertices_list(B::BallInf)
+    vertices_list(B::BallInf{N})::Vector{Vector{N}} where {N<:Real}
 
 Return the list of vertices of a ball in the infinity norm.
 
 ### Input
 
-- `B` -- a ball in the infinity norm
+- `B` -- ball in the infinity norm
 
 ### Output
 
-The list of vertices as an array of floating-point vectors.
+A list of vertices.
 
 ### Notes
 
-For high-dimensions, it is preferable to develop a `vertex_iterator` approach.
+For high dimensions, it is preferable to develop a `vertex_iterator` approach.
 
 ### Examples
 
@@ -114,17 +124,15 @@ julia> vertices_list(B)
  [-0.1, -0.1]
 ```
 """
-function vertices_list(B::BallInf)
-    return [B.center .+ si .* B.radius for si in IterTools.product([[1, -1] for i = 1:dim(B)]...)]
+function vertices_list(B::BallInf{N})::Vector{Vector{N}} where {N<:Real}
+    return [B.center .+ si .* B.radius
+        for si in IterTools.product([[1, -1] for i = 1:dim(B)]...)]
 end
 
 """
-    norm(B::BallInf, [p])
+    norm(B::BallInf, [p]::Real=Inf)::Real
 
 Return the norm of a ball in the infinity norm.
-
-It is defined as the norm of the enclosing ball, of the given
-``p``-norm, of minimal volume.
 
 ### Input
 
@@ -134,46 +142,57 @@ It is defined as the norm of the enclosing ball, of the given
 ### Output
 
 A real number representing the norm.
+
+### Notes
+
+The norm of an infinity ball is defined as the norm of the enclosing ball, of
+the given ``p``-norm, of minimal volume.
 """
 function norm(B::BallInf, p::Real=Inf)::Real
     return maximum(map(x -> norm(x, p), vertices_list(B)))
 end
 
 """
-    radius(B::BallInf, [p])
+    radius(B::BallInf, [p]::Real=Inf)
 
 Return the radius of a ball in the infinity norm.
 
-It is defined as the radius of the enclosing ball of the given ``p``-norm of
-minimal volume with the same center.
-
 ### Input
 
-- `B` -- a ball in the infinity norm
+- `B` -- ball in the infinity norm
 - `p` -- (optional, default: `Inf`) norm
 
 ### Output
 
 A real number representing the radius.
+
+### Notes
+
+The radius is defined as the radius of the enclosing ball of the given
+``p``-norm of minimal volume with the same center.
 """
-radius(B::BallInf, p::Real=Inf) = (p == Inf) ? B.radius : norm(fill(B.radius, dim(B)), p)
+radius(B::BallInf, p::Real=Inf) =
+    (p == Inf) ? B.radius : norm(repeated(B.radius, dim(B)), p)
 
 """
-    diameter(B::BallInf, [p])
+    diameter(B::BallInf, [p]::Real=Inf)
 
 Return the diameter of a ball in the infinity norm.
 
-It corresponds to the maximum distance between any two elements of the set.
-Equivalently, it is the diameter of the enclosing ball of the given ``p``-norm
-of minimal volume with the same center.
-
 ### Input
 
-- `B` -- a ball in the infinity norm
+- `B` -- ball in the infinity norm
 - `p` -- (optional, default: `Inf`) norm
 
 ### Output
 
 A real number representing the diameter.
+
+### Notes
+
+The diameter is defined as the maximum distance in the given ``p``-norm between
+any two elements of the set.
+Equivalently, it is the diameter of the enclosing ball of the given ``p``-norm
+of minimal volume with the same center.
 """
-diameter(B::BallInf, p::Real=Inf) = 2 * radius(B, p)
+diameter(B::BallInf, p::Real=Inf) = radius(B, p) * 2
