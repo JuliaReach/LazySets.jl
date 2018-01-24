2176e57184ff771a9d0a7048f3e0eebc7329b01f
mforets <marcelo.forets-irurtia@univ-grenoble-alpes.fr>
2018-01-24 08:02:43 -0300

    decompose different set types

--- test/unit_decompose.jl
+++ test/unit_decompose.jl
@@ -1,3 +1,5 @@
+import LazySets.Approximations.decompose
+
 # ==============================
 # Check that Issue #43 is fixed
 # ==============================
@@ -9,10 +11,25 @@ Y = [1.92664 1.00674 1.0731 -0.995149;
     -2.43723 0.782825 -3.99255 3.93324]
 Y = Y * CPA([BallInf([0.767292, 0.936613], 0.1), BallInf([0.734104, 0.87296], 0.1)])
 Ω0 = CH(X, Y)
-dec = Approximations.decompose(Ω0)
+dec = decompose(Ω0)
 dec1 = dec.sfarray[1]
 
 @test dec1.constraints_list[1].b ≈ 2.84042586
 @test dec1.constraints_list[2].b ≈ 4.04708832
 @test dec1.constraints_list[3].b ≈ -0.667292
 @test dec1.constraints_list[4].b ≈ -0.836613
+
+# ======================================
+# Run decompose for different set types
+# ======================================
+b = BallInf(zeros(6), 1.)
+d = decompose(b, Inf, HPolygon)
+@test d.sfarray[1] isa HPolygon
+d = decompose(b, Inf, Hyperrectangle)
+@test d.sfarray[1] isa Hyperrectangle
+d = decompose(b, 1e-2, Hyperrectangle)
+@test d.sfarray[1] isa HPolygon  # with p not Inf the set type is ignored
+d = decompose(b, 1e-2) # by default uses HPolygon
+@test d.sfarray[1] isa HPolygon
+d = decompose(b, [1e-1, 1e-2, 1e-3])
+@test d.sfarray[1] isa HPolygon
