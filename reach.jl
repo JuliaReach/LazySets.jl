7db29c83210c07303a0cc7a78775e08d9daa7f37
schillic <schillic@informatik.uni-freiburg.de>
2018-01-31 11:47:34 +0100

    first version

---
+++ reach.jl
@@ -0,0 +1,87 @@
+using LazySets
+
+function reach_hybrid(As, Gs, init, δ, μ, T)
+    queue = [init]
+
+    res = []
+    while !isempty(queue)
+        init, loc = pop!(queue)
+        R = reach_continuous(As[loc], init, δ, μ, T)
+        append!(res, R)
+        for (guard, tgt_loc) in Gs[loc]
+            for S in R
+                if !is_intersection_empty(S, guard)
+                    push!(queue, (S, tgt_loc))
+                end
+            end
+        end
+    end
+end
+
+
+
+function reach_continuous(A, X0, δ, μ, T)
+
+    # bloating factors
+    Anorm = norm(A, Inf)
+    α = (expm(δ*Anorm) - 1 - δ*Anorm)/norm(X0, Inf)
+    β = (expm(δ*Anorm) - 1)*μ/Anorm
+
+    # discretized system
+    n = size(A, 1)
+    ϕ = expm(δ*A)
+    N = floor(Int, T/δ)
+
+    # preallocate arrays
+    Q = Vector{LazySet}(N)
+    R = Vector{LazySet}(N)
+
+    # initial reach set in the time interval [0, δ]
+    ϕp = (I+ϕ)/2
+    ϕm = (I-ϕ)/2
+    c = X0.center
+    Q1_generators = hcat(ϕp * X0.generators, ϕm * c, ϕm * X0.generators)
+    Q[1] = Zonotope(ϕp * c, Q1_generators) ⊕ BallInf(zeros(n), α + β)
+    R[1] = Q[1]
+
+    # set recurrence for [δ, 2δ], ..., [(N-1)δ, Nδ]
+    ballβ = BallInf(zeros(n), β)
+    for i in 2:N
+        Q[i] = ϕ * Q[i-1] ⊕ ballβ
+        R[i] = Q[i]
+    end
+    return R
+end
+
+
+
+function example()
+    # dynamics
+    A1 = A2 = [-1.0 0.0; 1.0 0.0]
+    A3 = A4 = [-1.0 0.0; 1.0 -1.0]
+    As = [A1, A2, A3, A4]
+
+    # transitions
+    t1 = [(Hyperplane([1., 0.], -1.), 2), (Hyperplane([0., 1.], 1.), 3)]
+    t2 = [(Hyperplane([0., 1.], 1.), 3)]
+    t3 = [(Hyperplane([0., 1.], 0.), 1), (Hyperplane([1., 0.], -1.), 4)]
+    t4 = [(Hyperplane([0., 1.], 0.),2), (Hyperplane([1., 0.], 1.), 3)]
+    Gs = [t1, t2, t3, t4]
+
+    # initial condition
+    X0 = Zonotope([2., 1.], 0.5*eye(2))
+    init_loc = 3
+    init = (X0, init_loc)
+
+    # input uncertainty
+    μ = 0.05
+
+    # discretization step
+    δ = 0.02
+
+    # time bound
+    T = 2.
+
+    # run analysis
+    reach_hybrid(As, Gs, init, δ, μ, T)
+end
