829c5d8b3485e4b78fa1b3cf172310f057bfc2a9
mforets <marcelo.forets-irurtia@univ-grenoble-alpes.fr>
2018-02-05 16:52:49 -0300

    update vpolytope

--- src/VPolytope.jl
+++ src/VPolytope.jl
@@ -15,180 +15,101 @@ Type that represents a convex polytope in V-representation.
 struct VPolytope{N<:Real} <: AbstractPolytope{N}
     vertices::Vector{Vector{N}}
 end
-# constructor for a HPolytope with no constraints
-VPolytope{N}() where {N<:Real} = HPolytope{N}(Vector{N}(0))
+# constructor for a VPolytope with empty vertices list
+VPolytope{N}() where {N<:Real} = VPolytope{N}(Vector{N}(0))
 
-# constructor for a HPolytope with no constraints of type Float64
-HPolytope() = HPolytope{Float64}()
+# constructor for a VPolytope with no constraints of type Float64
+VPolytope() = VPolytope{Float64}()
 
-# constructor from a polygon in H-representation
-HPolytope(P::HPolygon) = HPolytope(P.constraints_list)
+# constructor from a polygon in V-representation
+VPolytope(P::VPolygon) = VPolytope(P.vertices_list)
 
-# constructor for a HPolytope from a simple H-representation
-function HPolytope(A::Matrix{N}, b::Vector{N}) where {N<:Real}
-    m = size(A, 1)
-    constraints = LinearConstraint{N}[]
-    @inbounds for i in 1:m
-        push!(constraints, LinearConstraint(A[i, :], b[i]))
-    end
-    return HPolytope(constraints)
-end
 
 # --- LazySet interface functions ---
 
 
 """
-    dim(P::HPolytope)::Int
+    dim(P::VPolytope)::Int
 
-Return the dimension of a polytope in H-representation.
+Return the dimension of a polytope in V-representation.
 
 ### Input
 
-- `P`  -- polytope in H-representation
+- `P`  -- polytope in V-representation
 
 ### Output
 
-The ambient dimension of the polytope in H-representation.
-If it has no constraints, the result is ``-1``.
+The ambient dimension of the polytope in V-representation.
+If it is empty, the result is ``-1``.
+
+### Examples
+
+```jldoctest
+julia> v = VPolytope()
+julia> dim(v) > 0
+false
+julia> v = VPolytope([ones(3)])
+LazySets.VPolytope{Float64}(Array{Float64,1}[[1.0, 1.0, 1.0]])
+julia> dim(v) == 3
+true
+```
 """
-function dim(P::HPolytope)::Int
-    return length(P.constraints) == 0 ? -1 : length(P.constraints[1].a)
+function dim(P::VPolytope)::Int
+    return length(P.vertices) == 0 ? -1 : length(P.vertices[1])
 end
 
 """
-    σ(d::AbstractVector{<:Real}, P::HPolytope)::Vector{<:Real}
+    σ(d::AbstractVector{<:Real}, P::VPolytope; algorithm="hrep")::Vector{<:Real}
 
-Return the support vector of a polyhedron (in H-representation) in a given
+Return the support vector of a polyhedron (in V-representation) in a given
 direction.
 
 ### Input
 
-- `d` -- direction
-- `P` -- polyhedron in H-representation
+- `d`         -- direction
+- `P`         -- polyhedron in V-representation
+- `algorithm` -- (optional, default: `'hrep'`) method to compute the support vector
 
 ### Output
 
 The support vector in the given direction.
-
-### Algorithm
-
-This implementation uses `GLPKSolverLP` as linear programming backend.
 """
-function σ(d::AbstractVector{<:Real}, P::HPolytope)::Vector{<:Real}
-    c = -d
-    m = length(constraints_list(P))
-    A = zeros(m, dim(P))
-    b = zeros(m)
-    for (i, Pi) in enumerate(constraints_list(P))
-        A[i, :] = Pi.a
-        b[i] = Pi.b
+function σ(d::AbstractVector{<:Real}, P::VPolytope; algorithm="hrep")::Vector{<:Real}
+    if algorithm == "hrep"
+        @assert isdefined(:Polyhedra) "you need to load Polyhedra for this algorithm"
+        return σ(d, tohrep(P))
+    else
+        error("the algorithm $(hrep) is not known")
     end
-    sense = '<'
-    l = -Inf
-    u = Inf
-    solver = GLPKSolverLP()
-    lp = linprog(c, A, sense, b, l, u, solver)
-    return lp.sol
 end
 
-"""
-    ∈(x::AbstractVector{N}, P::HPolytope{N})::Bool where {N<:Real}
+# --- VPolytope functions ---
 
-Check whether a given 2D point is contained in a polytope in constraint
-representation.
+"""
+    vertices_list(P::VPolytope{N})::Vector{Vector{N}} where {N<:Real}
+
+Return the list of vertices of a polytope in constraint representation.
 
 ### Input
 
-- `x` -- two-dimensional point/vector
-- `P` -- polytope in constraint representation
+- `P`         -- polytope in constraint representation
+- `backend`   -- (optional, default: `CDDLib.CDDLibrary()`) the polyhedral
+                 computations backend, see Polyhedra's documentation
+                 for further information
+- `prunefunc` -- (optional, default: `removevredundancy!`) function to post-process
+                 the output of `vreps`
 
 ### Output
 
-`true` iff ``x ∈ P``.
-
-### Algorithm
-
-This implementation checks if the point lies on the outside of each hyperplane.
-This is equivalent to checking if the point lies in each half-space.
+List of vertices.
 """
-function ∈(x::AbstractVector{N}, P::HPolytope{N})::Bool where {N<:Real}
-    @assert length(x) == dim(P)
-
-    for c in P.constraints
-        if dot(c.a, x) > c.b
-            return false
-        end
-    end
-    return true
+function vertices_list(P::HPolytope{N};
+                       backend=CDDLib.CDDLibrary(),
+                       prunefunc=removevredundancy!)::Vector{Vector{N}} where {N<:Real}
+    return P.vertices
 end
 
-
-# --- HPolytope functions ---
-
-
-"""
-    addconstraint!(P::HPolytope{N},
-                   constraint::LinearConstraint{N})::Void where {N<:Real}
-
-Add a linear constraint to a polyhedron in H-representation.
-
-### Input
-
-- `P`          -- polyhedron in H-representation
-- `constraint` -- linear constraint to add
-
-### Output
-
-Nothing.
-
-### Notes
-
-It is left to the user to guarantee that the dimension of all linear constraints
-is the same.
-"""
-function addconstraint!(P::HPolytope{N},
-                        constraint::LinearConstraint{N})::Void where {N<:Real}
-    push!(P.constraints, constraint)
-    return nothing
-end
-
-"""
-    constraints_list(P::HPolytope{N})::Vector{LinearConstraint{N}} where {N<:Real}
-
-Return the list of constraints defining a polyhedron in H-representation.
-
-### Input
-
-- `P` -- polytope in H-representation
-
-### Output
-
-The list of constraints of the polyhedron.
-"""
-function constraints_list(P::HPolytope{N}
-                         )::Vector{LinearConstraint{N}} where {N<:Real}
-    return P.constraints
-end
-
-"""
-    tosimplehrep(P::HPolytope)
-
-Return the simple H-representation ``Ax ≤ b`` of a polytope.
-
-### Input
-
-- `P` -- polytope
-
-### Output
-
-The tuple `(A, b)` where `A` is the matrix of normal directions and `b` are the offsets.
-"""
-function tosimplehrep(P::HPolytope{N}) where {N}
-    A = hcat([ci.a for ci in P.constraints]...)'
-    b = [ci.b for ci in P.constraints]
-    return (A, b)
-end
-
+#=
 @require Polyhedra begin
 
 using CDDLib # default backend
@@ -308,32 +229,7 @@ function cartesian_product(P1::HPolytope, P2::HPolytope; backend=CDDLib.CDDLibra
     return HPolytope(Pcp)
 end
 
-"""
-    vertices_list(P::HPolytope{N};
-                  [backend]=CDDLib.CDDLibrary(),
-                  [prunefunc]=removevredundancy!)::Vector{Vector{N}} where {N<:Real}
 
-Return the list of vertices of a polytope in constraint representation.
-
-### Input
-
-- `P`         -- polytope in constraint representation
-- `backend`   -- (optional, default: `CDDLib.CDDLibrary()`) the polyhedral
-                 computations backend, see Polyhedra's documentation
-                 for further information
-- `prunefunc` -- (optional, default: `removevredundancy!`) function to post-process
-                 the output of `vreps`
-
-### Output
-
-List of vertices.
-"""
-function vertices_list(P::HPolytope{N};
-                       backend=CDDLib.CDDLibrary(),
-                       prunefunc=removevredundancy!)::Vector{Vector{N}} where {N<:Real}
-    P = polyhedron(P, backend)
-    prunefunc(P)
-    return [vi for vi in vreps(P)]
-end
 
 end
+=#
