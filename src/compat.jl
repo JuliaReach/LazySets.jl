3239cd2c986e4f2b88deb22fe5500de360b5cbf6
mforets <marcelo.forets-irurtia@univ-grenoble-alpes.fr>
2018-05-29 18:02:06 -0300

    update compat

--- src/compat.jl
+++ src/compat.jl
@@ -3,6 +3,16 @@ This file defines internal functions for compatibility across
 different Julia versions.
 =#
 
+using Compat
+import Compat.String
+
+if VERSION < v"0.7-"
+    import Base.LinAlg:norm, checksquare
+else
+    using SparseArrays, LinearAlgebra
+    import LinearAlgebra:norm, checksquare
+end
+
 export _At_mul_B
 
 @inline function _At_mul_B(A, B)
