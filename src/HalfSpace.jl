30824a2a81333734b18e2fc08a9075a379316675
mforets <marcelo.forets-irurtia@univ-grenoble-alpes.fr>
2018-07-20 17:06:36 -0300

    add 2D assertion and update docstring

--- src/HalfSpace.jl
+++ src/HalfSpace.jl
@@ -121,7 +121,8 @@ end
     halfspace_left(p::AbstractVector{N},
                    q::AbstractVector{N})::HalfSpace{N} where {N<:Real}
 
-Return a half-space describing the 'left' of a line segment through two points.
+Return a half-space describing the 'left' of a two-dimensional line segment through
+two points.
 
 ### Input
 
@@ -137,11 +138,45 @@ describes the left-hand side of the directed line segment `pq`.
 
 The implementation is simple: the half-space ``a⋅x ≤ b`` is calculated as
 `a = [dy, -dx]`, where ``d = (dx, dy)`` denotes the line segment
-`pq`, that is, ``\\vec{d} = \\vec{p} - \\vec{q}``, and `b = dot(p, a)` because
-`p` is on this line (we can equally choose `q` instead of `p`). 
+`pq`, that is, ``\\vec{d} = \\vec{p} - \\vec{q}``, and `b = dot(p, a)`.
+
+### Examples
+
+The left half-space of the "east" and "west" directions in two-dimensions are the
+upper and lower half-spaces:
+
+```jldoctest halfspace_left
+julia> import LazySets.halfspace_left
+
+julia> halfspace_left([0.0, 0.0], [1.0, 0.0])
+LazySets.HalfSpace{Float64}([0.0, -1.0], 0.0)
+
+julia> halfspace_left([0.0, 0.0], [-1.0, 0.0])
+LazySets.HalfSpace{Float64}([0.0, 1.0], 0.0)
+```
+
+We create a box from the sequence of line segments that describe its edges:
+
+```jldoctest halfspace_left
+julia> H1 = halfspace_left([-1.0, -1.0], [1.0, -1.0]);
+
+julia> H2 = halfspace_left([1.0, -1.0], [1.0, 1.0]);
+
+julia> H3 = halfspace_left([1.0, 1.0], [-1.0, 1.0]);
+
+julia> H4 = halfspace_left([-1.0, 1.0], [-1.0, -1.0]);
+
+julia> H = HPolygon([H1, H2, H3, H4]);
+
+julia> B = BallInf([0.0, 0.0], 1.0);
+
+julia> B ⊆ H && H ⊆ B
+true
+```
 """
 function halfspace_left(p::AbstractVector{N},
                         q::AbstractVector{N})::HalfSpace{N} where {N<:Real}
+    @assert length(p) == length(q) == 2 "the points must be two-dimensional"
     @assert p != q "the points must not be equal"
     a = [q[2] - p[2], p[1] - q[1]]
     return HalfSpace(a, dot(p, a))
