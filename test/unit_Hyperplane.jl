be9838b2f26ed3225343a3663aa606858da06f01
schillic <schillic@informatik.uni-freiburg.de>
2018-07-23 21:54:49 +0200

    add unit test for Hyperplane

--- test/unit_Hyperplane.jl
+++ test/unit_Hyperplane.jl
@@ -1,28 +1,29 @@
 for N in [Float64, Rational{Int}, Float32]
     # normal constructor
-    normal = ones(N, 3)
-    hp = Hyperplane(normal, N(5.))
+    a = ones(N, 3)
+    hp = Hyperplane(a, N(5.))
 
     # dimension
     @test dim(hp) == 3
 
     # support vector and membership function
-    function test_svec(hp, d)
-        @test σ(d, hp) ∈ hp
-        @test σ(N(2.) * d, hp) ∈ hp
+    function test_svec(hp)
+        d1 = copy(hp.a)
+        @test σ(d1, hp) ∈ hp
+        @test σ(N(2.) * d1, hp) ∈ hp
         d2 = N[1., 0., 0.]
         @test_throws ErrorException σ(d2, hp)
-        d2 = zeros(N, 3)
-        @test σ(d2, hp) ∈ hp
+        d3 = N[1., 0., 0.]
+        @test_throws ErrorException σ(d3, hp)
+        d4 = zeros(N, 3)
+        @test σ(d4, hp) ∈ hp
     end
     # tests 1
-    normal = ones(N, 3)
-    d = ones(N, 3)
-    test_svec(Hyperplane(normal, N(5.)), d)
+    a = ones(N, 3)
+    test_svec(Hyperplane(a, N(5.)))
     # tests 2
-    normal = zeros(N, 3); normal[3] = N(1.)
-    d = zeros(N, 3); d[3] = N(1.)
-    test_svec(Hyperplane(normal, N(5.)), d)
+    a = zeros(N, 3); a[3] = N(1.)
+    test_svec(Hyperplane(a, N(5.)))
 
     # an_element function and membership function
     @test an_element(hp) ∈ hp
