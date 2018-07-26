d948381b9b0f764f696eb6208b18d8aa8cb9d3ac
schillic <schillic@informatik.uni-freiburg.de>
2018-07-26 20:44:05 +0200

    add an_element function for Line

--- src/Line.jl
+++ src/Line.jl
@@ -1,4 +1,5 @@
-export Line
+export Line,
+       an_element
 
 """
     Line{N<:Real} <: LazySet{N}
@@ -27,6 +28,7 @@ struct Line{N<:Real, V<:AbstractVector{N}} <: LazySet{N}
     # default constructor with length constraint
     function Line{N, V}(a::V, b::N) where {N<:Real, V<:AbstractVector{N}}
         @assert length(a) == 2 "lines must be two-dimensional"
+        @assert a != zeros(N, 2) "the normal vector of a line must not be zero"
         return new{N, V}(a, b)
     end
 end
@@ -75,4 +77,35 @@ the more general `Hyperplane`.
 """
 function σ(d::V, L::Line) where {N<:Real, V<:AbstractVector{N}}
     return σ(d, Hyperplane(L.a, L.b))
+end
+
+"""
+    an_element(L::Line{N})::Vector{N} where N<:Real
+
+Return some element of a line.
+
+### Input
+
+- `L` -- line
+
+### Output
+
+An element on the line.
+
+### Algorithm
+
+If the ``b`` value of the line is zero, the result is the origin.
+Otherwise the result is some ``x = [x1, x2]`` such that ``a·[x1, x2] = b``.
+We first find out in which dimension ``a`` is nonzero, say, dimension 1, and
+then choose ``x1 = 1`` and accordingly ``x2 = \\frac{b - a1}{a2}``.
+"""
+function an_element(L::Line{N})::Vector{N} where N<:Real
+    if L.b == zero(N)
+        return zeros(N, 2)
+    end
+    i = L.a[1] == zero(N) ? 2 : 1
+    x = Vector{N}(2)
+    x[3-i] = one(N)
+    x[i] = (L.b - L.a[3-i]) / L.a[i]
+    return x
 end
