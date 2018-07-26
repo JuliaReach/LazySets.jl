c139b9628f38ed8d0ca06355cc1e9c95b2576255
schillic <schillic@informatik.uni-freiburg.de>
2018-07-26 20:53:26 +0200

    change concrete intersection of two lines

--- src/compat.jl
+++ src/compat.jl
@@ -8,14 +8,15 @@ import Compat.String
 export _At_mul_B
 
 if VERSION < v"0.7-"
-    import Base.LinAlg:norm, checksquare
+    import Base.LinAlg: norm, checksquare, LAPACKException, SingularException
     import Base: eye, ×
     @inline function _At_mul_B(A, B)
         return At_mul_B(A, B)
     end
 else
     using SparseArrays, LinearAlgebra
-    import LinearAlgebra:norm, checksquare, eye, ×
+    import LinearAlgebra: norm, checksquare, LAPACKException, SingularException,
+                          eye, ×
     @inline function _At_mul_B(A, B)
         return transpose(A) * B
     end
