1bae7d97c36b3e90ba9175d8d311fe88422ccb60
schillic <schillic@informatik.uni-freiburg.de>
2018-08-02 21:24:11 +0200

    remove builds for v0.6.0 and v0.6.2

--- docs/make.jl
+++ docs/make.jl
@@ -40,7 +40,7 @@ deploydocs(
     repo = "github.com/JuliaReach/LazySets.jl.git",
     target = "build",
     osname = "linux",
-    julia  = "0.6.0",
+    julia  = "0.6.4",
     deps = nothing,
     make = nothing
 )
