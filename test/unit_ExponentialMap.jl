98d8176c49a3c105034e8e38eb0f76128e25c2df
schillic <schillic@informatik.uni-freiburg.de>
2018-08-05 20:23:41 +0200

    add unit tests for ExponentialMap and related types

--- test/unit_ExponentialMap.jl
+++ test/unit_ExponentialMap.jl
@@ -1,21 +1,38 @@
-for N in [Float64] # TODO Float32
+for N in [Float64, Float32]
     # dimension (choose a multiple of 3)
     n = 3*2
 
-    # occupation probability for these benchs
-    p = 0.4
+#     p = 0.4 # occupation probability for these benchs
+#     m = sprandn(n, n, p)
 
     # sparse matrix
-    m = sprandn(n, n, p)
+    m = spzeros(N, n, n)
+    m[1, 1] = N(0.0439726)
+    m[1, 2] = N(0.206197)
+    m[3, 2] = N(-1.64948)
+    m[4, 2] = N(-1.5343)
+    m[5, 2] = N(-1.22162)
+    m[6, 3] = N(-0.281368)
+    m[2, 4] = N(0.535221)
+    m[5, 4] = N(-0.768595)
+    m[6, 4] = N(-0.499827)
+    m[3, 5] = N(-0.76484)
+    m[2, 6] = N(0.821664)
+    m[3, 6] = N(0.715326)
+    m[4, 6] = N(-0.545632)
+    m[5, 6] = N(0.312998)
 
     # a set
-    b = BallInf(ones(N, n), to_N(N, 0.1))
+    b = BallInf(ones(N, n), N(0.1))
 
     # linear map
     lm = m * b
 
     # the (lazy) exponential of a sparse matrix
     me = SparseMatrixExp(m)
+
+    # constructor from a dense matrix
+    @test_throws ErrorException SparseMatrixExp(eye(N, 2))
 
     # size
     @test size(me, 1) == n
@@ -26,11 +43,22 @@ for N in [Float64] # TODO Float32
 
     # check that the eye method is defined
     @test method_exists(eye, Tuple{typeof(me)})
-    y = eye(me)
+    y = to_N(N, eye(me))
     @test norm(y - diagm(ones(N, n))) == 0
+
+    # columns & rows
+    me2 = SparseMatrixExp(speye(N, n))
+    v = zeros(N, n)
+    v[1] = N(e)
+    @test get_columns(me2, [1, 2])[:, 1] == get_column(me2, 1) == v
+    @test get_rows(me2, [1, 2])[1, :] == transpose(get_row(me2, 1)) == v
 
     # the exponential map of a convex set, same as ExponentialMap(me, b)
     emap = me * b
+    @test emap isa ExponentialMap{N}
+
+    # dimension
+    @test dim(emap) == n
 
     # the support vector of an exponential map
     d = randn(N, n)
@@ -40,7 +68,13 @@ for N in [Float64] # TODO Float32
 
     # check consistency with respect to explicit computation of the matrix exponential
     svec_explicit = σ(d, expm(full(m)) * b)
-    @test svec ≈ svec_explicit
+    if N == Float64
+        # precision with Float32 is not sufficient
+        @test svec ≈ svec_explicit
+    end
+
+    # membership
+    # @test fill(N(e), n) ∈ me2 * b # TODO
 
     # construct an exponential map where we only pass the matrix
     #ExponentialMap(m, b) # ExponentialMap
@@ -49,7 +83,7 @@ for N in [Float64] # TODO Float32
     cpem = emap * emap # CartesianProduct of ExponentialMap
 
     # the exponential map applied to an exponential map
-    emap2 = ExponentialMap(SparseMatrixExp(2*m), emap)
+    ExponentialMap(SparseMatrixExp(2*m), emap)
 
     # for projection tests, let's assume that n is divisible by three
     assert(mod(n, 3) == 0)
@@ -77,5 +111,4 @@ for N in [Float64] # TODO Float32
     P = L * expm(full(m)) * R
     svec_explicit = σ(d, P*b)
     @test svec ≈ svec_explicit
-
 end
