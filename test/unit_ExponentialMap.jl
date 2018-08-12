55e4a024feeaaf1aedf2cb699fcd63e381a3dff4
schillic <schillic@informatik.uni-freiburg.de>
2018-08-12 10:21:54 +0200

    fix remaining 'diagm'

--- test/unit_ExponentialMap.jl
+++ test/unit_ExponentialMap.jl
@@ -44,7 +44,7 @@ for N in [Float64, Float32]
     # check that the eye method is defined
     @test hasmethod(eye, Tuple{typeof(me)})
     y = to_N(N, eye(me))
-    @test norm(y - diagm(ones(N, n))) == 0
+    @test norm(y - Diagonal(ones(N, n))) == 0
 
     # columns & rows
     me2 = SparseMatrixExp(sparse(N(1) * I, n, n))
