23b41056ca2b737df04c3373bfb5d8a6821130cb
schillic <schillic@informatik.uni-freiburg.de>
2018-08-12 11:22:09 +0200

    unify LinearAlgebra imports

--- src/compat.jl
+++ src/compat.jl
@@ -6,19 +6,18 @@ different Julia versions.
 using Compat
 using Compat: copyto!, axes, argmax
 import Compat.String
+using Compat.LinearAlgebra
+import Compat.LinearAlgebra: norm, checksquare, LAPACKException,
+                             SingularException, eye, ×
 import Compat.InteractiveUtils.subtypes
 export _At_mul_B
 
 if VERSION < v"0.7-"
-    import Base.LinAlg: norm, checksquare, LAPACKException, SingularException
-    import Base: eye, ×
     @inline function _At_mul_B(A, B)
         return At_mul_B(A, B)
     end
 else
-    using SparseArrays, LinearAlgebra
-    import LinearAlgebra: norm, checksquare, LAPACKException, SingularException,
-                          eye, ×
+    using SparseArrays
     @inline function _At_mul_B(A, B)
         return transpose(A) * B
     end
