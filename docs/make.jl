366b94c395f7545ba1e51745fd019e79e619c92a
schillic <schillic@informatik.uni-freiburg.de>
2018-08-18 17:09:48 +0200

    remove minor versions from Julia version

--- docs/make.jl
+++ docs/make.jl
@@ -40,7 +40,7 @@ deploydocs(
     repo = "github.com/JuliaReach/LazySets.jl.git",
     target = "build",
     osname = "linux",
-    julia  = "0.6.4",
+    julia  = "0.6",
     deps = nothing,
     make = nothing
 )
