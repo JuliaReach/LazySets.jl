7acabcb668d12cbe40454d9a930d573dc60b4a2b
mforets <mforets@gmail.com>
2018-09-30 20:27:31 -0300

    fix &&

--- test/runtests.jl
+++ test/runtests.jl
@@ -36,7 +36,9 @@ else
 end
 
 if test_suite_polyhedra || test_suite_plotting
-    VERSION < v"0.7-" && Pkg.add("Polyhedra") && Pkg.add("CDDLib")
+    if VERSION < v"0.7-"
+        Pkg.add("Polyhedra"); Pkg.add("CDDLib")
+    end
     using Polyhedra
 
     # fix namespace conflicts with Polyhedra
