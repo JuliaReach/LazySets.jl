cf3d9840fcfe372e9f28d91a7e20ada349c69617
schillic <schillic@informatik.uni-freiburg.de>
2018-09-30 21:46:27 +0200

    remove failing unit test ('empty' HPolytope is not empty)

--- test/unit_Polytope.jl
+++ test/unit_Polytope.jl
@@ -96,7 +96,6 @@ if test_suite_polyhedra
     # check isempty function of the AbstractPolytope interface
     @test isempty(empty_H_polytope)
     @test isempty(empty_V_polytope)
-    is_intersection_empty(empty_H_polytope, empty_V_polytope)
 end
 
 # Polyhedra tests that only work with Float64
