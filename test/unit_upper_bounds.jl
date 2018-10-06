9f40b3c61e537f4270788102eaffe9c08a57eefc
mforets <mforets@gmail.com>
2018-10-06 11:12:03 -0300

    fix tests

--- test/unit_upper_bounds.jl
+++ test/unit_upper_bounds.jl
@@ -3,10 +3,8 @@ import LazySets.Approximations.ρ_upper_bound
 X = Hyperrectangle(low=[0.0, 0.0], high=[1.0, 1.0])
 d = [1.0, 2.0]
 @test ρ(d, X) == ρ_upper_bound(d, X)
-@test ρ(d, X) == ρ_upper_bound(d, X)
 
-@test_throws ErrorException ρ(d, X ∩ X)
 using Optim
-@test ρ(d, X) = ρ_upper_bound(d, X ∩ X)
+@test ρ(d, X) == ρ_upper_bound(d, X ∩ X)
 M = [1.0 1.0; 1.0 1.0]
 @test isapprox(ρ_upper_bound(d, X ∩ (M * X)), 2.5, atol=1e-6)
