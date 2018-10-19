eb35530c5fe5f4d9ad04289251880554ec4dda69
schillic <schillic@informatik.uni-freiburg.de>
2018-10-19 23:41:55 +0200

    make variables non-global

--- test/unit_Intersection.jl
+++ test/unit_Intersection.jl
@@ -62,37 +62,38 @@ end
 # ======================
 # Tests for Float64 only
 # ======================
+for N in [Float64]
+    # HalfSpace vs. Ball1 intersection
+    X = Ball1(zeros(2), N(1));
+    d = normalize(N[1, 0])
 
-# HalfSpace vs. Ball1 intersection
-X = Ball1(zeros(2), 1.0);
-d = normalize([1.0, 0.0])
+    # flat intersection at x = 1
+    H = HalfSpace(N[-1, 0], N(-1)); # x >= 1
 
-# flat intersection at x = 1
-H = HalfSpace([-1.0, 0.0], -1.0); # x >= 1
+    # default algorithm
+    @test ρ(d, X ∩ H) == ρ(d, H ∩ X) == N(1)
 
-# default algorithm
-@test ρ(d, X ∩ H) == ρ(d, H ∩ X) == 1.0
+    # intersection at x = 0
+    H = HalfSpace(N[1, 0], N(0)); # x <= 0
 
-# intersection at x = 0
-H = HalfSpace([1.0, 0.0], 0.0); # x <= 0
+    # default algorithm
+    @test ρ(d, X ∩ H) < 1e-6 && ρ(d, H ∩ X) < 1e-6
 
-# default algorithm
-@test ρ(d, X ∩ H) < 1e-6 && ρ(d, H ∩ X) < 1e-6
+    # specify  line search algorithm
+    @test ρ(d, X ∩ H, algorithm="line_search") < 1e-6 &&
+        ρ(d, H ∩ X, algorithm="line_search") < 1e-6
 
-# specify  line search algorithm
-@test ρ(d, X ∩ H, algorithm="line_search") < 1e-6 &&
-      ρ(d, H ∩ X, algorithm="line_search") < 1e-6
+    # HalfSpace vs. Ball2 intersection
+    B2 = Ball2(zeros(2), N(1));
+    @test ρ(d, B2 ∩ H) < 1e-6 && ρ(d, H ∩ B2) < 1e-6
 
-# HalfSpace vs. Ball2 intersection
-B2 = Ball2(zeros(2), 1.0);
-@test ρ(d, B2 ∩ H) < 1e-6 && ρ(d, H ∩ B2) < 1e-6
-
-# Ball1 vs. Hyperplane intersection
-H = Hyperplane([1.0, 0.0], 0.5); # x = 0.5
-@test isapprox(ρ(d, X ∩ H, algorithm="line_search"), 0.5, atol=1e-6)
-# For the projection algorithm, if the linear map is taken lazily we can use Ball1
-@test isapprox(ρ(d, X ∩ H, algorithm="projection", lazy_linear_map=true), 0.5, atol=1e-6)
-# But the default is to take the linear map concretely; in this case, we *may*
-# need Polyhedra (in the general case), for the concrete linear map. As a valid workaround
-# if we don't want to load Polyhedra here is to convert the given set to a polygon in V-representation
-@test isapprox(ρ(d, convert(VPolygon, X) ∩ H, algorithm="projection", lazy_linear_map=false), 0.5, atol=1e-6)
+    # Ball1 vs. Hyperplane intersection
+    H = Hyperplane(N[1, 0], N(0.5)); # x = 0.5
+    @test isapprox(ρ(d, X ∩ H, algorithm="line_search"), N(0.5), atol=1e-6)
+    # For the projection algorithm, if the linear map is taken lazily we can use Ball1
+    @test isapprox(ρ(d, X ∩ H, algorithm="projection", lazy_linear_map=true), N(0.5), atol=1e-6)
+    # But the default is to take the linear map concretely; in this case, we *may*
+    # need Polyhedra (in the general case), for the concrete linear map. As a valid workaround
+    # if we don't want to load Polyhedra here is to convert the given set to a polygon in V-representation
+    @test isapprox(ρ(d, convert(VPolygon, X) ∩ H, algorithm="projection", lazy_linear_map=false), N(0.5), atol=1e-6)
+end
