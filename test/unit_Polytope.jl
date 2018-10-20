5b3c9ad781b58a053536e385f416d184cd80f880
schillic <schillic@informatik.uni-freiburg.de>
2018-10-20 21:33:04 +0200

    exclude unit test for v1.0

--- test/unit_Polytope.jl
+++ test/unit_Polytope.jl
@@ -190,9 +190,11 @@ if test_suite_polyhedra
         p3 = VPolygon(vertices_list(p2))
         cap = intersection(p1, p3)
         @test vertices_list(cap) == [N[1, 1]]
-        p4 = BallInf(N[2, 2], N(1))
-        cap = intersection(p1, p4)
-        @test vertices_list(cap) == [N[1, 1]]
+        @static if VERSION < v"1.0" # does not work in v1.0 (see #834)
+            p4 = BallInf(N[2, 2], N(1))
+            cap = intersection(p1, p4)
+            @test vertices_list(cap) == [N[1, 1]]
+        end
 
         # convex hull
         v1 = N[1, 0]
