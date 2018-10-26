03ef52bf70b17a540ad77e3db7e1a829de7ef4a8
schillic <schillic@informatik.uni-freiburg.de>
2018-10-26 20:45:01 +0200

    add option to choose algorithm for <=

--- src/AbstractPolygon.jl
+++ src/AbstractPolygon.jl
@@ -48,7 +48,7 @@ The ambient dimension of the polygon, which is 2.
 end
 
 """
-    jump2pi(x::N)::N where {N<:AbstractFloat}
+    jump2pi(x::N)::N where N<:AbstractFloat
 
 Return ``x + 2π`` if ``x`` is negative, otherwise return ``x``.
 
@@ -75,18 +75,18 @@ julia> jump2pi(0.5)
 0.5
 ```
 """
-@inline function jump2pi(x::N)::N where {N<:AbstractFloat}
+@inline function jump2pi(x::N)::N where N<:AbstractFloat
     x < zero(N) ? 2 * pi + x : x
 end
 
 """
-    quadrant(w::AbstractVector{N})::Int where {N<:Real}
+    quadrant(w::AbstractVector{N})::Int where N<:Real
 
 Compute the quadrant where the direction `w` belongs.
 
 ### Input
 
-- `w` --  direction
+- `w` -- direction
 
 ### Output
 
@@ -107,21 +107,21 @@ The idea is to encode the following logic function:
 This function is inspired from AGPX's answer in:
 [Sort points in clockwise order?](https://stackoverflow.com/a/46635372)
 """
-@inline function quadrant(w::AbstractVector{N})::Int where {N<:Real}
+@inline function quadrant(w::AbstractVector{N})::Int where N<:Real
     dwx = w[1] >= zero(N) ? 1 : 0
     dwy = w[2] >= zero(N) ? 1 : 0
     return (1 - dwx) + (1 - dwy) + ((dwx & (1 - dwy)) << 1)
 end
 
 """
-    <=(u::AbstractVector{N}, v::AbstractVector{N})::Bool where {N<:Real}
+    le_quadrant(u::AbstractVector{N}, v::AbstractVector{N})::Bool where N<:Real
 
 Compares two 2D vectors by their direction.
 
 ### Input
 
-- `u` --  first 2D direction
-- `v` --  second 2D direction
+- `u` -- first 2D direction
+- `v` -- second 2D direction
 
 ### Output
 
@@ -134,30 +134,33 @@ direction (1, 0).
 
 ### Algorithm
 
-The implementation checks the quadrant of each direction, and compares directions
-using the right-hand rule. In particular, it doesn't use the arctangent.
+The implementation checks the quadrant of each direction, and compares
+directions using the right-hand rule.
+In particular, it doesn't use the arctangent.
 """
-function <=(u::AbstractVector{N}, v::AbstractVector{N})::Bool where {N<:Real}
+function le_quadrant(u::AbstractVector{N},
+                     v::AbstractVector{N})::Bool where N<:Real
     qu, qv = quadrant(u), quadrant(v)
     if qu == qv
         # same quadrant, check right-turn with center 0
-        ans = u[1] * v[2] - v[1] * u[2] >= zero(N)
+        le = u[1] * v[2] - v[1] * u[2] >= zero(N)
     else
         # different quadrant
-        ans = qu < qv
+        le = qu < qv
     end
-    return ans
+    return le
 end
 
 """
-    <=(u::AbstractVector{N}, v::AbstractVector{N})::Bool where {N<:AbstractFloat}
+    le_jump2pi(u::AbstractVector{N},
+               v::AbstractVector{N})::Bool where N<:AbstractFloat
 
 Compares two 2D vectors by their direction.
 
 ### Input
 
-- `u` --  first 2D direction
-- `v` --  second 2D direction
+- `u` -- first 2D direction
+- `v` -- second 2D direction
 
 ### Output
 
@@ -174,9 +177,50 @@ The implementation uses the arctangent function with sign, `atan`, which for two
 arguments implements the
 [`atan2` function](https://en.wikipedia.org/wiki/Atan2).
 """
-function <=(u::AbstractVector{N},
-            v::AbstractVector{N})::Bool where {N<:AbstractFloat}
+function le_jump2pi(u::AbstractVector{N},
+                    v::AbstractVector{N})::Bool where N<:AbstractFloat
     return jump2pi(atan(u[2], u[1])) <= jump2pi(atan(v[2], v[1]))
+end
+
+"""
+    <=(u::AbstractVector{N},
+       v::AbstractVector{N};
+       [algorithm]="quadrant")::Bool where N<:Real
+
+Compares two 2D vectors by their direction.
+
+### Input
+
+- `u`         -- first 2D direction
+- `v`         -- second 2D direction
+- `algorithm` -- (optional; default: "quadrant") algorithm backend
+
+### Output
+
+True iff ``\\arg(u) [2π] ≤ \\arg(v) [2π]``
+
+### Notes
+
+The argument is measured in counter-clockwise fashion, with the 0 being the
+direction (1, 0).
+"""
+function <=(u::AbstractVector{N},
+            v::AbstractVector{N},
+            algorithm::String)::Bool where N<:Real
+    if algorithm == "quadrant"
+        res = le_quadrant(u, v)
+    elseif algorithm == "jump2pi"
+        res = le_jump2pi(u, v)
+    else
+        error("unknown algorithm $algorithm")
+    end
+    return res
+end
+
+# faster default implementation
+function <=(u::AbstractVector{N},
+            v::AbstractVector{N})::Bool where N<:Real
+    return le_quadrant(u, v)
 end
 
 """
