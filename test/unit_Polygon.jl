ace5729a4ed9cdd1ab70d252a8e085da4a02a65d
schillic <schillic@informatik.uni-freiburg.de>
2018-11-20 21:24:12 +0100

    fix unit test

--- test/unit_Polygon.jl
+++ test/unit_Polygon.jl
@@ -29,7 +29,7 @@ for N in [Float64, Float32, Rational{Int}]
     @test LazySets.isredundant(h2, h1, h3)
     @test !LazySets.isredundant(h3, h2, h4)
     @test LazySets.isredundant(h2, h2, h3) && LazySets.isredundant(h3, h2, h3)
-    p2 = HPolygon{N}([h1, h2, h3, h4]) # sorted in the right order
+    p2 = HPolygon{N}([h1, h2, h3, h4]; sort_constraints=false) # sorted in the right order
     @test length(p2.constraints) == 4
     remove_redundant_constraints!(p2)
     @test length(p2.constraints) == 3
