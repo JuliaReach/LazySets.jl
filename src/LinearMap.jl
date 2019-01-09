09deb3f872e8ed548cf5aa19908edd679ce09779
schillic <schillic@informatik.uni-freiburg.de>
2019-01-09 19:51:20 +0100

    faster boundedness check for invertible LinearMap

--- src/LinearMap.jl
+++ src/LinearMap.jl
@@ -216,11 +216,17 @@ Determine whether a linear map is bounded.
 ### Algorithm
 
 We first check if the matrix is zero or the wrapped set is bounded.
+If not, we perform a sufficient check whether the matrix is invertible.
+If the matrix is invertible, then the map being bounded is equivalent to the
+wrapped set being bounded, and hence the map is unbounded.
 Otherwise, we check boundedness via [`isbounded_unit_dimensions`](@ref).
 """
 function isbounded(lm::LinearMap)::Bool
     if iszero(lm.M) || isbounded(lm.X)
         return true
+    end
+    if isinvertible_sufficient(lm.M)
+        return false
     end
     return isbounded_unit_dimensions(lm)
 end
