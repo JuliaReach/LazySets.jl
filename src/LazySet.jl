0ca36f0af8266cf03e2f7976293ee8b6521d2412
Christian Schilling <schillic@informatik.uni-freiburg.de>
2019-01-23 07:39:23 -0300

    Update src/LazySet.jl
    Co-Authored-By: mforets <mforets@gmail.com>

--- src/LazySet.jl
+++ src/LazySet.jl
@@ -340,7 +340,7 @@ offsets.
 
 This function uses `constraints_list(S)`. It is a fallback implementation that
 works only for those sets that can be represented exactly by a list of linear
-constraints, and that this list is available through the `constraints_list(S)`
+constraints, which is available through the `constraints_list(S)`
 function.
 """
 tosimplehrep(S::LazySet) = tosimplehrep(constraints_list(S))
