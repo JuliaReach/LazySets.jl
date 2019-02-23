e53fba9062257ac17aac1abef6f030fd2ea9af22
Marcelo Forets <mforets@gmail.com>
2019-02-23 19:46:12 +0100

    Update src/AbstractHyperrectangle.jl
    Co-Authored-By: schillic <schillic@informatik.uni-freiburg.de>

--- src/AbstractHyperrectangle.jl
+++ src/AbstractHyperrectangle.jl
@@ -108,7 +108,7 @@ The support vector in the given direction.
 If the direction has norm zero, the vertex with biggest values is returned.
 """
 function σ(d::AbstractVector{N}, H::AbstractHyperrectangle{N}) where {N<:Real}
-    @assert length(d) == dim(H) "an $(length(d))-dimensional vector is " *
+    @assert length(d) == dim(H) "a $(length(d))-dimensional vector is " *
                                 "incompatible with a $(dim(H))-dimensional set"
     return center(H) .+ sign_cadlag.(d) .* radius_hyperrectangle(H)
 end
