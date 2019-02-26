4cf632fb80bf99508d11feff8d7566ff43eca6bd
mforets <mforets@gmail.com>
2019-02-26 11:18:04 -0300

    refactor convex hull for higher dimensional point sets

--- src/concrete_convex_hull.jl
+++ src/concrete_convex_hull.jl
@@ -1,21 +1,57 @@
-"""
-    convex_hull(points::Vector{VN}; [algorithm]::String="monotone_chain"
-               )::Vector{VN} where {N<:Real, VN<:AbstractVector{N}}
+function default_convex_hull_algorithm(points)
+    if length(points) != 0 && length(first(points)) == 2
+        return "monotone_chain"
+    else
+        return nothing
+    end
+end
 
-Compute the convex hull of points in the plane.
+"""
+    convex_hull(points::Vector{VN};
+                [algorithm]=default_convex_hull_algorithm(points),
+                [backend]=nothing
+                )::Vector{VN} where {N<:Real, VN<:AbstractVector{N}}
+
+Compute the convex hull of the given points.
 
 ### Input
 
-- `points`    -- list of 2D vectors
+- `points`    -- list of vectors
 - `algorithm` -- (optional, default: `"monotone_chain"`) the convex hull
-                 algorithm, valid options are:
-
-    * `"monotone_chain"`
-    * `"monotone_chain_sorted"`
+                 algorithm, see valid options below
+- `backend`   -- (optional, default: `"nothing"`) polyhedral computation backend
+                 for higher-dimensional point sets
 
 ### Output
 
-The convex hull as a list of 2D vectors with the coordinates of the points.
+The convex hull as a list of vectors with the coordinates of the points.
+
+### Algorithm
+
+A pre-processing step treats the cases with `0`, `1` and `2` points in any dimension.
+For more than `3` points, the algorithm used depends on the dimension.
+
+For the one-dimensional case we sort the points and return the minimum and maximum points,
+in that order.
+
+The two-dimensional case is handled with a planar convex hull algorithm. The
+following algorithms are available:
+
+* `"monotone_chain"`        -- compute the convex hull of points in the plane
+                               using Andrew's monotone chain method
+* `"monotone_chain_sorted"` -- the same as `"monotone_chain"` but assuming that
+                               the points are already sorted in counter-clockwise
+                               fashion
+
+See the reference docstring of each of those algorithms for details.
+
+The higher dimensional case is treated using the concrete polyhedra library
+`Polyhedra`, that gives access to libraries such as `CDDLib` and `ConvexHull.jl`.
+These libraries can be chosen by the setting the `backend` argument.
+
+### Notes
+
+For the in-place version use `convex_hull!` instead of `convex_hull`.
 
 ### Examples
 
@@ -40,57 +76,80 @@ julia> plot([Tuple(pi) for pi in points], seriestype=:scatter);
 julia> plot!(VPolygon(hull), alpha=0.2);
 ```
 """
-function convex_hull(points::Vector{VN}; algorithm::String="monotone_chain"
-                    )::Vector{VN} where {N<:Real, VN<:AbstractVector{N}}
-    return convex_hull!(copy(points), algorithm=algorithm)
+function convex_hull(points::Vector{VN};
+                     algorithm=default_convex_hull_algorithm(points),
+                     backend=nothing
+                     )::Vector{VN} where {N<:Real, VN<:AbstractVector{N}}
+    return convex_hull!(copy(points), algorithm=algorithm, backend=backend)
 end
 
-"""
-    convex_hull!(points::Vector{VN}; [algorithm]::String="monotone_chain"
-                )::Vector{VN} where {N<:Real, VN<:AbstractVector{N}}
-
-Compute the convex hull of points in the plane, in-place.
-
-### Input
-
-- `points`    -- list of 2D vectors (is modified)
-- `algorithm` -- (optional, default: `"monotone_chain"`) the convex hull
-                 algorithm; valid options are:
-
-    * `"monotone_chain"`
-    * `"monotone_chain_sorted"`
-
-### Output
-
-The convex hull as a list of 2D vectors with the coordinates of the points.
-
-### Notes
-
-See the non-modifying version `convex_hull` for more details.
-"""
 function convex_hull!(points::Vector{VN};
-                      algorithm::String="monotone_chain"
-                     )::Vector{VN} where {N<:Real, VN<:AbstractVector{N}}
+                      algorithm=default_convex_hull_algorithm(points),
+                      backend=nothing)::Vector{VN} where {N<:Real, VN<:AbstractVector{N}}
 
-    isempty(points) && return points
+    m = length(points)
 
-    @assert length(points[1]) == 2 "this function can only be used for 2-dimensional " *
-            "point clouds; try converting the points to a `VPolytope` first"
+    # =====================
+    # Treat special cases
+    # =====================
 
-    if length(points) == 1
+    # zero or one point
+    if m == 0 || m == 1
         return points
-    elseif length(points) == 2 # See #876
-        p1, p2 = points[1], points[2]
-        if p1 == p2  # check for redundancy
-            pop!(points)
-        elseif p1 <= p2
-            nothing
-        else
-            points[1], points[2] = p2, p1
+    end
+
+    n = length(first(points))
+
+    # two points
+    if m == 2
+        if n == 1 || n == 2
+            # special case, see #876
+            p1, p2 = points[1], points[2]
+            if p1 == p2  # check for redundancy
+                pop!(points)
+            elseif p1 <= p2
+                nothing
+            else
+                points[1], points[2] = p2, p1
+            end
         end
         return points
     end
 
+    # ===========================
+    # Dispatch for general cases
+    # ===========================
+
+    # _convex_hull_x can assume that there are at least three points
+    if n == 1
+        return _convex_hull_1d!(points)
+    elseif n == 2 
+        return _convex_hull_2d!(points, algorithm=algorithm)
+    else
+        return _convex_hull_nd!(points, backend=backend)
+    end
+end
+
+function _convex_hull_1d!(points::Vector{VN})::Vector{VN} where {N<:Real, VN<:AbstractVector{N}}
+    n = length(points)
+    sort!(points)
+    deleteat!(points, 2:n-1)
+end
+
+function _convex_hull_nd!(points::Vector{VN};
+                          backend=nothing
+                          )::Vector{VN} where {N<:Real, VN<:AbstractVector{N}}
+    V = VPolytope(points)
+    if backend == nothing
+        backend = default_polyhedra_backend(V, N)
+    end
+    Vch = remove_redundant_vertices(V, backend=backend)
+    return vertices_list(Vch)
+end
+
+function _convex_hull_2d!(points::Vector{VN};
+                          algorithm::String="monotone_chain"
+                          )::Vector{VN} where {N<:Real, VN<:AbstractVector{N}}
     if algorithm == "monotone_chain"
         return monotone_chain!(points)
     elseif algorithm == "monotone_chain_sorted"
@@ -98,6 +157,17 @@ function convex_hull!(points::Vector{VN};
     else
         error("the convex hull algorithm $algorithm is unknown")
     end
+end
+
+function convex_hull(P1::Vector{N},
+                     P2::HPoly{N};
+                     backend=default_polyhedra_backend(P1, N)) where {N}
+    @assert isdefined(@__MODULE__, :Polyhedra) "this function "*
+        "needs the package 'Polyhedra'"
+    Pch = convexhull(polyhedron(P1; backend=backend),
+                     polyhedron(P2; backend=backend))
+    removehredundancy!(Pch)
+    return convert(typeof(P1), Pch)
 end
 
 """
