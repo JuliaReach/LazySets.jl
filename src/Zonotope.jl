5013aeeb8ddc90276c9f6eb8177d377954502e97
Aadesha <aadeshdeshmukh25@gmail.com>
2019-04-29 22:31:26 +0530

    getter function for the generator of a Zonotope
    Update Zonotope.jl
    
    Update Zonotope.jl
    
    Update representations.md
    
    Update unit_Zonotope.jl

--- src/Zonotope.jl
+++ src/Zonotope.jl
@@ -9,7 +9,8 @@ export Zonotope,
        scale,
        ngens,
        reduce_order,
-       constraints_list
+       constraints_list,
+       generators
 
 """
     Zonotope{N<:Real} <: AbstractCentrallySymmetricPolytope{N}
@@ -129,6 +130,22 @@ function center(Z::Zonotope{N})::Vector{N} where {N<:Real}
     return Z.center
 end
 
+"""
+   generators(Z::Zonotope)
+
+Return the generators of a zonotope.
+
+### Input
+
+- `Z` -- zonotope
+
+### Output
+
+The generators of the zonotope.
+"""
+function generators(Z::Zonotope)
+    return Z.generators
+end
 
 # --- AbstractPolytope interface functions ---
 
