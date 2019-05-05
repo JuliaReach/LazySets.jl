dbcb333e1aeb78b07590ad59e75bff4c71b00481
mforets <mforets@gmail.com>
2019-05-05 15:31:46 +1000

    add algorithm

--- src/difference.jl
+++ src/difference.jl
@@ -46,6 +46,30 @@ Depending on the position of the intervals, the output is one of the following:
 - An `EmptySet`.
 - An `Interval`.
 - A `UnionSet` of two `Interval` sets.
+
+### Algorithm
+
+Let ``I₁ = [a, b]`` and ``I₂ = [c, d]`` be intervals. Their set difference is
+``I₁ \\setminus I₂ = \\{x: x ∈ I₁ \\text{ and } x ∉ I₂ \\}`` and depending on their
+position three different results may occur:
+
+- If ``I₁`` and ``I₂`` do not overlap, i.e. if their intersection is empty, then the
+  set difference is just ``I₁``.
+- Otherwise, let `I₁₂ = I₁ ∩ I₂` and assume that it is not empty, then either
+  ``I₁₂`` splits `I₁` into one interval or into two intervals. The latter case
+   happens when the inclusion is strict.
+
+To check for strict inclusion, we asssume that the inclusion is strict and then
+check if the resulting intervals that cover `I₁` (one to its left and one to its
+right, let them be `Ileft` and `Iright`), obtained by intersection with `I₂`,
+are flat or not. Three cases may arise:
+
+- If both `Ileft` and `Iright` are flat then it means that `I₁ = I₂`, then the
+  set difference is the empty set.
+- If only `Ileft` is flat, then the remaining interval not covered by `I₂` is
+  `Iright`. In a similar manner, if only `Iright` is flat, then `Ileft` is returned.
+- Finally, if none of the intervals is flat, then `I₂` is strictly contained in
+  `I₁` and the set union of `Ileft` and `Iright` is returned.
 """
 function difference(I1::IN, I2::IN)::Union{EmptySet{N}, IN, UnionSet{N, IN, IN}} where {N, IN<:Interval{N}}
     I12 = intersection(I1, I2)
@@ -55,17 +79,22 @@ function difference(I1::IN, I2::IN)::Union{EmptySet{N}, IN, UnionSet{N, IN, IN}}
         Ileft = Interval(min(I1), min(I12))
         Iright = Interval(max(I12), max(I1))
 
-        Ileft_iszero = isapproxzero(IntervalArithmetic.diam(Ileft.dat))
-        Iright_iszero = isapproxzero(IntervalArithmetic.diam(Iright.dat))
+        flat_left = isflat(Ileft)
+        flat_right = isflat(Iright)
 
-        if Ileft_iszero && Iright_iszero
+        if flat_left && flat_right
             return EmptySet{N}()
-        elseif Ileft_iszero && !Iright_iszero
+        elseif flat_left && !flat_right
             return Iright
-        elseif !Ileft_iszero && Iright_iszero
+        elseif !flat_left && flat_right
             return Ileft
         else
             return UnionSet(Ileft, Iright)
         end
     end
+end
+
+# return whether is flat, i.e. if the extreme values or the interval coincide.
+function isflat(I::Interval)
+    return isapproxzero(IntervalArithmetic.diam(I.dat))
 end
