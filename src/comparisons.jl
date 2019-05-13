a887eb37cd1009e80cea39619c6aac027d078d22
schillic <schillic@informatik.uni-freiburg.de>
2019-05-13 15:57:27 +0200

    line breaks

--- src/comparisons.jl
+++ src/comparisons.jl
@@ -47,7 +47,8 @@ arguments to a common numeric type, returning them as a tuple. The conversion
 is such that the common type to which the values are converted can represent
 them as faithfully as possible.
 """
-_leq(x::N, y::M; kwargs...) where {N<:Real, M<:Real} = _leq(promote(x, y)...; kwargs...)
+_leq(x::N, y::M; kwargs...) where {N<:Real, M<:Real} =
+    _leq(promote(x, y)...; kwargs...)
 
 """
     _geq(x::Real, y::Real; [kwargs...])
@@ -107,8 +108,8 @@ A boolean that is `true` iff `x ≈ 0`.
 
 ### Algorithm
 
-It is considered that `x ≈ 0` whenever `x` (in absolute value) is smaller than the
-tolerance for zero, `ztol`.
+It is considered that `x ≈ 0` whenever `x` (in absolute value) is smaller than
+the tolerance for zero, `ztol`.
 """
 function isapproxzero(x::N; ztol::Real=ABSZTOL(N)) where {N<:AbstractFloat}
     return abs(x) <= ztol
@@ -127,7 +128,8 @@ Determine if `x` is approximately equal to `y`.
 - `x`    -- number
 - `y`    -- another number (of the same numeric type as `x`)
 - `rtol` -- (optional, default: `Base.rtoldefault(N)`) relative tolerance
-- `ztol` -- (optional, default: `ABSZTOL(N)`) absolute tolerance for comparison against zero
+- `ztol` -- (optional, default: `ABSZTOL(N)`) absolute tolerance for comparison
+            against zero
 - `atol` -- (optional, default: `zero(N)`) absolute tolerance
 
 ### Output
@@ -136,14 +138,16 @@ A boolean that is `true` iff `x ≈ y`.
 
 ### Algorithm
 
-We first check if `x` and `y` are both approximately zero, using `isapproxzero(x, y)`.
+We first check if `x` and `y` are both approximately zero, using
+`isapproxzero(x, y)`.
 If that fails, we check if `x ≈ y`, using Julia's `isapprox(x, y)`.
-In the latter check we use `atol` absolute tolerance and `rtol` relative tolerance.
+In the latter check we use `atol` absolute tolerance and `rtol` relative
+tolerance.
 
-Comparing to zero with default tolerances is a special case in Julia's `isapprox`,
-see the last paragraph in `?isapprox`. This function tries to combine `isapprox`
-with its default values and a branch for `x ≈ y ≈ 0` which includes
-`x == y == 0` but also admits a tolerance `ztol`.
+Comparing to zero with default tolerances is a special case in Julia's
+`isapprox`, see the last paragraph in `?isapprox`. This function tries to
+combine `isapprox` with its default values and a branch for `x ≈ y ≈ 0` which
+includes `x == y == 0` but also admits a tolerance `ztol`.
 
 Note that if `x = ztol` and `y = -ztol`, then `|x-y| = 2*ztol` and still
 `_isapprox` returns `true`.
@@ -172,7 +176,8 @@ Determine if `x` is smaller than or equal to `y`.
 - `x`    -- number
 - `y`    -- another number (of the same numeric type as `x`)
 - `rtol` -- (optional, default: `Base.rtoldefault(N)`) relative tolerance
-- `ztol` -- (optional, default: `ABSZTOL(N)`) absolute tolerance for comparison against zero
+- `ztol` -- (optional, default: `ABSZTOL(N)`) absolute tolerance for comparison
+            against zero
 - `atol` -- absolute tolerance
 
 ### Output
@@ -181,9 +186,9 @@ A boolean that is `true` iff `x <= y`.
 
 ### Algorithm
 
-The `x <= y` comparison is split into `x < y` or `x ≈ y`; the latter is implemented
-by extending Juila's built-in `isapprox(x, y)` with an absolute tolerance that is
-used to compare against zero.
+The `x <= y` comparison is split into `x < y` or `x ≈ y`; the latter is
+implemented by extending Juila's built-in `isapprox(x, y)` with an absolute
+tolerance that is used to compare against zero.
 """
 function _leq(x::N, y::N;
               rtol::Real=Base.rtoldefault(N),
