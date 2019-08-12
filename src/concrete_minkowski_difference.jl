137b1d02d9e60cb6de78cf35e21ebce3647b238e
ueliwechsler <wueli@student.ethz.ch>
2019-08-12 04:59:20 -0400

    Correction error message applicable(constraints_list, Q)
    Co-Authored-By: Marcelo Forets <mforets@gmail.com>

--- src/concrete_minkowski_difference.jl
+++ src/concrete_minkowski_difference.jl
@@ -24,7 +24,7 @@ available and that the set `P` is bounded.
 function minkowski_difference(Q::LazySet{N}, P::LazySet{N}) where {N<:Real}
 
     @assert applicable(constraints_list, Q)  "this function " *
-    "requires that the list of constraints of its is applicable, but it is not; " *
+        "requires that the list of constraints of its first argument is applicable, but it is not; " *
     "try overapproximating with an `HPolytope` first"
     @assert isbounded(P)
 
