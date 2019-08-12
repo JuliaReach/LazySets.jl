220d6ad04c7da9805397a5b947938526f90e2718
ueliwechsler <wueli@student.ethz.ch>
2019-08-12 05:00:24 -0400

    Spacing in error message
    Co-Authored-By: Christian Schilling <schillic@informatik.uni-freiburg.de>

--- src/concrete_minkowski_difference.jl
+++ src/concrete_minkowski_difference.jl
@@ -25,7 +25,7 @@ function minkowski_difference(Q::LazySet{N}, P::LazySet{N}) where {N<:Real}
 
     @assert applicable(constraints_list, Q)  "this function " *
         "requires that the list of constraints of its first argument is applicable, but it is not; " *
-    "try overapproximating with an `HPolytope` first"
+        "try overapproximating with an `HPolytope` first"
     @assert isbounded(P) "this function requires that its second argument is bounded, but it is not"
 
     A, b = tosimplehrep(Q)
