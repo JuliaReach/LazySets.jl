52b33763904e4f154f4af305a5225239a0ca77d6
schillic <schillic@informatik.uni-freiburg.de>
2019-08-27 15:20:58 +0200

    fix math formula in docs

--- src/VPolytope.jl
+++ src/VPolytope.jl
@@ -142,10 +142,10 @@ Let ``\\{v_j\\}`` be the ``m`` vertices of `P`.
 Then we solve the following ``m``-dimensional linear program.
 
 ```math
-\\max 0 \\text{s.t.}
-\bigwedge_{i=1}^n \\sum_{j=1}^m λ_j v_j[i] = x[i]
-\\sum_{j=1}^m λ_j = 1
-\bigwedge_{j=1}^m λ_j ≥ 0
+\\max 0 \\text{ s.t. }
+\\bigwedge_{i=1}^n \\sum_{j=1}^m λ_j v_j[i] = x[i]
+∧ \\sum_{j=1}^m λ_j = 1
+∧ \\bigwedge_{j=1}^m λ_j ≥ 0
 ```
 """
 function ∈(x::AbstractVector{N}, P::VPolytope{N};
