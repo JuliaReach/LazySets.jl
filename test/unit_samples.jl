9d332f2cdb01cc2aebae7f287fdb3c4f965cf9fe
ueliwechsler <wueli@student.ethz.ch>
2019-09-07 14:33:19 +0200

    #1669 Rejection sampling follow-up (#1671)
    * Remove _canonical_length tests
    
    * Remove _canonical_length and add RejectionSampler as subtype of Sampler
    
    * Updates @docs bindings in utils.md
    
    * Update src/Utils/samples.jl
    
    Co-Authored-By: Marcelo Forets <mforets@gmail.com>
    
    * Update src/Utils/samples.jl
    
    Co-Authored-By: Christian Schilling <schillic@informatik.uni-freiburg.de>
    
    * Update src/Utils/samples.jl
    
    Co-Authored-By: Christian Schilling <schillic@informatik.uni-freiburg.de>
    
    * Update src/Utils/samples.jl
    
    Co-Authored-By: Christian Schilling <schillic@informatik.uni-freiburg.de>
    
    * Update src/Utils/samples.jl
    
    Co-Authored-By: Christian Schilling <schillic@informatik.uni-freiburg.de>
    
    * Update src/Utils/samples.jl
    
    Co-Authored-By: Christian Schilling <schillic@informatik.uni-freiburg.de>
    
    * Update src/Utils/samples.jl
    
    Co-Authored-By: Christian Schilling <schillic@informatik.uni-freiburg.de>
    
    * docs tweaks
    
    * Update docs/src/lib/utils.md

--- test/unit_samples.jl
+++ test/unit_samples.jl
@@ -16,11 +16,4 @@ p1 = LazySets.sample(P1)
 p1_samples = LazySets.sample(P1, 100)
 @test sum(p1_samples .∈ Ref(P1)) == length(p1_samples)
 
-# test _canonical_length
-@test LazySets._canonical_length(P1+-P1.center) ≈ radius(P1)*[-ones(5) ones(5)]
-@test LazySets._canonical_length(P2) == [-b[3:4] b[1:2]]
-#does not work with box_approximation in _canonical_length at the moment
-@test LazySets._canonical_length(P3) == [-Inf Inf; -Inf 30.0]
-@test LazySets._canonical_length(P4+-P4.center) ≈ radius(P4)*[-ones(5) ones(5)]
-
-@test LazySets.Sampler(P2).box_approx == [Uniform(-3.0,1.0), Uniform(-4.0,2.0)]
+@test LazySets.RejectionSampler(P2).box_approx == [Uniform(-3.0,1.0), Uniform(-4.0,2.0)]
