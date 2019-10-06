f610d1da3c145fd3a6f66d2905b432e995fffa61
schillic <schillic@informatik.uni-freiburg.de>
2019-10-06 18:39:52 +0200

    fix tests

--- test/unit_Polytope.jl
+++ test/unit_Polytope.jl
@@ -132,16 +132,13 @@ for N in [Float64, Rational{Int}, Float32]
     L2 = linear_map(M, P, algorithm="division") # uses transpose(M) \ c.a for each constraint c of P
     L3 = linear_map(M, P, algorithm="vrep") # uses V-representaion
     L4 = linear_map(M, P, cond_tol=1e3)  # set a custom tolerance for the condition number (invertibility check)
-    L5 = linear_map(M, P, check_invertibility=true)  # invertibility known
+    L5 = linear_map(M, P, check_invertibility=false)  # invertibility known
     L6 = linear_map(M, P, inverse=inv(M))  # pass inverse
     # needs M * an_element(Li) to be stable for rational, see #1105
     p = convert(Vector{N}, an_element(P))
     @test p ∈ P
     @test all([M * p ∈ Li for Li in [L1, L2, L3, L4, L5, L6]])
-
-    # do not check for invertibility => use the vertices
-    L4 = linear_map(M, P, check_invertibility=false)
-    @test L4 isa VPolygon{N}
+    @test L3 isa VPolygon{N}
 
     # linear map for mixed types
     M = [2 1; 0 1] # Int's
