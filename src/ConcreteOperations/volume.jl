b288a689542466f992916282acb5ba379286baa3
mforets <mforets@gmail.com>
2019-12-03 16:38:41 -0300

    add volume function for abstracthyperrectangle

---
+++ src/ConcreteOperations/volume.jl
@@ -0,0 +1,34 @@
+export volume
+
+"""
+    volume(H::AbstractHyperrectangle)
+
+Return the volume of a hyperrectangular set.
+
+### Input
+
+- `H` -- hyperrectangular set
+
+### Output
+
+The (Euclidean) volume of ``H``.
+
+### Algorithm
+
+The volume of the hyperrectangle ``H`` with vector radius ``r`` is
+``2ⁿ ∏ᵢ rᵢ`` where ``rᵢ`` denotes the ``i``-th component of ``r``.
+"""
+function volume(H::AbstractHyperrectangle{N}) where {N<:AbstractFloat}
+    r = radius_hyperrectangle(H)
+    n = dim(H)
+    α = exp(n*log(2))
+    vol = α * prod(r)
+    return  α * prod(r)
+end
+
+# fallback for rational
+function volume(X::AbstractHyperrectangle{N}) where {N}
+    r = radius_hyperrectangle(X)
+    vol = prod(2 .* r)
+    return vol
+end
