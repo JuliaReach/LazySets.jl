d20e7279a2f748313d42e30e0f147bca811b08ca
ueliwechsler <wueli@student.ethz.ch>
2019-12-10 14:28:14 +0100

    Update src/Interfaces/LazySet.jl
    Co-Authored-By: Christian Schilling <schillic@informatik.uni-freiburg.de>

--- src/Interfaces/LazySet.jl
+++ src/Interfaces/LazySet.jl
@@ -702,7 +702,7 @@ Return whether two LazySets are equal in the mathematical sense, i.e. equivalent
 
 First, the check `X == Y` is performed which returns `true` if and only if the given sets are of the same type,
 and have the same values (modulo floating-point tolerance). Otherwise, the double inclusion check `X ⊆ Y && Y ⊆ X` is
-computed.
+used.
 
 ### Examples
 
