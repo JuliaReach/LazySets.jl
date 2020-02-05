99625226f223ee555dcc7bc486d115e8b7951e22
Marcelo Forets <mforets@gmail.com>
2020-02-05 13:56:12 -0300

    #1936 - Concrete linear map using variable lifting (#1939)
    * add new extend matrix function
    
    * typos
    
    * add missing import
    
    * add missing import
    
    * add lift function
    
    * Update test/unit_util.jl
    
    * Update test/unit_util.jl
    
    * Update test/unit_util.jl
    
    * Update test/unit_util.jl
    
    * Update test/unit_util.jl
    
    * remove test that fails with Float32
    
    * add lift function
    
    * Add test
    
    * fix type stability
    
    * Update src/Interfaces/AbstractPolyhedron_functions.jl
    
    Co-Authored-By: Christian Schilling <schillic@informatik.uni-freiburg.de>
    
    * Update src/Interfaces/AbstractPolyhedron_functions.jl
    
    Co-Authored-By: Christian Schilling <schillic@informatik.uni-freiburg.de>
    
    * Update src/Interfaces/AbstractPolyhedron_functions.jl
    
    Co-Authored-By: Christian Schilling <schillic@informatik.uni-freiburg.de>
    
    * Update src/Interfaces/AbstractPolyhedron_functions.jl
    
    Co-Authored-By: Christian Schilling <schillic@informatik.uni-freiburg.de>
    
    Co-authored-by: Christian Schilling <schillic@informatik.uni-freiburg.de>

--- test/unit_Polytope.jl
+++ test/unit_Polytope.jl
@@ -125,6 +125,23 @@ for N in [Float64, Rational{Int}, Float32]
     LM = linear_map(M, P)
     @test LM isa HPolytope{N}
 
+    if N != Rational{Int}
+        M = N[1 1; 2 2; 3 4] # non-invertible matrix of rank 2
+        P = HPolytope([HalfSpace(N[1, 0], N(1)),
+                       HalfSpace(N[0, 1], N(1)),
+                       HalfSpace(N[-1, -0], N(1)),
+                       HalfSpace(N[-0, -1], N(1))])
+        L7 = linear_map(M, P, algorithm="lift")
+        @test L7 isa HPolytope{N}
+        if test_suite_polyhedra
+            L7_vrep = linear_map(M, P, algorithm="vrep")
+            if N == Float64
+                @test L7 ⊆ L7_vrep && L7_vrep ⊆ L7
+            end
+            # For Float32 we need to support mixed types,
+            # ρ(::Array{Float64,1}, ::HPolytope{Float32})
+        end
+    end
     # -----
     # V-rep
     # -----
