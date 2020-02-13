2595a5f0dddfc4d445995f3c47e7fb8263ca0a90
schillic <schillic@informatik.uni-freiburg.de>
2020-02-13 21:25:59 +0100

    concrete projection of a HalfSpace

--- src/Approximations/decompositions.jl
+++ src/Approximations/decompositions.jl
@@ -457,10 +457,24 @@ Concrete projection of a half-space.
 A set representing the projection of the half-space `H` on the dimensions
 specified by `block`.
 
-### Notes
+### Algorithm
 
-Currently only the case where the unconstrained dimensions of `H` are a subset
-of the `block` variables is implemented.
+If the unconstrained dimensions of `H` are a subset of the `block` variables,
+the projection is applied to the normal direction of `H`.
+Otherwise, the projection results in the universal set.
+
+The latter can be seen as follows.
+Without loss of generality consider a projection onto a single and constrained
+dimension ``xₖ`` (projections in multiple dimensions can be modeled as repeated
+one-dimensional projections).
+We can write the projection as an existentially quantified linear constraint:
+
+```math
+    ∃xₖ: a₁x₁ + … + aₖxₖ + … + aₙxₙ ≤ b
+```
+
+Since ``aₖ ≠ 0``, there is always a value for ``xₖ`` that satisfies the
+constraint for any valuation of the other variables.
 
 ### Examples
 
@@ -471,8 +485,6 @@ of variables to be `[1, 2, 3]`:
 ```jldoctest project_halfspace
 julia> H = HalfSpace([1.0, 1.0, 0.0], 1.0)
 HalfSpace{Float64,Array{Float64,1}}([1.0, 1.0, 0.0], 1.0)
-
-julia> using LazySets.Approximations: project
 
 julia> project(H, [1, 2, 3])
 HalfSpace{Float64,Array{Float64,1}}([1.0, 1.0, 0.0], 1.0)
@@ -492,13 +504,23 @@ the half-space projected on the dimensions where it is constrained only:
 julia> project(H, constrained_dimensions(H))
 HalfSpace{Float64,Array{Float64,1}}([1.0, 1.0], 1.0)
 ```
+
+If a constrained dimension is projected, we get the universal set of the
+dimension corresponding to the projection.
+
+```jldoctest project_halfspace
+julia> project(H, [1, 3])
+Universe{Float64}(2)
+
+julia> project(H, [1])
+Universe{Float64}(1)
+```
 """
 function project(H::HalfSpace{N}, block::AbstractVector{Int}) where {N}
     if constrained_dimensions(H) ⊆ block
         return HalfSpace(H.a[block], H.b)
     else
-        error("the concrete projection of a half-space " *
-              "for a general block structure is not implemented yet")
+        return Universe(length(block))
     end
 end
 
