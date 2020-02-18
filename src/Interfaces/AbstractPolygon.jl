b117d56085a43a7402b9b204acdd6bb34176267b
mforets <mforets@gmail.com>
2020-02-18 10:28:05 -0300

    remove trigonometric algo for 2d comparison

--- src/Interfaces/AbstractPolygon.jl
+++ src/Interfaces/AbstractPolygon.jl
@@ -148,33 +148,3 @@ function <=(u::AbstractVector{N}, v::AbstractVector{N}) where {N<:Real}
     # different quadrant
     return qu < qv
 end
-
-"""
-    <=(u::AbstractVector{N}, v::AbstractVector{N}) where {N<:AbstractFloat}
-
-Compares two 2D vectors by their direction.
-
-### Input
-
-- `u` --  first 2D direction
-- `v` --  second 2D direction
-
-### Output
-
-True iff ``\\arg(u) [2π] ≤ \\arg(v) [2π]``
-
-### Notes
-
-The argument is measured in counter-clockwise fashion, with the 0 being the
-direction (1, 0).
-
-### Algorithm
-
-The implementation uses the arctangent function with sign, `atan`, which for two
-arguments implements the
-[`atan2` function](https://en.wikipedia.org/wiki/Atan2).
-"""
-function <=(u::AbstractVector{N},
-            v::AbstractVector{N}) where {N<:AbstractFloat}
-    return jump2pi(atan(u[2], u[1])) <= jump2pi(atan(v[2], v[1]))
-end
