d43222a6c0ea8438f4433dfa707412267219e673
mforets <mforets@gmail.com>
2020-02-28 23:42:00 -0300

    add docstring

--- src/ConcreteOperations/minkowski_sum.jl
+++ src/ConcreteOperations/minkowski_sum.jl
@@ -248,6 +248,25 @@ function minkowski_sum(Z1::AbstractZonotope{N}, Z2::AbstractZonotope{N};
     return Zonotope(cnew, Gnew, remove_zero_generators=remove_zero_generators)
 end
 
+"""
+    minkowski_sum(x::Interval, y::Interval)
+
+Concrete Minkowski sum of a pair of intervals.
+
+### Input
+
+- `x` -- hyperrectangular set
+- `y` -- hyperrectangular set
+
+### Output
+
+An `Interval` corresponding to the concrete Minkowski sum of `x` and `y`.
+
+### Algorithm
+
+The function takes the sum of `x` and `y` following the rules of interval
+arithmetic.
+"""
 function minkowski_sum(x::Interval, y::Interval)
     return Interval(x.dat + y.dat)
 end
