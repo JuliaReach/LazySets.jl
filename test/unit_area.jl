54854e0518b0ac5da5f5a7793a202eb743df181d
Marcelo Forets <mforets@gmail.com>
2020-03-12 16:01:35 -0300

    Update test/unit_area.jl
    Co-Authored-By: Christian Schilling <schillic@informatik.uni-freiburg.de>

--- test/unit_area.jl
+++ test/unit_area.jl
@@ -1,25 +1,31 @@
-# not implemented for dimension != 2
-p = BallInf(zeros(3), 1.0)
-@test_throws ArgumentError surface(p)
-@test_throws AssertionError area(p)
+for N in [Float64, Float32, Rational{Int}]
+    # not implemented for dimension != 2
+    p = BallInf(zeros(N, 3), N(1))
+    @test_throws ArgumentError surface(p)
+    @test_throws AssertionError area(p)
 
-# sets with zero area
-p = Singleton([0.0, 1.0])
-@test area(p) ≈ 0.0
-p = Interval(0.0, 1.0) × Interval(0.0, 0.0)
-@test area(p) ≈ 0.0
+    # sets with zero area
+    p = Singleton([0, 1])
+    @test area(p) ≈ N(0)
+    p = Interval(N(0), N(1)) × Interval(N(0), N(0))
+    @test area(p) ≈ N(0)
 
-# triangle
-p = VPolygon([[13, 14.], [16, 30.], [50, 10.]])
-@test area(p) ≈ 302.0
+    # triangle
+    p = VPolygon([N[13, 14], N[16, 30], N[50, 10]])
+    @test area(p) ≈ N(302)
 
-# quadrilateral
-vlist = [[3, 4.], [5, 11.], [12, 8.], [9, 5.], [5, 6.]]
-p = VPolygon(vlist) # convex hull => 4 points
-@test area(p) ≈ 35.0
+    # quadrilateral
+    vlist = [N[3, 4], N[5, 11], N[12, 8], N[9, 5], N[5, 6]]
+    p = VPolygon(vlist) # convex hull => 4 points
+    @test area(p) ≈ N(35)
 
-# test surface function, which falls back to the area function for 2D sets
-@test surface(p) ≈ 35.0
+    # more than four vertices
+    p2 = VPolygon([N[1, 0], N[0, 0], N[0, 1], N[1/2, 2], [1, 1]])
+    @test area(p2) == N[3/2]
 
-# general case (non-convex)
-@test LazySets._area_polygon(vlist) ≈ 30.0
+    # non-convex polygon (via list of vertices)
+    @test LazySets._area_polygon(vlist) ≈ N(30)
+
+    # test surface function, which falls back to the area function for 2D sets
+    @test surface(p) ≈ N(35)
+end
