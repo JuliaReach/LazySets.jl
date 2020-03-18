1fe47eafb94e77d0c5faf90df7afbff63de46274
mforets <mforets@gmail.com>
2020-03-18 17:14:43 -0300

    fix initialization

--- src/Initialization/init_MiniQhull.jl
+++ src/Initialization/init_MiniQhull.jl
@@ -1,16 +1,9 @@
-eval(quote
-    using .MiniQhull
-)
-
-eval(load_delaunay_triangulation())
-
-
-function load_delaunay_triangulation()
-
-return quote
+using .MiniQhull
+import .MiniQhull: delaunay
+export delaunay
 
 """
-    MiniQhull.delaunay(X::LazySet)
+    delaunay(X::LazySet)
 
 Compute the Delaunay triangulation of the given convex set.
 
@@ -31,9 +24,8 @@ the library [Qhull](http://www.qhull.org/).
 The method works in arbitrary dimension and the requirement is that the list of
 vertices of `X` can be obtained.
 """
-function MiniQhull.delaunay(X::LazySet)
+function delaunay(X::LazySet)
     n = dim(X)
-
     v = vertices_list(X)
     m = length(v)
     coordinates = vcat(v...)
@@ -42,5 +34,3 @@ function MiniQhull.delaunay(X::LazySet)
     elements = [VPolytope(v[connectivity_matrix[:, i]]) for i in 1:nelements]
     return UnionSetArray(elements)
 end
-
-end end
