56504c39e7adb6371c6d4d5159f84d2bc80fbf9d
Marcelo Forets <mforets@gmail.com>
2020-03-18 20:02:58 -0300

    Add delaunay triangulation through MiniQhull / Qhull (#2075)
    * Add miniqhull optional dependency
    
    * extend delaunay triangulation function for lazysets
    
    * fix initialization
    
    * Update src/Initialization/init_MiniQhull.jl
    
    Co-Authored-By: Christian Schilling <schillic@informatik.uni-freiburg.de>
    
    Co-authored-by: Christian Schilling <schillic@informatik.uni-freiburg.de>

--- src/init.jl
+++ src/init.jl
@@ -3,6 +3,7 @@ function __init__()
     @require Distributions = "31c24e10-a181-5473-b8eb-7969acd0382f" include("Initialization/init_Distributions.jl")
     @require Expokit = "a1e7a1ef-7a5d-5822-a38c-be74e1bb89f4" include("Initialization/init_Expokit.jl")
     @require Makie = "ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" include("Initialization/init_Makie.jl")
+    @require MiniQhull = "978d7f02-9e05-4691-894f-ae31a51d76ca" include("Initialization/init_MiniQhull.jl")
     @require Optim = "429524aa-4258-5aef-a3af-852621145aeb" include("Initialization/init_Optim.jl")
     @require Polyhedra = "67491407-f73d-577b-9b50-8179a7c68029" include("Initialization/init_Polyhedra.jl")
 end
