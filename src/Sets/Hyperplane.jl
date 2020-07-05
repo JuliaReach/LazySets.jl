7f2e9db3fa6de290ad53eefa42b28d80c23f5734
Marcelo Forets <mforets@gmail.com>
2020-07-05 13:33:48 -0300

    Update src/Sets/Hyperplane.jl

--- src/Sets/Hyperplane.jl
+++ src/Sets/Hyperplane.jl
@@ -496,7 +496,7 @@ Return the hyperplane given by a symbolic expression.
 - `vars` -- (optional, default: `get_variables(expr)`), if an array of variables is given,
             use those as the ambient variables in the set with respect to which derivations
             take place; otherwise, use only the variables which appear in the given
-            expression (but be careful because the order may change; see `Notes` below fo details)
+            expression (but be careful because the order may change; in doubt, pass `vars` explicitly)
 - `N`    -- (optional, default: `Float64`) the numeric type of the returned hyperplane
 
 ### Output
