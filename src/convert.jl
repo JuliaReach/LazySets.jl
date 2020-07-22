56ba351faf14229ba12981da5ff9c7b3415a4d3e
SebastianGuadalupe <sebastianguadalupe00@gmail.com>
2020-07-22 14:32:34 -0300

    add order assertion in convertion

--- src/convert.jl
+++ src/convert.jl
@@ -990,7 +990,8 @@ end
 """
     convert(::Type{<:HParallelotope}, Z::AbstractZonotope)
 
-Converts a zonotopic set into a parallelotope in constraint representation.
+Converts a zonotopic set of order 1 into a parallelotope in constraint
+representation.
 
 ### Input
 
@@ -1002,6 +1003,8 @@ Converts a zonotopic set into a parallelotope in constraint representation.
 A parallelotope in constraint representation.
 """
 function convert(::Type{<:HParallelotope}, Z::AbstractZonotope)
+    @assert order(Z) == 1 "cannot convert a zonotope that is not of order 1 to"*
+                            "a parallelotope"
     n = Int(2order(Z)*dim(Z))
     D = reduce(hcat, [constraints_list(Z)[i].a for i in 1:2:n])'
     c = [constraints_list(Z)[i].b for i in 1:2:n-1]
