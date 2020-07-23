97cba49d1fded1578f5a7fe282a0ccd1d6ad1e56
schillic <christian.schilling@ist.ac.at>
2020-07-23 10:47:18 +0200

    SingletonIterator

--- src/Utils/iterators.jl
+++ src/Utils/iterators.jl
@@ -17,6 +17,31 @@ function Base.iterate(::EmptyIterator, state=nothing)
     return nothing
 end
 
+# ==============================
+# iterator over a single element
+# ==============================
+
+struct SingletonIterator{T}
+    element::T
+end
+
+function Base.length(::SingletonIterator)
+    return 1
+end
+
+function Base.eltype(::Type{SingletonIterator{T}}) where {T}
+    return T
+end
+
+function Base.iterate(it::SingletonIterator, state=0)
+    if state == 0
+        element = it.element
+        state = nothing
+        return (element, state)
+    end
+    return nothing
+end
+
 # ======================
 # iterator over a vector
 # ======================
