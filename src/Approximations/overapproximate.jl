edcad7f81bcb0b9ba805fc3112c78b9dddb85f6f
SebastianGuadalupe <sebastianguadalupe00@gmail.com>
2020-08-13 16:04:29 -0300

    undo Lazy projection

--- src/Approximations/overapproximate.jl
+++ src/Approximations/overapproximate.jl
@@ -1724,9 +1724,9 @@ function _overapproximate_zonotope_cpa(X::LazySet, dir::Type{<:AbstractDirection
     n = dim(X)
     # overapproximate 2D blocks
     if n > 1
-        πX_2D = [Projection(X, [i, i+1]) for i in 1:2:n]
-        aux = [overapproximate(poly, dir(2)) for poly in πX_2D]
-        Z_2D = [_overapproximate_zonotope_vrep(poly, dir(2)) for poly in aux]
+        nblocks = Int(floor(n/2))
+        πX_2D = [project(X, [i, i+1]) for i in 1:2:nblocks]
+        Z_2D = [_overapproximate_zonotope_vrep(poly, dir(2)) for poly in πX_2D]
     end
 
     if iseven(n)
