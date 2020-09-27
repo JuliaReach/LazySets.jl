d693e12f0e7247e926bcb5d213bc1612a0070bad
schillic <christian.schilling@ist.ac.at>
2020-09-27 18:30:03 +0200

    combine PackageSpecs; unspecify Plots version

--- deps/build.jl
+++ deps/build.jl
@@ -2,17 +2,16 @@ import Pkg
 
 # https://docs.travis-ci.com/user/languages/julia/#dependency-management
 @static if VERSION < v"1.4"
-    Pkg.add(Pkg.PackageSpec("Distributions", v"0.22"))
-    Pkg.add(Pkg.PackageSpec("Documenter", v"0.25"))
-    Pkg.add(Pkg.PackageSpec("GLPK", v"0.13"))
-    Pkg.add(Pkg.PackageSpec("GLPKMathProgInterface", v"0.5"))
-    Pkg.add(Pkg.PackageSpec("GR", v"0.44"))
-    Pkg.add(Pkg.PackageSpec("IntervalArithmetic", v"0.15"))
-    Pkg.add(Pkg.PackageSpec("IntervalMatrices", v"0.6"))
-    Pkg.add(Pkg.PackageSpec("ModelingToolkit", v"0.8.0"))
-    Pkg.add(Pkg.PackageSpec("Optim", v"0.21"))
-    Pkg.add(Pkg.PackageSpec("Plots", v"0.28"))
-    Pkg.add(Pkg.PackageSpec("RecipesBase", v"0.7"))
-    Pkg.add(Pkg.PackageSpec("Requires", v"0.5"))
-    Pkg.add(Pkg.PackageSpec("TaylorModels", v"0.1"))
+    Pkg.add([Pkg.PackageSpec("Distributions", v"0.22"),
+             Pkg.PackageSpec("Documenter", v"0.25"),
+             Pkg.PackageSpec("GLPK", v"0.13"),
+             Pkg.PackageSpec("GLPKMathProgInterface", v"0.5"),
+             Pkg.PackageSpec("GR", v"0.44"),
+             Pkg.PackageSpec("IntervalArithmetic", v"0.15"),
+             Pkg.PackageSpec("IntervalMatrices", v"0.6"),
+             Pkg.PackageSpec("ModelingToolkit", v"0.8.0"),
+             Pkg.PackageSpec("Optim", v"0.21"),
+             Pkg.PackageSpec("RecipesBase", v"0.7"),
+             Pkg.PackageSpec("Requires", v"0.5"),
+             Pkg.PackageSpec("TaylorModels", v"0.1")])
 end
