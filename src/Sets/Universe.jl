0f5b158a38cd369a0715a1cdf6ceec7e915d6894
schillic <christian.schilling@ist.ac.at>
2020-11-08 10:56:36 +0100

    define linear_map_inverse for Universe

--- src/Sets/Universe.jl
+++ src/Sets/Universe.jl
@@ -338,3 +338,10 @@ function translate(U::Universe{N}, v::AbstractVector{N}) where {N<:Real}
                                 "set by a $(length(v))-dimensional vector"
     return U
 end
+
+function linear_map_inverse(Minv::AbstractMatrix{N}, U::Universe{N}) where {N}
+    @assert size(Minv, 1) == dim(U) "a linear map of size $(size(Minv)) " *
+        "cannot be applied to a universe of dimension $(dim(U))"
+    n = size(Minv, 2)
+    return Universe{N}(n)
+end
