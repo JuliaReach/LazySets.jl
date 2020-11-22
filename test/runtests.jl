7ede2fba3ca864b38a961ca630f6456e7445baa4
schillic <christian.schilling@ist.ac.at>
2020-11-22 19:43:17 +0100

    add isoperationtype for HParallelotope

--- test/runtests.jl
+++ test/runtests.jl
@@ -81,6 +81,7 @@ if test_suite_basic
     # =======================================
     # Testing types that inherit from LazySet
     # =======================================
+    @time @testset "LazySets.LazySet" begin include("unit_LazySet.jl") end
     @time @testset "LazySets.Singleton" begin include("unit_Singleton.jl") end
     @time @testset "LazySets.Ball1" begin include("unit_Ball1.jl") end
     @time @testset "LazySets.Ball2" begin include("unit_Ball2.jl") end
