d3be4cf26f6ea53792fec04499a439ce52a5fa0c
schillic <christian.schilling@ist.ac.at>
2020-12-09 22:23:08 +0100

    remove 'N<:Real' for HParallelotope

--- src/Sets/HParallelotope.jl
+++ src/Sets/HParallelotope.jl
@@ -10,7 +10,7 @@ export HParallelotope,
        constraints_list
 
 """
-    HParallelotope{N<:Real, VN<:AbstractVector{N}, MN<:AbstractMatrix{N}} <: AbstractZonotope{N}
+    HParallelotope{N, VN<:AbstractVector{N}, MN<:AbstractMatrix{N}} <: AbstractZonotope{N}
 
 Type that represents a parallelotope in constraint form.
 
@@ -53,12 +53,12 @@ we refer to [3].
 [3] Matthias Althoff, Olaf Stursberg, and Martin Buss. *Computing reachable sets of hybrid systems using
     a combination of zonotopes and polytopes.* Nonlinear analysis: hybrid systems 4.2 (2010): 233-249.
 """
-struct HParallelotope{N<:Real, VN<:AbstractVector{N}, MN<:AbstractMatrix{N}} <: AbstractZonotope{N}
+struct HParallelotope{N, VN<:AbstractVector{N}, MN<:AbstractMatrix{N}} <: AbstractZonotope{N}
     directions::MN
     offset::VN
 
     # default constructor with dimension check
-    function HParallelotope(D::MN, c::VN) where {N<:Real, VN<:AbstractVector{N}, MN<:AbstractMatrix{N}}
+    function HParallelotope(D::MN, c::VN) where {N, VN<:AbstractVector{N}, MN<:AbstractMatrix{N}}
         @assert length(c) == 2*checksquare(D) "the length of the offset direction should be twice the size " *
             "of the directions matrix, but they are $(length(c)) and $(size(D)) dimensional respectively"
         return new{N, VN, MN}(D, c)
