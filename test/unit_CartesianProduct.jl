94e79b267cf69901361fab1062ed2e4aa9df6955
Sebastián Guadalupe <49855687+SebastianGuadalupe@users.noreply.github.com>
2021-01-28 16:07:16 -0300

    Update test/unit_CartesianProduct.jl
    Co-authored-by: Christian Schilling <christian.schilling@ist.ac.at>

--- test/unit_CartesianProduct.jl
+++ test/unit_CartesianProduct.jl
@@ -385,8 +385,8 @@ for N in [Float64, Float32]
 
     # projection
     cp = LazySets.Interval(N(0), N(2)) × Hyperrectangle(N[2, 3], N[1, 1])
-    @test _project(cp, 1:2) == Hyperrectangle(N[1, 2], N[1, 1])
-    @test _project(cp, 2:3) == Hyperrectangle(N[2, 3], N[1, 1])
+    @test project(cp, 1:2) == Hyperrectangle(N[1, 2], N[1, 1])
+    @test project(cp, 2:3) == Hyperrectangle(N[2, 3], N[1, 1])
 end
 
 for N in [Float64]
