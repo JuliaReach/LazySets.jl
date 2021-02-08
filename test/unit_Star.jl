d82da3858733e587396d6e3c961e3fa76e0a23d9
mforets <mforets@gmail.com>
2021-02-08 17:26:44 -0300

    update tests

--- test/unit_Star.jl
+++ test/unit_Star.jl
@@ -1,6 +1,15 @@
-for N in [Float64, Float32]
+for N in [Float64, Float32, Rational{Int}]
+
+    # constructor with basis matrix
     S = Star(N[3, 3], N[1 0; 0 1], BallInf(N[0, 0], N(1)))
-    @test Star(N[3, 3], [N[1, 0], N[0, 1]], BallInf(N[0, 0], N(1))) == S
+
+    # constructor vector-of-vectors basis
+    W = Star(N[3, 3], [N[1, 0], N[0, 1]], BallInf(N[0, 0], N(1)))
+
+    # S == W FIXME requires Star <: LazySet
+    @test S.c == W.c && S.V == W.V && S.P == W.P
+
+    # getter functions
     @test center(S) == N[3, 3]
     @test basis(S) == N[1 0; 0 1]
     @test predicate(S) == BallInf(N[0, 0], N(1))
