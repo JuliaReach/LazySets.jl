9f9f1b7881a8e592ae1a47873987833a0f2b5967
schillic <christian.schilling@ist.ac.at>
2021-02-26 17:47:16 +0100

    deactivate plotting tests in v1.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -177,15 +177,17 @@ if test_suite_basic
 end
 
 if test_suite_plotting
-    # define `plot` function as `RecipesBase.apply_recipe`
-    import RecipesBase
-    struct DummyBackend <: RecipesBase.AbstractBackend end
-    struct DummyPlot <: RecipesBase.AbstractPlot{DummyBackend} end
-    Base.length(::DummyPlot) = 0
-    dict = Dict{Symbol, Any}(:plot_object => DummyPlot())
-    plot(args...; kwargs...) = RecipesBase.apply_recipe(dict, args...; kwargs...)
+    @static if VERSION >= v"1.1"
+        # define `plot` function as `RecipesBase.apply_recipe`
+        import RecipesBase
+        struct DummyBackend <: RecipesBase.AbstractBackend end
+        struct DummyPlot <: RecipesBase.AbstractPlot{DummyBackend} end
+        Base.length(::DummyPlot) = 0
+        dict = Dict{Symbol, Any}(:plot_object => DummyPlot())
+        plot(args...; kwargs...) = RecipesBase.apply_recipe(dict, args...; kwargs...)
 
-    @time @testset "LazySets.plotting" begin include("unit_plot.jl") end
+        @time @testset "LazySets.plotting" begin include("unit_plot.jl") end
+    end
 end
 
 if test_suite_doctests
