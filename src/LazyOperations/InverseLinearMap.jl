3d6f0bed523043f0deb9c873a2df1c424805af11
mforets <mforets@gmail.com>
2021-03-07 09:52:41 -0300

    remove convx

--- src/LazyOperations/InverseLinearMap.jl
+++ src/LazyOperations/InverseLinearMap.jl
@@ -150,7 +150,7 @@ If the direction has norm zero, the result depends on the wrapped set.
 
 ### Notes
 
-If ``L = M^{-1}⋅X``, where ``M`` is a matrix and ``X`` is a convex set, since
+If ``L = M^{-1}⋅X``, where ``M`` is a matrix and ``X`` is a set, since
 (M^T)^{-1}=(M^{-1})^T, it follows that ``σ(d, L) = M^{-1}⋅σ((M^T)^{-1} d, X)``
 for any direction ``d``.
 """
@@ -192,7 +192,7 @@ Check whether a given point is contained in the inverse linear map of a set.
 ### Input
 
 - `x`   -- point/vector
-- `ilm` -- inverse linear map of a convex set
+- `ilm` -- inverse linear map of a set
 
 ### Output
 
