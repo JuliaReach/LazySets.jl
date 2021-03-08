89b8f192067ca6aafc585f8321b17583edf47933
Marcelo Forets <mforets@gmail.com>
2021-03-08 07:30:06 -0300

    #1958 - Refactor tests folder  (#2617)
    * refactor tests
    
    * Update test/runtests.jl
    
    Co-authored-by: Christian Schilling <christian.schilling@ist.ac.at>
    
    Co-authored-by: Christian Schilling <christian.schilling@ist.ac.at>

--- test/runtests.jl
+++ test/runtests.jl
@@ -73,90 +73,90 @@ if test_suite_basic
     # =========================
     # Testing utility functions
     # =========================
-    @time @testset "LazySets.Util" begin include("unit_util.jl") end
-    @time @testset "LazySets.Comparisons" begin include("unit_comparisons.jl") end
-    @time @testset "LazySets.Interior" begin include("unit_interior.jl") end
+    @time @testset "LazySets.Util" begin include("Utils/util.jl") end
+    @time @testset "LazySets.Comparisons" begin include("Utils/comparisons.jl") end
+    @time @testset "LazySets.Interior" begin include("Utils/interior.jl") end
 
     # =======================================
     # Testing interfaces to external packages
     # =======================================
-    @time @testset "LazySets.CDDLib" begin include("unit_CDDLib.jl") end
+    @time @testset "LazySets.CDDLib" begin include("Interfaces/CDDLib.jl") end
 
     # =======================================
     # Testing types that inherit from LazySet
     # =======================================
-    @time @testset "LazySets.LazySet" begin include("unit_LazySet.jl") end
-    @time @testset "LazySets.Singleton" begin include("unit_Singleton.jl") end
-    @time @testset "LazySets.Ball1" begin include("unit_Ball1.jl") end
-    @time @testset "LazySets.Ball2" begin include("unit_Ball2.jl") end
-    @time @testset "LazySets.Ballp" begin include("unit_Ballp.jl") end
-    @time @testset "LazySets.BallInf" begin include("unit_BallInf.jl") end
-    @time @testset "LazySets.Hyperrectangle" begin include("unit_Hyperrectangle.jl") end
-    @time @testset "LazySets.Polygon" begin include("unit_Polygon.jl") end
-    @time @testset "LazySets.Polytope" begin include("unit_Polytope.jl") end
-    @time @testset "LazySets.Polyhedron" begin include("unit_Polyhedron.jl") end
-    @time @testset "LazySets.Zonotope" begin include("unit_Zonotope.jl") end
-    @time @testset "LazySets.ZeroSet" begin include("unit_ZeroSet.jl") end
-    @time @testset "LazySets.EmptySet" begin include("unit_EmptySet.jl") end
-    @time @testset "LazySets.Ellipsoid" begin include("unit_Ellipsoid.jl") end
-    @time @testset "LazySets.Hyperplane" begin include("unit_Hyperplane.jl") end
-    @time @testset "LazySets.HalfSpace" begin include("unit_HalfSpace.jl") end
-    @time @testset "LazySets.Interval" begin include("unit_Interval.jl") end
-    @time @testset "LazySets.LineSegment" begin include("unit_LineSegment.jl") end
-    @time @testset "LazySets.Line2D" begin include("unit_Line2D.jl") end
-    @time @testset "LazySets.Line" begin include("unit_Line.jl") end
-    @time @testset "LazySets.Universe" begin include("unit_Universe.jl") end
-    @time @testset "LazySets.HParallelotope" begin include("unit_HParallelotope.jl") end
+    @time @testset "LazySets.LazySet" begin include("Interfaces/LazySet.jl") end
+    @time @testset "LazySets.Singleton" begin include("Sets/Singleton.jl") end
+    @time @testset "LazySets.Ball1" begin include("Sets/Ball1.jl") end
+    @time @testset "LazySets.Ball2" begin include("Sets/Ball2.jl") end
+    @time @testset "LazySets.Ballp" begin include("Sets/Ballp.jl") end
+    @time @testset "LazySets.BallInf" begin include("Sets/BallInf.jl") end
+    @time @testset "LazySets.Hyperrectangle" begin include("Sets/Hyperrectangle.jl") end
+    @time @testset "LazySets.Polygon" begin include("Sets/Polygon.jl") end
+    @time @testset "LazySets.Polytope" begin include("Sets/Polytope.jl") end
+    @time @testset "LazySets.Polyhedron" begin include("Sets/Polyhedron.jl") end
+    @time @testset "LazySets.Zonotope" begin include("Sets/Zonotope.jl") end
+    @time @testset "LazySets.ZeroSet" begin include("Sets/ZeroSet.jl") end
+    @time @testset "LazySets.EmptySet" begin include("Sets/EmptySet.jl") end
+    @time @testset "LazySets.Ellipsoid" begin include("Sets/Ellipsoid.jl") end
+    @time @testset "LazySets.Hyperplane" begin include("Sets/Hyperplane.jl") end
+    @time @testset "LazySets.HalfSpace" begin include("Sets/HalfSpace.jl") end
+    @time @testset "LazySets.Interval" begin include("Sets/Interval.jl") end
+    @time @testset "LazySets.LineSegment" begin include("Sets/LineSegment.jl") end
+    @time @testset "LazySets.Line2D" begin include("Sets/Line2D.jl") end
+    @time @testset "LazySets.Line" begin include("Sets/Line.jl") end
+    @time @testset "LazySets.Universe" begin include("Sets/Universe.jl") end
+    @time @testset "LazySets.HParallelotope" begin include("Sets/HParallelotope.jl") end
 
     # =========================================
     # Testing types representing set operations
     # =========================================
-    @time @testset "LazySets.Bloating" begin include("unit_Bloating.jl") end
-    @time @testset "LazySets.Intersection" begin include("unit_Intersection.jl") end
-    @time @testset "LazySets.ConvexHull" begin include("unit_ConvexHull.jl") end
-    @time @testset "LazySets.ExponentialMap" begin include("unit_ExponentialMap.jl") end
-    @time @testset "LazySets.LinearMap" begin include("unit_LinearMap.jl") end
-    @time @testset "LazySets.InverseLinearMap" begin include("unit_InverseLinearMap.jl") end
-    @time @testset "LazySets.MinkowskiSum" begin include("unit_MinkowskiSum.jl") end
-    @time @testset "LazySets.CartesianProduct" begin include("unit_CartesianProduct.jl") end
-    @time @testset "LazySets.ResetMap" begin include("unit_ResetMap.jl") end
-    @time @testset "LazySets.SymmetricIntervalHull" begin include("unit_SymmetricIntervalHull.jl") end
-    @time @testset "LazySets.concrete_convex_hull" begin include("unit_convex_hull.jl") end
-    @time @testset "LazySets.Translation" begin include("unit_Translation.jl") end
-    @time @testset "LazySets.AffineMap" begin include("unit_AffineMap.jl") end
+    @time @testset "LazySets.Bloating" begin include("LazyOperations/Bloating.jl") end
+    @time @testset "LazySets.Intersection" begin include("LazyOperations/Intersection.jl") end
+    @time @testset "LazySets.ConvexHull" begin include("LazyOperations/ConvexHull.jl") end
+    @time @testset "LazySets.ExponentialMap" begin include("LazyOperations/ExponentialMap.jl") end
+    @time @testset "LazySets.LinearMap" begin include("LazyOperations/LinearMap.jl") end
+    @time @testset "LazySets.InverseLinearMap" begin include("LazyOperations/InverseLinearMap.jl") end
+    @time @testset "LazySets.MinkowskiSum" begin include("LazyOperations/MinkowskiSum.jl") end
+    @time @testset "LazySets.CartesianProduct" begin include("LazyOperations/CartesianProduct.jl") end
+    @time @testset "LazySets.ResetMap" begin include("LazyOperations/ResetMap.jl") end
+    @time @testset "LazySets.SymmetricIntervalHull" begin include("LazyOperations/SymmetricIntervalHull.jl") end
+    @time @testset "LazySets.concrete_convex_hull" begin include("LazyOperations/convex_hull.jl") end
+    @time @testset "LazySets.Translation" begin include("LazyOperations/Translation.jl") end
+    @time @testset "LazySets.AffineMap" begin include("LazyOperations/AffineMap.jl") end
 
     # ======================
     # Testing set interfaces
     # ======================
-    @time @testset "LazySets.CompactSet" begin include("unit_CompactSet.jl") end
+    @time @testset "LazySets.CompactSet" begin include("Interfaces/CompactSet.jl") end
 
     # =========================================================
     # Testing other set types that do not inherit from LazySet
     # =========================================================
-    @time @testset "LazySets.Complement" begin include("unit_Complement.jl") end
-    @time @testset "LazySets.PolynomialZonotope" begin include("unit_PolynomialZonotope.jl") end
-    @time @testset "LazySets.Rectification" begin include("unit_Rectification.jl") end
-    @time @testset "LazySets.Star" begin include("unit_Star.jl") end
-    @time @testset "LazySets.UnionSet" begin include("unit_UnionSet.jl") end
+    @time @testset "LazySets.Complement" begin include("LazyOperations/Complement.jl") end
+    @time @testset "LazySets.PolynomialZonotope" begin include("Sets/PolynomialZonotope.jl") end
+    @time @testset "LazySets.Rectification" begin include("LazyOperations/Rectification.jl") end
+    @time @testset "LazySets.Star" begin include("Sets/Star.jl") end
+    @time @testset "LazySets.UnionSet" begin include("LazyOperations/UnionSet.jl") end
 
     # ===================
     # Concrete operations
     # ===================
-    @time @testset "LazySets.samples" begin include("unit_samples.jl") end
-    @time @testset "LazySets.area" begin include("unit_area.jl") end
+    @time @testset "LazySets.samples" begin include("ConcreteOperations/samples.jl") end
+    @time @testset "LazySets.area" begin include("ConcreteOperations/area.jl") end
 
     # =================================================================
     # Algorithms for approximation of convex sets using support vectors
     # =================================================================
-    @time @testset "LazySets.Approximations.overapproximation" begin include("unit_overapproximate.jl") end
-    @time @testset "LazySets.Approximations.underapproximation" begin include("unit_underapproximate.jl") end
-    @time @testset "LazySets.Approximations.template_directions" begin include("unit_template_directions.jl") end
-    @time @testset "LazySets.Approximations.box_approximation" begin include("unit_box_approximation.jl") end
-    @time @testset "LazySets.Approximations.ballinf_approximation" begin include("unit_ballinf_approximation.jl") end
-    @time @testset "LazySets.Approximations.radiusdiameter" begin include("unit_radiusdiameter.jl") end
-    @time @testset "LazySets.Approximations.decompose" begin include("unit_decompose.jl") end
-    @time @testset "LazySets.Approximations.distance" begin include("unit_distance.jl") end
-    @time @testset "LazySets.Approximations.hausdorff_distance" begin include("unit_hausdorff_distance.jl") end
+    @time @testset "LazySets.Approximations.overapproximation" begin include("Approximations/overapproximate.jl") end
+    @time @testset "LazySets.Approximations.underapproximation" begin include("Approximations/underapproximate.jl") end
+    @time @testset "LazySets.Approximations.template_directions" begin include("Approximations/template_directions.jl") end
+    @time @testset "LazySets.Approximations.box_approximation" begin include("Approximations/box_approximation.jl") end
+    @time @testset "LazySets.Approximations.ballinf_approximation" begin include("Approximations/ballinf_approximation.jl") end
+    @time @testset "LazySets.Approximations.radiusdiameter" begin include("Approximations/radiusdiameter.jl") end
+    @time @testset "LazySets.Approximations.decompose" begin include("Approximations/decompose.jl") end
+    @time @testset "LazySets.Approximations.distance" begin include("Approximations/distance.jl") end
+    @time @testset "LazySets.Approximations.hausdorff_distance" begin include("Approximations/hausdorff_distance.jl") end
 
     # ========================
     # Testing method ambiguity
@@ -173,8 +173,8 @@ if test_suite_basic
     # (must be the last test because it
     #  loads Polyhedra.jl)
     # ====================================
-    include("check_method_implementation.jl")
-    @time @testset "LazySets.interfaces" begin include("unit_interfaces.jl") end
+    include("Utils/check_method_implementation.jl")
+    @time @testset "LazySets.interfaces" begin include("Interfaces/interfaces.jl") end
 end
 
 if test_suite_plotting
@@ -187,7 +187,7 @@ if test_suite_plotting
         dict = Dict{Symbol, Any}(:plot_object => DummyPlot())
         plot(args...; kwargs...) = RecipesBase.apply_recipe(dict, args...; kwargs...)
 
-        @time @testset "LazySets.plotting" begin include("unit_plot.jl") end
+        @time @testset "LazySets.plotting" begin include("Utils/plot.jl") end
     end
 end
 
