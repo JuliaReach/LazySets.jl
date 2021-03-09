f1aca4e67d9272a28354e69b4d910b36bed43e6c
schillic <christian.schilling@ist.ac.at>
2021-03-09 11:42:13 +0100

    add deps script for v1.3 build (again)

---
+++ deps/build_private.jl
@@ -0,0 +1,10 @@
+# NOTE: This script is only supposed to be executed by our own build script.
+
+import Pkg
+
+# https://docs.travis-ci.com/user/languages/julia/#dependency-management
+@static if VERSION < v"1.4"
+    Pkg.add([Pkg.PackageSpec(name="Documenter", version=v"0.25"),
+             Pkg.PackageSpec(name="ModelingToolkit", version=v"3.20.1"),
+             Pkg.PackageSpec(name="TaylorModels", version=v"0.3.6")])
+end
