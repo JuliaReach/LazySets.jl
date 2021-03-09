b2f074954005d28f57306e5a77907b899cb23176
schillic <christian.schilling@ist.ac.at>
2021-03-09 16:12:42 +0100

    use different package versions in v1.3 build

--- deps/build_private.jl
+++ deps/build_private.jl
@@ -4,6 +4,7 @@ import Pkg
 
 # https://docs.travis-ci.com/user/languages/julia/#dependency-management
 @static if VERSION < v"1.4"
-    Pkg.add([Pkg.PackageSpec(name="Documenter", version=v"0.25"),
-             Pkg.PackageSpec(name="TaylorModels", version=v"0.3.6")])
+    Pkg.add([Pkg.PackageSpec(name="Documenter", version=v"0.26"),
+             Pkg.PackageSpec(name="Symbolics", version=v"0.1.2"),
+             Pkg.PackageSpec(name="TaylorModels", version=v"0.0.1")])
 end
