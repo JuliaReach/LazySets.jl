543732c8933e6a5960d599692327928e5fc52db7
schillic <christian.schilling@ist.ac.at>
2021-03-09 15:53:57 +0100

    remove ModelingToolkit from deps script

--- deps/build_private.jl
+++ deps/build_private.jl
@@ -5,6 +5,5 @@ import Pkg
 # https://docs.travis-ci.com/user/languages/julia/#dependency-management
 @static if VERSION < v"1.4"
     Pkg.add([Pkg.PackageSpec(name="Documenter", version=v"0.25"),
-             Pkg.PackageSpec(name="ModelingToolkit", version=v"3.20.1"),
              Pkg.PackageSpec(name="TaylorModels", version=v"0.3.6")])
 end
