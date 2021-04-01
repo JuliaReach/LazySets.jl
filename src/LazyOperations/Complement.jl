bf776b067a79299f364fbc4a42e47415f0690847
Marcelo Forets <mforets@gmail.com>
2021-04-01 14:50:42 -0300

    add docs for Complement

--- src/LazyOperations/Complement.jl
+++ src/LazyOperations/Complement.jl
@@ -7,18 +7,17 @@ export Complement,
 """
     Complement{N, S<:LazySet{N}}
 
-Type that represents the complement of a convex set.
+Type that represents the complement of a set.
 
 ### Fields
 
-- `X` -- convex set
+- `X` -- set
 
 ### Notes
 
 Since `X` is assumed to be closed, unless `X` is empty or the universe, its
 complement is open (i.e., not closed).
-Since `X` is assumed to be closed, unless `X` is empty, the universe, or a
-half-space, its complement is not convex.
+If `X` is empty, the universe, or a half-space, its complement is convex.
 
 The complement of the complement is the original set again.
 
@@ -54,15 +53,15 @@ Complement(C::Complement) = C.X
 """
     dim(C::Complement)
 
-Return the dimension of the complement of a convex set.
+Return the dimension of the complement of a set.
 
 ### Input
 
-- `C` -- complement of a convex set
+- `C` -- complement of a set
 
 ### Output
 
-The dimension of the complement of a convex set.
+The dimension of the complement of a set.
 """
 function dim(C::Complement)
     return dim(C.X)
@@ -71,12 +70,12 @@ end
 """
     ∈(x::AbstractVector, C::Complement)
 
-Check whether a given point is contained in the complement of a convex set.
+Check whether a given point is contained in the complement of a set.
 
 ### Input
 
 - `x` -- point/vector
-- `C` -- complement of a convex set
+- `C` -- complement of a set
 
 ### Output
 
@@ -96,11 +95,11 @@ end
 """
     isempty(C::Complement)
 
-Return if the complement of a convex set is empty or not.
+Return if the complement of a set is empty or not.
 
 ### Input
 
-- `C` -- complement of a convex set
+- `C` -- complement of a set
 
 ### Output
 
