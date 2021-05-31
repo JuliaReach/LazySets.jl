dbdceab856e70c1cc446f2c562d6bbcf5bd86714
schillic <git@christianschilling.net>
2021-05-31 22:34:44 +0200

    simplify methods

--- src/convert.jl
+++ src/convert.jl
@@ -353,14 +353,8 @@ return quote
     end
 end end  # quote / load_genmat_2D_static
 
-function convert(::Type{Zonotope}, S::Singleton{N, VN}) where {N, VN<:AbstractVector{N}}
-    MT = LazySets.Arrays._matrix_type(VN)
-    zero_genmat = MT(undef, dim(S), 0)
-    return Zonotope(element(S), zero_genmat)
-end
-
-function convert(::Type{Zonotope}, S::Singleton{N, VN}) where {N, VN<:SparseVector{N}}
-    return Zonotope(element(S), spzeros(N, dim(S), 0))
+function convert(::Type{Zonotope}, S::Singleton)
+    return Zonotope(center(S), genmat(S))
 end
 
 """
