a60f9a4674877c7b6752b27f4fe45a7374621ccb
mforets <mforets@gmail.com>
2021-06-15 14:38:23 -0300

    update deps

--- deps/build_private.jl
+++ deps/build_private.jl
@@ -1,10 +1,14 @@
 # NOTE: This script is only supposed to be executed by our own build script.
+# see: https://docs.travis-ci.com/user/languages/julia/#dependency-management
 
 import Pkg
 
-# https://docs.travis-ci.com/user/languages/julia/#dependency-management
 @static if VERSION < v"1.4"
     Pkg.add([Pkg.PackageSpec(name="Documenter", version=v"0.26"),
              Pkg.PackageSpec(name="Symbolics", version=v"0.1.2"),
              Pkg.PackageSpec(name="TaylorModels", version=v"0.0.1")])
+end
+
+@static if VERSION > v"1.4"
+    Pkg.add([Pkg.PackageSpec(name="Symbolics", version=v"0.1.32")])
 end
