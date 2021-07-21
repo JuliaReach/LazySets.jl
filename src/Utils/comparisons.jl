c402029539de8888b60b068e12514961ba8af353
schillic <git@christianschilling.net>
2021-07-21 10:39:40 +0200

    bring back factor 10 to ztol for AbstractFloat

--- src/Utils/comparisons.jl
+++ src/Utils/comparisons.jl
@@ -54,7 +54,7 @@ end
 default_tolerance(N::Type{<:Number}) = error("default tolerance for numeric type $N is not defined")
 default_tolerance(N::Type{<:Rational}) = Tolerance(zero(N), zero(N), zero(N))
 default_tolerance(N::Type{<:Integer}) = Tolerance(zero(N), zero(N), zero(N))
-default_tolerance(N::Type{<:AbstractFloat}) = Tolerance(Base.rtoldefault(N), sqrt(eps(N)), zero(N))
+default_tolerance(N::Type{<:AbstractFloat}) = Tolerance(Base.rtoldefault(N), N(10) * sqrt(eps(N)), zero(N))
 
 function set_tolerance(N, tolerance::Tolerance=default_tolerance(N))
     set_rtol(N, tolerance.rtol)
