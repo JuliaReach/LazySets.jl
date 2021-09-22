24e12c49bea2100ff1847cce969500adf40a7048
mforets <mforets@gmail.com>
2021-09-22 10:43:04 -0300

    in-place translation for Ballp

--- src/Sets/Ballp.jl
+++ src/Sets/Ballp.jl
@@ -256,11 +256,39 @@ A translated ball in the p- norm.
 ### Algorithm
 
 We add the vector to the center of the ball.
+
+### Notes
+
+See also [`translate!(::Ballp, AbstractVector)`](@ref) for the in-place version.
 """
 function translate(B::Ballp, v::AbstractVector)
+    return translate(copy(B), v)
+end
+
+"""
+    translate!(B::Ballp, v::AbstractVector)
+
+Translate (i.e., shift) a ball in the p-norm by a given vector, in-place.
+
+### Input
+
+- `B` -- ball in the p-norm
+- `v` -- translation vector
+
+### Output
+
+The ball `B` translated by `v`.
+
+### Notes
+
+See also [`translate(::Ballp, AbstractVector)`](@ref) for the out-of-place version.
+"""
+function translate!(B::Ballp, v::AbstractVector)
     @assert length(v) == dim(B) "cannot translate a $(dim(B))-dimensional " *
                                 "set by a $(length(v))-dimensional vector"
-    return Ballp(B.p, center(B) + v, B.radius)
+    c = center(B)
+    c .+= v
+    return B
 end
 
 function project(B::Ballp, block::AbstractVector{Int}; kwargs...)
