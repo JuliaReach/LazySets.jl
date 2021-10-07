be3e8b92f19e17afb22c4b4bf56290a98e2d1f38
Marcelo Forets <mforets@gmail.com>
2021-10-07 15:14:41 -0400

    Update isdisjoint.jl

--- test/ConcreteOperations/isdisjoint.jl
+++ test/ConcreteOperations/isdisjoint.jl
@@ -1,12 +1,12 @@
 for N in [Float64, Float32, Rational{Int}]
 
     # using IA types
-    X = interval(N(0), N(1)) # IA
-    Y = Interval(N(-1), N(2))
+    X = IA.interval(N(0), N(1)) # IA
+    Y = IA.Interval(N(-1), N(2))
     @test !isdisjoint(X, Y)
     @test !isdisjoint(Y, X)
 
-    X = IntervalBox(N(0) .. N(1), N(0) .. N(1))
+    X = IntervalBox(IA.interval(N(0), N(1)), IA.interval(N(0), N(1)))
     Y = Hyperrectangle(low=[N(-1), N(-1)], high=[N(2), N(2)])
     @test !isdisjoint(X, Y)
     @test !isdisjoint(Y, X)
