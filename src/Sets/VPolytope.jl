d9cd2a27f1ec8acbbba9a580d4e9f2acc2814a9a
Marcelo Forets <mforets@gmail.com>
2022-01-28 15:23:04 -0300

    swap args

--- src/Sets/VPolytope.jl
+++ src/Sets/VPolytope.jl
@@ -216,7 +216,7 @@ function ∈(x::AbstractVector{N}, P::VPolytope{N};
     end
     b = [x; one(N)]
     obj = zeros(N, m)
-    lp = linprog(obj, A, '=', b, Inf, zero(N), solver)
+    lp = linprog(obj, A, '=', b, zero(N), Inf, solver)
     if lp.status == OPTIMAL
         return true
     elseif lp.status == INFEASIBLE
