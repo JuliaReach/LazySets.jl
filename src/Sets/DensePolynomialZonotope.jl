00236c084ca063c40037ee90c2059d8d0438e7c8
Marcelo Forets <mforets@gmail.com>
2022-05-28 12:01:08 -0300

    Update src/Sets/DensePolynomialZonotope.jl
    Co-authored-by: Christian Schilling <git@christianschilling.net>

--- src/Sets/DensePolynomialZonotope.jl
+++ src/Sets/DensePolynomialZonotope.jl
@@ -96,7 +96,7 @@ Alias for `DensePolynomialZonotope`.
 
 ### Notes
 
-To review after LazySets#1543, which should use [1]. 
+To be reviewed after LazySets#1543 (should use [1]).
 
 [1] Kochdumper, N, and M. Althoff. *Sparse polynomial zonotopes: A novel set representation for reachability analysis.*
     IEEE Transactions on Automatic Control 66.9 (2020): 4043-4058.
