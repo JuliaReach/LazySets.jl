4cd704ef367a5ca49641b3942816121791ecb4c8
Luca Ferranti <lucaferranti@users.noreply.github.com>
2022-06-01 16:12:14 +0300

    incorporate review suggestions 1. make SimpleSparsePolynomialZonotope a subtype of AbstractSparsePolynomialZonotope 2. use assert-macro 3. check domain for overestimation is subset of [-1, 1]^q 4. change small --> capital letter for single letter set

--- src/Sets/SimpleSparsePolynomialZonotope.jl
+++ src/Sets/SimpleSparsePolynomialZonotope.jl
@@ -18,15 +18,15 @@ a constant offset ``c ∈ \\mathbb{R}^n``, a generator matrix ``G ∈ \\mathbb{R
 Sparse polynomial zonotopes were introduced in N. Kochdumper and M. Althoff.
 *Sparse Polynomial Zonotopes: A Novel Set Representation for Reachability Analysis*. Transactions on Automatic Control, 2021.
 """
-struct SimpleSparsePolynomialZonotope{N, VN<:AbstractVector{N}, MN<:AbstractMatrix{N}, ME<:AbstractMatrix{<:Integer}}
+struct SimpleSparsePolynomialZonotope{N, VN<:AbstractVector{N}, MN<:AbstractMatrix{N}, ME<:AbstractMatrix{<:Integer}} <: AbstractPolynomialZonotope{N}
     c::VN
     G::MN
     E::ME
 
     function SimpleSparsePolynomialZonotope(c::VN, G::MN, E::ME) where {N<:Real, VN<:AbstractVector{N}, MN<:AbstractMatrix{N}, ME<:AbstractMatrix{<:Integer}}
-        length(c) == size(G, 1) || throw(DimensionMismatch("c and G should have the same number of rows"))
-        size(G, 2) == size(E, 2) || throw(DimensionMismatch("G and E should have the same number of columns"))
-        all(>=(0), E) || throw(ArgumentError("E should have non-negative integers"))
+        @assert length(c) == size(G, 1) throw(DimensionMismatch("c and G should have the same number of rows"))
+        @assert size(G, 2) == size(E, 2) throw(DimensionMismatch("G and E should have the same number of columns"))
+        @assert all(>=(0), E) throw(ArgumentError("E should have non-negative integers"))
 
         return new{N, VN, MN, ME}(c, G, E)
     end
@@ -34,36 +34,35 @@ end
 
 const SSPZ = SimpleSparsePolynomialZonotope
 
-dim(p::SSPZ) = size(p.c, 1)
-ngens(p::SSPZ) = size(p.G, 2)
-nparams(p::SSPZ) = size(p.E, 1)
-order(p::SSPZ) = ngens(p) // dim(p)
+dim(P::SSPZ) = size(P.c, 1)
+ngens(P::SSPZ) = size(P.G, 2)
+nparams(P::SSPZ) = size(P.E, 1)
+order(P::SSPZ) = ngens(P) // dim(P)
 
-center(p::SSPZ) = p.c
-genmat(p::SSPZ) = p.G
-expmat(p::SSPZ) = p.E
-
+center(P::SSPZ) = P.c
+genmat(P::SSPZ) = P.G
+expmat(P::SSPZ) = P.E
 """
-    overapproximate(p::SSPZ, ::Type{Zonotope}; nsdiv=1)
+    overapproximate(P::SSPZ, ::Type{Zonotope}; nsdiv=1)
 
-Returns a zonotope containing ``p``.
+Returns a zonotope containing ``P``.
 
 ### Input
 
-- `p`     -- simple sparse polynomial zonotope
+- `P`     -- simple sparse polynomial zonotope
 - `nsdiv` -- (optional, default: `1`) size of uniform partitioning grid
 
 ### Output
 
-A zonotope containing `p`.
+A zonotope containing `P`.
 
 """
-function overapproximate(p::SSPZ, ::Type{Zonotope}; nsdiv=1)
-    (nsdiv != 1) && return overapproximate(p, UnionSetArray{Zonotope}; nsdiv=nsdiv)
+function overapproximate(P::SSPZ, ::Type{Zonotope}; nsdiv=1)
+    (nsdiv != 1) && return overapproximate(P, UnionSetArray{Zonotope}; nsdiv=nsdiv)
 
-    G = genmat(p)
-    E = expmat(p)
-    cnew = copy(center(p))
+    G = genmat(P)
+    E = expmat(P)
+    cnew = copy(center(P))
     Gnew = copy(G)
     @inbounds for (j, g) in enumerate(eachcol(G))
         if all(iseven, E[:, j])
@@ -75,15 +74,16 @@ function overapproximate(p::SSPZ, ::Type{Zonotope}; nsdiv=1)
 end
 
 """
-    overapproximate(p::SSPZ, ::Type{Zonotope}, dom::IntervalBox)
+    overapproximate(P::SSPZ, ::Type{Zonotope}, dom::IntervalBox)
 
 Compute the zonotope overapproximation of the given sparse polynomial zonotope over
-the parameter domain `dom`, which should be a subset of `[-1, 1]^q`, where `q = nparams(p)`.
+the parameter domain `dom`, which should be a subset of `[-1, 1]^q`, where `q = nparams(P)`.
 """
-function overapproximate(p::SSPZ, ::Type{Zonotope}, dom::IntervalBox)
-    G = genmat(p)
-    E = expmat(p)
-    cnew = copy(center(p))
+function overapproximate(P::SSPZ, ::Type{Zonotope}, dom::IntervalBox)
+    @assert dom ⊆ IntervalBox(-1..1, nparams(P)) "dom should be a subset of [-1, 1]^q"
+    G = genmat(P)
+    E = expmat(P)
+    cnew = copy(center(P))
     Gnew = similar(G)
     @inbounds for (j, g) in enumerate(eachcol(G))
         α = IA.Interval(1, 1)
@@ -98,9 +98,9 @@ function overapproximate(p::SSPZ, ::Type{Zonotope}, dom::IntervalBox)
     return Zonotope(cnew, Gnew)
 end
 
-function overapproximate(p::SSPZ, ::Type{UnionSetArray{Zonotope}}; nsdiv=100)
-    q = size(p.E, 1)
+function overapproximate(P::SSPZ, ::Type{UnionSetArray{Zonotope}}; nsdiv=100)
+    q = size(P.E, 1)
     dom = IntervalBox(IA.Interval(-1, 1), q)
     cells = mince(dom, nsdiv)
-    return UnionSetArray([overapproximate(p, Zonotope, c) for c in cells])
+    return UnionSetArray([overapproximate(P, Zonotope, c) for c in cells])
 end
