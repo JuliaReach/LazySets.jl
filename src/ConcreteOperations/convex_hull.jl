14b94922af570bf3018863325a4769c800dfc8fb
Luca Ferranti <49938764+lucaferranti@users.noreply.github.com>
2022-06-15 20:52:29 +0300

    linear combination and convex hull on sspz

--- src/ConcreteOperations/convex_hull.jl
+++ src/ConcreteOperations/convex_hull.jl
@@ -595,3 +595,38 @@ end
 convex_hull(X::LazySet, ::EmptySet) = X
 convex_hull(::EmptySet, X::LazySet) = X
 convex_hull(∅::EmptySet, ::EmptySet) = ∅
+
+"""
+    convex_hull(P1::SimpleSparsePolynomialZonotope, P2::SimpleSparsePolynomialZonotope)
+
+computes the convex hull of the simple spare polynomial zonotopes `P1` and `P2`.
+
+### Input
+
+- `P1` : simple sparse polynomial zonotopes
+- `P2` : simple sparse polynomial zonotopes
+
+### Outuput
+
+Tightest convex simple sparse polynomial zonotope containing `P1` and `P2`.
+"""
+function convex_hull(P1::SimpleSparsePolynomialZonotope, P2::SimpleSparsePolynomialZonotope)
+    return linear_combination(linear_combination(P1, P1), linear_combination(P2, P2))
+end
+
+"""
+    convex_hull(P::SimpleSparsePolynomialZonotope)
+
+computes the convex hull of the simple spare polynomial zonotope `P`.
+
+### Input
+
+- `P` -- simple sparse polynomial zonotopes
+
+### Outuput
+
+Tightest convex simple sparse polynomial zonotope containing `P`.
+"""
+function convex_hull(P::SimpleSparsePolynomialZonotope)
+    return linear_combination(P, P)
+end
---
+++ src/ConcreteOperations/linear_combination.jl
@@ -0,0 +1,29 @@
+export linear_combination
+
+"""
+    linear_combination(P1::SimpleSparsePolynomialZonotope, P2::SimpleSparsePolynomialZonotope)
+
+compute the linear combination of simple sparse polynomial zonotopes `P1` and `P2`.
+
+### Input
+
+- `P1` -- simple sparse polynomial zonotope
+- `P2` -- simple sparse polynomial zonotope
+
+### Output
+
+linear combination of `P1` and `P2`.
+"""
+function linear_combination(P1::SimpleSparsePolynomialZonotope, P2::SimpleSparsePolynomialZonotope)
+    c = 0.5 * (center(P1) + center(P2))
+    G = 0.5 * hcat(center(P1) - center(P2), genmat(P1), genmat(P1), genmat(P2), -genmat(P2))
+    E1, E2 = expmat(P1), expmat(P2)
+    et = promote_type(eltype(E1), eltype(E2))
+    E = hcat(vcat(zeros(et, nparams(P1) + nparams(P2)), one(et)),
+             vcat(E1, zeros(et, nparams(P2) + 1, ngens(P1))),
+             vcat(E2, zeros(et, nparams(P2), ngens(P1)), ones(et, 1, ngens(P1))),
+             vcat(zeros(et, nparams(P1), ngens(P2)), E2, zeros(et, 1, ngens(P2))),
+             vcat(zeros(et, nparams(P1), ngens(P2)), E2, ones(et, 1, ngens(P2))))
+
+    return SimpleSparsePolynomialZonotope(c, G, E)
+end
