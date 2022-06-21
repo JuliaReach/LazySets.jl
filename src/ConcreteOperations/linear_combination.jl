68de2285bd271174b53680cd985a60beb97c5091
Luca Ferranti <49938764+lucaferranti@users.noreply.github.com>
2022-06-21 10:41:25 +0300

    incorporate review comments

--- src/ConcreteOperations/linear_combination.jl
+++ src/ConcreteOperations/linear_combination.jl
@@ -19,13 +19,17 @@ linear combination of `P1` and `P2`.
 The linear combination of two sets ``P₁`` and ``P₂`` is defined as
 
 ```math
-\\{λp₁ + (1-λ)p₂ | p₁ ∈ P₁, p₂ ∈ P₂, λ ∈ [0, 1]\\}
+\\{1/2(1+λ)p₁ + 1/2(1-λ)p₂ | p₁ ∈ P₁, p₂ ∈ P₂, λ ∈ [-1, 1]\\}
 ```
 """
 function linear_combination(P1::SimpleSparsePolynomialZonotope, P2::SimpleSparsePolynomialZonotope)
-    c = 0.5 * (center(P1) + center(P2))
-    G = 0.5 * hcat(center(P1) - center(P2), genmat(P1), genmat(P1), genmat(P2), -genmat(P2))
+    c1, c2 = center(P1), center(P2)
+    G1, G2 = genmat(P1), genmat(P2)
     E1, E2 = expmat(P1), expmat(P2)
+
+    c = 0.5 * (c1 + c2)
+    G = 0.5 * hcat(c1 - c2, G1, G1, G2, -G2)
+
     et = promote_type(eltype(E1), eltype(E2))
     E = hcat(vcat(zeros(et, nparams(P1) + nparams(P2)), one(et)),
              vcat(E1, zeros(et, nparams(P2) + 1, ngens(P1))),
