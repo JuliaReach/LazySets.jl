769811e712aa743f7ab5fc794da3edb2400acca2
schillic <git@christianschilling.net>
2022-06-23 06:53:58 +0200

    fix linear_combination for SSPZ

--- src/ConcreteOperations/linear_combination.jl
+++ src/ConcreteOperations/linear_combination.jl
@@ -33,7 +33,7 @@ function linear_combination(P1::SimpleSparsePolynomialZonotope, P2::SimpleSparse
     N = promote_type(eltype(E1), eltype(E2))
     E = hcat(vcat(zeros(N, nparams(P1) + nparams(P2)), one(N)),
              vcat(E1, zeros(N, nparams(P2) + 1, ngens(P1))),
-             vcat(E2, zeros(N, nparams(P2), ngens(P1)), ones(N, 1, ngens(P1))),
+             vcat(E1, zeros(N, nparams(P2), ngens(P1)), ones(N, 1, ngens(P1))),
              vcat(zeros(N, nparams(P1), ngens(P2)), E2, zeros(N, 1, ngens(P2))),
              vcat(zeros(N, nparams(P1), ngens(P2)), E2, ones(N, 1, ngens(P2))))
 
