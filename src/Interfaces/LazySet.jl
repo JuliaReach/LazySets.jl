f863c851a2a36efe8fef6af9a9e76548fa3df61b
Luca Ferranti <49938764+lucaferranti@users.noreply.github.com>
2022-07-11 21:40:43 +0300

    update docstring

--- src/Interfaces/LazySet.jl
+++ src/Interfaces/LazySet.jl
@@ -30,5 +30,60 @@ julia> subtypes(LazySet, false)
  ConvexSet
 ```
 
+If we only consider *concrete* subtypes, then:
+
+```jldoctest; setup = :(using LazySets: subtypes)
+julia> concrete_subtypes = subtypes(LazySet, true);
+
+julia> length(concrete_subtypes)
+44
+
+julia> println.(concrete_subtypes);
+AffineMap
+Ball1
+Ball2
+BallInf
+Ballp
+Bloating
+CachedMinkowskiSumArray
+CartesianProduct
+CartesianProductArray
+ConvexHull
+ConvexHullArray
+Ellipsoid
+EmptySet
+ExponentialMap
+ExponentialProjectionMap
+HParallelotope
+HPolygon
+HPolygonOpt
+HPolyhedron
+HPolytope
+HalfSpace
+Hyperplane
+Hyperrectangle
+Intersection
+IntersectionArray
+Interval
+InverseLinearMap
+Line
+Line2D
+LineSegment
+LinearMap
+MinkowskiSum
+MinkowskiSumArray
+ResetMap
+RotatedHyperrectangle
+Singleton
+Star
+SymmetricIntervalHull
+Translation
+Universe
+VPolygon
+VPolytope
+ZeroSet
+Zonotope
+
+```
 """
 abstract type LazySet{N} end
