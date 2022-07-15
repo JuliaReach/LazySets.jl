d2a6b4c389e0ea68819ff7bb347bd5e6656d655e
Marcelo Forets <mforets@gmail.com>
2022-07-15 15:21:51 -0300

    Update src/ConcreteOperations/issubset.jl
    Co-authored-by: Christian Schilling <git@christianschilling.net>

--- src/ConcreteOperations/issubset.jl
+++ src/ConcreteOperations/issubset.jl
@@ -495,7 +495,7 @@ end
 
 function ⊆(L::LineSegment, S::LazySet, witness::Bool=false)
     throw(ArgumentError("this function requires the set type to be convex, but " *
-    "it is not the case for a $ST"))
+    "it is not the case for a $(typeof(S))"))
 end
 
 function _issubset_line_segment(L, S, witness)
