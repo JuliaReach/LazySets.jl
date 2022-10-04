83d6e032aad0d44f48942216f77134786608283c
schillic <git@christianschilling.net>
2022-10-04 21:02:52 +0200

    fix (de)activate_assertions

--- src/Utils/assertions.jl
+++ src/Utils/assertions.jl
@@ -1,12 +1,12 @@
 # convenience functions to (de)activate assertions
 function activate_assertions()
-    for m in [LazySets, Arrays, Approximations, Parallel]
-        Assertions.activate_assertions(m)
+    for m in [LazySets, Approximations, Parallel]
+        ReachabilityBase.Assertions.activate_assertions(m)
     end
 end
 
 function deactivate_assertions()
-    for m in [LazySets, Arrays, Approximations, Parallel]
-        Assertions.deactivate_assertions(m)
+    for m in [LazySets, Approximations, Parallel]
+        ReachabilityBase.Assertions.deactivate_assertions(m)
     end
 end
