402c2b40f5f2a3ad822c6611e467f363289415f8
schillic <git@christianschilling.net>
2022-10-10 22:43:19 +0200

    revise decompositions code

--- src/Approximations/decompositions.jl
+++ src/Approximations/decompositions.jl
@@ -1,76 +1,7 @@
 """
-     uniform_partition(n::Int, block_size::Int)
-
-Compute a uniform block partition of the given size.
-
-### Input
-
-- `n`          -- number of dimensions of the partition
-- `block_size` -- size of each block
-
-### Output
-
-A vector of ranges, `Vector{UnitRange{Int}}`, such that the size of each block
-is the same, if possible.
-
-### Examples
-
-If the number of dimensions `n` is 2, we have two options: either two blocks
-of size `1` or one block of size `2`:
-
-```jldoctest partition
-julia> LazySets.Approximations.uniform_partition(2, 1)
-2-element Vector{UnitRange{Int64}}:
- 1:1
- 2:2
-
-julia> LazySets.Approximations.uniform_partition(2, 2)
-1-element Vector{UnitRange{Int64}}:
- 1:2
-```
-
-If the block size argument is not compatible with (i.e. does not divide) `n`, the
-output is filled with one block of the size needed to reach `n`:
-
-```jldoctest partition
-julia> LazySets.Approximations.uniform_partition(3, 1)
-3-element Vector{UnitRange{Int64}}:
- 1:1
- 2:2
- 3:3
-
-julia> LazySets.Approximations.uniform_partition(3, 2)
-2-element Vector{UnitRange{Int64}}:
- 1:2
- 3:3
-
-julia> LazySets.Approximations.uniform_partition(10, 6)
-2-element Vector{UnitRange{Int64}}:
- 1:6
- 7:10
-```
-"""
-@inline function uniform_partition(n::Int, block_size::Int)
-    m = div(n, block_size)
-    r = n % block_size
-    res = Vector{UnitRange{Int}}(undef, r > 0 ? m + 1 : m)
-    k = 1
-    @inbounds for i in 1:m
-        l = k + block_size - 1
-        res[i] = k:l
-        k = l + 1
-    end
-    if r > 0
-        res[m+1] = k:n
-    end
-    return res
-end
-
-"""
-    decompose(S::ConvexSet{N},
+    decompose(S::LazySet{N},
               partition::AbstractVector{<:AbstractVector{Int}},
-              block_options
-             ) where {N}
+              block_options) where {N}
 
 Decompose a high-dimensional set into a Cartesian product of overapproximations
 of the projections over the specified subspaces.
@@ -96,16 +27,15 @@ corresponding overapproximation option.
 
 ### Notes
 
-The argument `partition` requires some discussion.
+The argument `partition` deserves some explanation.
 Typically, the list of blocks should form a partition of the set
 ``\\{1, \\dots, n\\}`` represented as a list of consecutive blocks, where ``n``
 is the ambient dimension of set `S`.
 
 However, technically there is no problem if the blocks are not consecutive,
 blocks are missing, blocks occur more than once, or blocks are overlapping.
-This function will, however, stick to the order of blocks, so the resulting set
-must be interpreted with care in such cases.
-One use case is the need of a projection consisting of several blocks.
+The resulting set must be interpreted with care in such cases (e.g., it will not
+necessarily be an overapproximation of `S`).
 
 For convenience, the argument `block_options` can also be given as a single
 option instead of a mapping, which is then interpreted as the option for all
@@ -113,16 +43,16 @@ blocks.
 
 ### Examples
 
-This function supports different options: one can specify the target set, the
-degree of accuracy, and template directions.
+The argument `block_options` supports different options: one can specify the
+target set, the degree of accuracy, and template directions.
 These options are exemplified below, where we use the following example.
 
 ```jldoctest decompose_examples
 julia> S = Ball2(zeros(4), 1.0);  # set to be decomposed (4D 2-norm unit ball)
 
-julia> P2d = [1:2, 3:4];  # a partition with two blocks of size two
+julia> P2d = [1:2, 3:4];  # a partition with two blocks, each of size two
 
-julia> P1d = [[1], [2], [3], [4]];  # a partition with four blocks of size one
+julia> P1d = [[1], [2], [3], [4]];  # a partition with four blocks, each of size one
 ```
 
 #### Different set types
@@ -130,14 +60,18 @@ julia> P1d = [[1], [2], [3], [4]];  # a partition with four blocks of size one
 We can decompose using polygons in constraint representation:
 
 ```jldoctest decompose_examples
-julia> all(ai isa HPolygon for ai in array(decompose(S, P2d, HPolygon)))
+julia> Y = decompose(S, P2d, HPolygon);
+
+julia> all(ai isa HPolygon for ai in array(Y))
 true
 ```
 
 For decomposition into 1D subspaces, we can use `Interval`:
 
 ```jldoctest decompose_examples
-julia> all(ai isa Interval for ai in array(decompose(S, P1d, Interval)))
+julia> Y = decompose(S, P1d, Interval);
+
+julia> all(ai isa Interval for ai in array(Y))
 true
 ```
 
@@ -146,7 +80,7 @@ interface presented so far does not apply.
 See the paragraph *Advanced input for different block approximations* below for
 how to do that.
 
-#### Refining the decomposition I: ``ε``-close approximation
+#### Refining the decomposition: ``ε``-close approximation
 
 The ``ε`` option can be used to refine a decomposition, i.e., obtain a more
 accurate result.
@@ -171,10 +105,10 @@ julia> [length(constraints_list(d(ε, 1))) for ε in [Inf, 0.1, 0.01]]
  32
 ```
 
-#### Refining the decomposition II: template polyhedra
+#### Refining the decomposition: template polyhedra
 
 Another way to refine a decomposition is by using template polyhedra.
-The idea is to specify a set of template directions and then to compute on each
+The idea is to specify a set of template directions and then compute on each
 block the polytopic overapproximation obtained by evaluating the support
 function of the given input set over the template directions.
 
@@ -202,10 +136,11 @@ true
 
 Instead of defining the approximation option uniformly for each block, we can
 define different approximations for different blocks.
-The third argument has to be a mapping from block index (in the partition) to
-the corresponding approximation option.
+For this purpose, the argument `block_options` can also be a mapping from block
+index (in the partition) to the corresponding approximation option.
 
-For example:
+For example, we can approximate the first block with a `Hyperrectangle` and the
+second block with ``ε``-close approximation for ``ε = 0.1``:
 
 ```jldoctest decompose_examples
 julia> res = array(decompose(S, P2d, Dict(1 => Hyperrectangle, 2 => 0.1)));
@@ -214,12 +149,11 @@ julia> typeof(res[1]), typeof(res[2])
 (Hyperrectangle{Float64, Vector{Float64}, Vector{Float64}}, HPolygon{Float64, Vector{Float64}})
 ```
 """
-function decompose(S::ConvexSet{N},
+function decompose(S::LazySet{N},
                    partition::AbstractVector{<:AbstractVector{Int}},
-                   block_options
-                  ) where {N}
+                   block_options) where {N}
     n = dim(S)
-    result = Vector{ConvexSet{N}}(undef, length(partition))
+    result = Vector{ConvexSet{N}}(undef, length(partition))  # TODO generalize to LazySet
 
     @inbounds for (i, block) in enumerate(partition)
         result[i] = project(S, block, block_options[i], n)
@@ -228,17 +162,15 @@ function decompose(S::ConvexSet{N},
 end
 
 # convenience method with uniform block options
-function decompose(S::ConvexSet{N},
+function decompose(S::LazySet{N},
                    partition::AbstractVector{<:AbstractVector{Int}},
-                   block_options::Union{Type{<:ConvexSet},
+                   block_options::Union{Type{<:LazySet},
                                         Pair{<:UnionAll, <:Real},
                                         Real,
                                         Type{<:AbstractDirections},
-                                        Nothing
-                                       }
-                  ) where {N}
+                                        Nothing}) where {N}
     n = dim(S)
-    result = Vector{ConvexSet{N}}(undef, length(partition))
+    result = Vector{ConvexSet{N}}(undef, length(partition))  # TODO generalize to LazySet
 
     @inbounds for (i, block) in enumerate(partition)
         result[i] = project(S, block, block_options, n)
@@ -246,8 +178,25 @@ function decompose(S::ConvexSet{N},
     return CartesianProductArray(result)
 end
 
-# convenience method with uniform block size
-function decompose(S::ConvexSet, block_options; block_size::Int=1)
+"""
+    decompose(S::LazySet, block_options; [block_size]::Int=1)
+
+Decompose a high-dimensional set into a Cartesian product of overapproximations
+of the projections over uniformly-sized subspaces.
+
+### Input
+
+- `S`             -- set
+- `block_options` -- overapproximation option or mapping from block indices to a
+                     corresponding overapproximation option
+- `block_size`    -- (optional; default: `1`) size of the blocks
+
+### Output
+
+A `CartesianProductArray` containing the low-dimensional approximated
+projections.
+"""
+function decompose(S::LazySet, block_options; block_size::Int=1)
     partition = uniform_partition(dim(S), block_size)
     return decompose(S, partition, block_options)
 end
