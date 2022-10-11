474d22a49dc030013f4191a7f911ce4bed5f11db
Mauricio Vanzulli <50339940+mvanzulli@users.noreply.github.com>
2022-10-11 17:40:47 -0300

    Update src/Sets/VPolygonNC.jl
    Co-authored-by: Christian Schilling <account@christianschilling.net>

--- src/Sets/VPolygonNC.jl
+++ src/Sets/VPolygonNC.jl
@@ -12,7 +12,7 @@ Type that represents a non-convex polygon by its vertices.
 ### Examples
 
 A non-convex polygon in vertex representation can be constructed by passing the list of
-vertices. For example, we can build a teeth:
+vertices. For example, we can build a tooth:
 
 ```jldoctest polygon_v_ncrep
 julia> NCP = VPolygonNC( [[0., 0], [0, 2], [2, 2], [2, 0], [1, 1]]);
