83aef27512c4dc84df2d4ceac225d7381e6d660d
schillic <git@christianschilling.net>
2022-10-25 21:07:07 +0200

    revise AbstractPolyhedron code

--- src/Interfaces/AbstractPolyhedron.jl
+++ src/Interfaces/AbstractPolyhedron.jl
@@ -8,8 +8,16 @@ Abstract type for compact convex polyhedral sets.
 ### Notes
 
 Every concrete `AbstractPolyhedron` must define the following functions:
+
 - `constraints_list(::AbstractPolyhedron{N})` -- return a list of all facet
     constraints
+
+Polyhedra are defined as the intersection of a finite number of closed
+half-spaces.
+As such, polyhedra are closed and convex but not necessarily bounded.
+Bounded polyhedra are called *polytopes* (see [`AbstractPolytope`](@ref)).
+
+The subtypes of `AbstractPolyhedron` (including abstract interfaces):
 
 ```jldoctest; setup = :(using LazySets: subtypes)
 julia> subtypes(AbstractPolyhedron)
@@ -23,17 +31,8 @@ julia> subtypes(AbstractPolyhedron)
  Star
  Universe
 ```
-
-Polyhedra are defined as the intersection of a finite number of closed
-half-spaces.
-As such, polyhedra are closed and convex but not necessarily bounded.
-Bounded polyhedra are called *polytopes* (see [`AbstractPolytope`](@ref)).
 """
 abstract type AbstractPolyhedron{N} <: ConvexSet{N} end
-
-isconvextype(::Type{<:AbstractPolyhedron}) = true
-
-# --- common AbstractPolyhedron functions ---
 
 # supertype for all linear map algorithms
 abstract type AbstractLinearMapAlgorithm end
