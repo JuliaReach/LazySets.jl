8252a1a812d753eac48f7d1815ff97678b3337e9
schillic <git@christianschilling.net>
2023-04-02 21:04:58 +0200

    add complement method for EmptySet

--- src/Sets/EmptySet.jl
+++ src/Sets/EmptySet.jl
@@ -527,3 +527,20 @@ The empty set.
 function rectify(∅::EmptySet)
     return ∅
 end
+
+"""
+    complement(∅::EmptySet{N}) where {N}
+
+Return the complement of an empty set.
+
+### Input
+
+- `∅` -- empty set
+
+### Output
+
+The universe of the same dimension.
+"""
+function complement(∅::EmptySet{N}) where {N}
+    return Universe{N}(dim(∅))
+end
