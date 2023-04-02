45073d11ec9cbe55eb0e22cb7e8dc820b5b2ccdc
schillic <git@christianschilling.net>
2023-04-02 18:11:54 +0200

    add reflect method for VPolytope

--- src/Sets/VPolytope.jl
+++ src/Sets/VPolytope.jl
@@ -594,3 +594,21 @@ function permute(P::VPolytope, p::AbstractVector{Int})
     vlist = [v[p] for v in P.vertices]
     return VPolytope(vlist)
 end
+
+"""
+    reflect(P::VPolytope)
+
+Concrete reflection of a polytope in vertex representation `P`, resulting in the
+reflected set `-P`.
+
+### Input
+
+- `P` -- polytope in vertex representation
+
+### Output
+
+The `VPolytope` representing `-P`.
+"""
+function reflect(P::VPolytope)
+    return VPolytope(-P.vertices)
+end
