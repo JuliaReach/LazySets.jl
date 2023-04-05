fc7f6c1eddaafb71c73f0d2521b6a0edebd750e3
Marcelo Forets <mforets@gmail.com>
2023-04-06 00:14:47 +0100

    Update src/Sets/Tetrahedron.jl
    Co-authored-by: Christian Schilling <git@christianschilling.net>

--- src/Sets/Tetrahedron.jl
+++ src/Sets/Tetrahedron.jl
@@ -106,8 +106,8 @@ function ∈(x::AbstractVector, T::Tetrahedron)
 end
 
 function rand(::Type{Tetrahedron}; N::Type{<:Real}=Float64, rng::AbstractRNG=GLOBAL_RNG, seed::Union{Int, Nothing}=nothing)
-    # Dummy result for tests.
-    vertices = [N[1, 0, -1/sqrt(2)], N[-1, 0, -1/sqrt(2)], N[0, 1, 1/sqrt(2)], N[0, -1, 1/sqrt(2)]];
+    P = rand(VPolytope; N=N, dim=3, rng=rng, seed=seed, num_vertices=4)
+    vertices = P.vertices
     return Tetrahedron(vertices)
 end
 
