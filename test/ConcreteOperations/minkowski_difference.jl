6d946ada2508e09bfdb0058737fb5a44174a03fa
schillic <git@christianschilling.net>
2023-04-06 22:18:54 +0200

    move tests and add test in 3D

--- test/ConcreteOperations/minkowski_difference.jl
+++ test/ConcreteOperations/minkowski_difference.jl
@@ -38,4 +38,30 @@ for N in [Float64, Float32, Rational{Int}]
     Zs3 = Zonotope(N[0, 0], N[2 0; -1/2 1/2])
     D3 = minkowski_difference(Zm, Zs3)
     @test D3 isa EmptySet{N} && dim(D3) == 2
+
+    # zonotope in 3D
+    Zm = Zonotope(N[1, 1, 1], N[1 0 0; 0 1 0; 0 0 1])
+    Zs = Zonotope(N[0, 0, 1], hcat(N[1/2; 0; 0]))
+    D = minkowski_difference(Zm, Zs)
+    @test isequivalent(D, HPolytope([
+        HalfSpace(N[0, 0, 1], N(1)), HalfSpace(N[0, 0, -1], N(1)),
+        HalfSpace(N[0, 1, 0], N(2)), HalfSpace(N[0, -1, 0], N(0)),
+        HalfSpace(N[1, 0, 0], N(3/2)), HalfSpace(N[-1, 0, 0], N(-1/2))]))
+    if N == Float64
+        @test isequivalent(D, Zonotope(N[1, 1, 0], N[1/2 0 0; 0 1 0; 0 0 1]))
+    end
+end
+
+for N in [Float64]
+    if test_suite_polyhedra
+        # concrete Minkowski difference for unbounded P (HPolyhedron)
+        mx1 = N(2)
+        mx2 = N(5)
+        P3 = HPolyhedron(N[mx1 0; 0 mx2], N[3, 3])
+        radius = 2
+        Q3 = Ball1(N[0, 0], N(radius))
+        C3 = minkowski_difference(P3, Q3)
+        C3_res = HPolyhedron(N[mx1 0; 0 mx2], N[3 - mx1*radius, 3 - mx2*radius])
+        @test C3 ⊆ C3_res && C3_res ⊆ C3
+    end
 end
