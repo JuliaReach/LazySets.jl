75ff8b34c3ec5c11c96b5f87d6718b1039dc79be
schillic <git@christianschilling.net>
2023-04-07 21:00:10 +0200

    support function for Ball1

--- src/Sets/Ball1.jl
+++ src/Sets/Ball1.jl
@@ -135,6 +135,33 @@ function σ(d::AbstractVector, B::Ball1)
 end
 
 """
+    ρ(d::AbstractVector, B::Ball1)
+
+Evaluate the support function of a ball in the 1-norm in the given direction.
+
+### Input
+
+- `d` -- direction
+- `B` -- ball in the 1-norm
+
+### Output
+
+Evaluation of the support function in the given direction.
+
+### Algorithm
+
+Let ``c`` and ``r`` be the center and radius of the ball ``B`` in the 1-norm,
+respectively. Then:
+
+```math
+ρ(d, B) = ⟨d, c⟩ + r ‖d‖_∞.
+```
+"""
+function ρ(d::AbstractVector, B::Ball1)
+    return dot(d, B.center) + B.radius * maximum(abs, d)
+end
+
+"""
     ∈(x::AbstractVector, B::Ball1, [failfast]::Bool=false)
 
 Check whether a given point is contained in a ball in the 1-norm.
