411666ef46d88fc705e75ac0dfde858df26466cc
schillic <git@christianschilling.net>
2023-04-07 09:22:48 +0200

    use kwargs in default default_lp_solver_polyhedra method

--- src/Interfaces/AbstractPolyhedron_functions.jl
+++ src/Interfaces/AbstractPolyhedron_functions.jl
@@ -32,7 +32,7 @@ end
 # default LP solver for Polyhedra (fallback method)
 # NOTE: exists in parallel to `default_lp_solver` because we use different
 # interfaces (see #1493)
-function default_lp_solver_polyhedra(N, varargs...)
+function default_lp_solver_polyhedra(N; kwargs...)
     require(@__MODULE__, :Polyhedra; fun_name="default_lp_solver_polyhedra")
     error("no default solver for numeric type $N")
 end
