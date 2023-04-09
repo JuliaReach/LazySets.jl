b1d99fb68ea981cd8e03119c803a896fe260d9d4
schillic <git@christianschilling.net>
2023-04-09 08:37:59 +0200

    define convert methods to HPolygon in eval loop

--- src/convert.jl
+++ src/convert.jl
@@ -8,62 +8,6 @@ for T in subtypes(LazySet, true)
 end
 
 """
-    convert(T::Type{HPOLYGON}, P::VPolygon) where {HPOLYGON<:AbstractHPolygon}
-
-Convert a polygon in vertex representation to a polygon in constraint
-representation.
-
-### Input
-
-- `HPOLYGON` -- target type
-- `P`        -- polygon in vertex representation
-
-### Output
-
-A polygon in constraint representation.
-"""
-function convert(T::Type{HPOLYGON},
-                 P::VPolygon) where {HPOLYGON<:AbstractHPolygon}
-    return tohrep(P, T)
-end
-
-"""
-    convert(::Type{HPOLYGON}, X::LazySet; [check_boundedness]::Bool=true,
-            prune::Bool=true) where {HPOLYGON<:AbstractHPolygon}
-
-Convert a two-dimensional polytope to a polygon in vertex representation.
-
-### Input
-
-- `HPOLYGON`          -- target type
-- `X`                 -- two-dimensional polytope
-- `check_boundedness` -- (optional, default `!isboundedtype(typeof(X))`) if
-                         `true` check whether the set `X` is bounded before
-                         creating the polygon
-- `prune`             -- (optional, default: `true`) flag for removing redundant
-                         constraints in the end
-
-### Output
-
-A polygon in constraint representation.
-
-### Algorithm
-
-We compute the list of constraints of `X`, then instantiate the polygon.
-"""
-function convert(::Type{HPOLYGON}, X::LazySet;
-                 check_boundedness::Bool=!isboundedtype(typeof(X)),
-                 prune::Bool=true) where {HPOLYGON<:AbstractHPolygon}
-    @assert dim(X) == 2 "set must be two-dimensional for conversion, but it " *
-        "has dimension $(dim(X))"
-    PT = basetype(HPOLYGON)
-    if check_boundedness && !isbounded(X)
-        throw(ArgumentError("expected a bounded set for conversion to `$PT`"))
-    end
-    return PT(constraints_list(X); prune=prune)
-end
-
-"""
     convert(::Type{VPolygon}, P::AbstractHPolygon)
 
 Convert a polygon in constraint representation to a polygon in vertex
@@ -401,64 +345,6 @@ applies the (concrete) linear map to the zonotope.
 function convert(::Type{Zonotope}, S::LinearMap{N, CartesianProductArray{N, HN}}
                 ) where {N, HN<:AbstractHyperrectangle}
     return linear_map(S.M, convert(Zonotope, S.X))
-end
-
-"""
-    convert(::Type{HPOLYGON}, S::AbstractSingleton{N}
-           ) where {N, HPOLYGON<:AbstractHPolygon}
-
-Convert a singleton to a polygon in constraint representation.
-
-### Input
-
-- `HPOLYGON` -- target type
-- `S`        -- singleton
-
-### Output
-
-A polygon in constraint representation with the minimal number of constraints
-(three).
-"""
-function convert(::Type{HPOLYGON}, S::AbstractSingleton{N}
-                ) where {N, HPOLYGON<:AbstractHPolygon}
-    constraints_list = Vector{HalfSpace{N, Vector{N}}}(undef, 3)
-    o = one(N)
-    z = zero(N)
-    v = element(S)
-    constraints_list[1] = HalfSpace([o, o], v[1] + v[2])
-    constraints_list[2] = HalfSpace([-o, z], -v[1])
-    constraints_list[3] = HalfSpace([z, -o], -v[2])
-    return HPOLYGON(constraints_list)
-end
-
-"""
-    convert(::Type{HPOLYGON}, L::LineSegment{N}
-          ) where {N, HPOLYGON<:AbstractHPolygon}
-
-Convert a line segment to a polygon in constraint representation.
-
-### Input
-
-- `HPOLYGON` -- target type
-- `L`        -- line segment
-- `prune`    -- (optional, default: `false`) flag for removing redundant
-                constraints in the end
-### Output
-
-A flat polygon in constraint representation with the minimal number of
-constraints (four).
-"""
-function convert(::Type{HPOLYGON}, L::LineSegment{N}) where {N, HPOLYGON<:AbstractHPolygon}
-    H = HPOLYGON{N}()
-    c = halfspace_left(L.p, L.q)
-    addconstraint!(H, c; prune=false)
-    addconstraint!(H, HalfSpace(-c.a, -c.b); prune=false)
-    line_dir = L.q - L.p
-    c = HalfSpace(line_dir, dot(L.q, line_dir))
-    addconstraint!(H, c; prune=false)
-    line_dir = -line_dir
-    addconstraint!(H, HalfSpace(line_dir, dot(L.p, line_dir)); prune=false)
-    return H
 end
 
 """
@@ -954,6 +840,120 @@ for T in [HPolygon, HPolygonOpt, HPolytope, HPolyhedron]
     function convert(::Type{$T}, P::IntersectionArray)
         clist = reduce(vcat, constraints_list.(array(P)))
         return ($T)(clist)
+    end
+end
+end
+
+for T in subtypes(AbstractHPolygon, true)
+@eval begin
+"""
+    convert(::Type{$($T)}, X::LazySet; [check_boundedness]::Bool=true,
+            prune::Bool=true)
+
+Convert a two-dimensional polytopic set to a polygon in constraint
+representation.
+
+### Input
+
+- `$($T)`             -- target type
+- `X`                 -- two-dimensional polytopic set
+- `check_boundedness` -- (optional, default `!isboundedtype(typeof(X))`) if
+                         `true` check whether the set `X` is bounded before
+                         creating the polygon
+- `prune`             -- (optional, default: `true`) flag for removing redundant
+                         constraints in the end
+
+### Output
+
+A polygon in constraint representation.
+
+### Algorithm
+
+We compute the list of constraints of `X`, then instantiate the polygon.
+"""
+    function convert(::Type{$T}, X::LazySet;
+                     check_boundedness::Bool=!isboundedtype(typeof(X)),
+                     prune::Bool=true)
+        @assert dim(X) == 2 "set must be two-dimensional for conversion, but it " *
+            "has dimension $(dim(X))"
+        if check_boundedness && !isbounded(X)
+            throw(ArgumentError("expected a bounded set for conversion to `$T`"))
+        end
+        return $T(constraints_list(X); prune=prune)
+    end
+
+"""
+    convert(T::Type{$($T)}, P::VPolygon)
+
+Convert a polygon in vertex representation to a polygon in constraint
+representation.
+
+### Input
+
+- `$($T)` -- target type
+- `P`     -- polygon in vertex representation
+
+### Output
+
+A polygon in constraint representation.
+"""
+    function convert(T::Type{$T}, P::VPolygon)
+        return tohrep(P, T)
+    end
+
+"""
+    convert(::Type{$($T)}, S::AbstractSingleton{N}) where {N}
+
+Convert a singleton to a polygon in constraint representation.
+
+### Input
+
+- `$($T)` -- target type
+- `S`     -- singleton
+
+### Output
+
+A polygon in constraint representation with the minimal number of constraints
+(three).
+"""
+    function convert(::Type{$T}, S::AbstractSingleton{N}) where {N}
+        constraints_list = Vector{HalfSpace{N, Vector{N}}}(undef, 3)
+        o = one(N)
+        z = zero(N)
+        v = element(S)
+        constraints_list[1] = HalfSpace([o, o], v[1] + v[2])
+        constraints_list[2] = HalfSpace([-o, z], -v[1])
+        constraints_list[3] = HalfSpace([z, -o], -v[2])
+        return $T(constraints_list)
+    end
+
+"""
+    convert(::Type{$($T)}, L::LineSegment{N}) where {N}
+
+Convert a line segment to a polygon in constraint representation.
+
+### Input
+
+- `$($T)` -- target type
+- `L`     -- line segment
+- `prune` -- (optional, default: `false`) flag for removing redundant
+             constraints in the end
+### Output
+
+A flat polygon in constraint representation with the minimal number of
+constraints (four).
+"""
+    function convert(::Type{$T}, L::LineSegment{N}) where {N}
+        H = $T{N}()
+        c = halfspace_left(L.p, L.q)
+        addconstraint!(H, c; prune=false)
+        addconstraint!(H, HalfSpace(-c.a, -c.b); prune=false)
+        line_dir = L.q - L.p
+        c = HalfSpace(line_dir, dot(L.q, line_dir))
+        addconstraint!(H, c; prune=false)
+        line_dir = -line_dir
+        addconstraint!(H, HalfSpace(line_dir, dot(L.p, line_dir)); prune=false)
+        return H
     end
 end
 end
