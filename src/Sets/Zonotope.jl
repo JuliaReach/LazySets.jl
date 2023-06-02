1eb38c9a63e934dfb088ef9e99d809dc97c0010a
Peng Yu <yupbank@users.noreply.github.com>
2023-06-02 11:12:49 -0400

    General 2d Zonotope  vertex enumeration with linear complexity (#2288)
    * update 2d zonotope enumeration
    
    * Update src/Sets/Zonotope.jl
    
    Co-authored-by: Marcelo Forets <mforets@gmail.com>
    
    * maybe we should force the direction
    
    * Update src/Sets/Zonotope.jl
    
    Co-authored-by: Marcelo Forets <mforets@gmail.com>
    
    ---------
    
    Co-authored-by: Marcelo Forets <mforets@gmail.com>
    Co-authored-by: Christian Schilling <git@christianschilling.net>

--- src/Sets/Zonotope.jl
+++ src/Sets/Zonotope.jl
@@ -438,6 +438,42 @@ function _bound_intersect_2D(Z::Zonotope, L::Line2D)
     return element(singleton)[2]
 end
 
+# ====================================
+# Zonotope vertex enumeration methods
+# ====================================
+    
+@inline function _angles(point::AbstractVector{N})  where{N}
+    (atand(point[2], point[1]) + 360) % 360
+end
+
+function _single_quadrant_vertices_enum(G::AbstractMatrix{N}, sorted::Bool=false) where{N}
+    if !sorted
+        G = sortslices(G, dims=2, by=_angles)
+    end
+    return VPolygon(2*cumsum(hcat(G, -G), dims=2) .- sum(G, dims=2))
+end
+
+function _vertices_list_2D(c::AbstractVector{N}, G::AbstractMatrix{N}; apply_convex_hull::Bool) where {N}
+    if apply_convex_hull
+        return _vertices_list_iterative(c, G, apply_convex_hull=apply_convex_hull)
+    end
+    angles = mapslices(_angles, G, dims=1)[1, :]
+    perm = sortperm(angles)
+    sorted_angles = angles[perm]
+    sorted_G = G[:, perm]
+    polygons  = Vector{VPolygon{N}}()
+    sizehint!(polygons, 4)
+
+    @inbounds for i in zip(0:90:360, 90:90:360)
+        index = i[1] .<= sorted_angles .< i[2]
+        if sum(index) > 0
+            push!(polygons, _single_quadrant_vertices_enum(sorted_G[:, index], true))
+        end
+    end
+
+    return vertices_list(translate(reduce(minkowski_sum, polygons), c))
+end
+
 """
     remove_redundant_generators(Z::Zonotope{N}) where {N}
 
