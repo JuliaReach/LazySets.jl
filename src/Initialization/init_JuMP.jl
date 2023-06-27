897ec19e407769da320134c24bebf4e3c48794c1
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-06-27 11:25:37 +0200

    More lin_prog performance with multiple dispatch, views, and vectorized constraints

--- src/Initialization/init_JuMP.jl
+++ src/Initialization/init_JuMP.jl
@@ -30,22 +30,16 @@ function has_lp_infeasibility_ray(model)
     return primal_status(model) == JuMP.INFEASIBILITY_CERTIFICATE
 end
 
-# solve a linear program (in the old MathProgBase interface)
-function linprog(c, A, sense::Char, b, l::Number, u::Number, solver_or_model)
-    n = length(c)
-    m = length(b)
-    return linprog(c, A, fill(sense, m), b, fill(l, n), fill(u, n), solver_or_model)
-end
-
+# solve a linear program (in the MathOptInterface)
 function linprog(c, A, sense, b, l, u, solver_or_model)
     n = length(c)
     model = linprog_model(solver_or_model)
-    @variable(model, l[i] <= x[i=1:n] <= u[i])
+
+    x = lin_prog_variable!(model, n, l, u)
+    lin_prog_constraints!(model, A, sense, b, x)
+
     @objective(model, Min, c' * x)
-    eq_rows, ge_rows, le_rows = sense .== '=', sense .== '>', sense .== '<'
-    @constraint(model, A[eq_rows, :] * x .== b[eq_rows])
-    @constraint(model, A[ge_rows, :] * x .>= b[ge_rows])
-    @constraint(model, A[le_rows, :] * x .<= b[le_rows])
+
     optimize!(model)
     return (status = termination_status(model),
             objval = objective_value(model),
@@ -58,3 +52,38 @@ end
     return model
 end
 @inline linprog_model(solver) = Model(solver)
+
+function lin_prog_variable!(model, n, l::Number, u::Number)
+    if isfinite(l) && isfinite(u)
+        return @variable(model, x[1:n], lower_bound=l, upper_bound=u)
+    elseif isfinite(l)
+        return @variable(model, x[1:n], lower_bound=l)
+    elseif isfinite(u)
+        return @variable(model, x[1:n], upper_bound=u)
+    else 
+        return @variable(model, x[1:n])
+    end
+end
+
+function lin_prog_variable!(model, n, l::Vector{T}, u::Vector{T}) where {T}
+    return @variable(model, l[i] <= x[i=1:n] <= u[i])
+end
+
+function lin_prog_constraints!(model, A, sense::Char, b, x)
+    if sense == '='
+        @constraint(model, A * x == b)
+    elseif sense == '>'
+        @constraint(model, A * x >= b)
+    elseif sense == '<'
+        @constraint(model, A * x <= b)
+    else
+        throw("Invalid sense ($sense) for constraints in lin_prog.")
+    end
+end
+
+function lin_prog_constraints!(model, A, sense, b, x)
+    eq_rows, ge_rows, le_rows = sense .== '=', sense .== '>', sense .== '<'
+    @constraint(model, view(A, eq_rows, :) * x == view(b, eq_rows))
+    @constraint(model, view(A, ge_rows, :) * x >= view(b, ge_rows))
+    @constraint(model, view(A, le_rows, :) * x <= view(b, le_rows))
+end
