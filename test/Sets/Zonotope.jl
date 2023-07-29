2c72086419e55c98ad4d2630bdab6c4db2223a95
schillic <git@christianschilling.net>
2023-07-29 21:25:59 +0200

    better 1D linear_map of AbstractZonotope

--- test/Sets/Zonotope.jl
+++ test/Sets/Zonotope.jl
@@ -95,6 +95,10 @@ for N in [Float64, Rational{Int}, Float32]
     Z5 = scale(1 // 2, Z3)
     @test Z5.center == N[0, 1]
     @test Z5.generators == N[1//2 1//2 1//2 0; -1//2 1//2 0 1//2]
+    # 1D simplifies to 1 generator
+    M = N[1 1;]
+    Z6 = linear_map(M, Z3)
+    @test ngens(Z6) == 1 && genmat(Z6) == hcat(N[4])
 
     # in-place linear map
     Zin = convert(Zonotope, BallInf(zeros(N, 2), N(1)))
