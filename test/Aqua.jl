0358fa78bdf64ce010c9fd28f76f74dbbaf18592
schillic <git@christianschilling.net>
2024-02-23 23:48:24 +0100

    add Aqua to tests

---
+++ test/Aqua.jl
@@ -0,0 +1,14 @@
+using LazySets, Test
+import Aqua
+
+@testset "Aqua tests" begin
+    Aqua.test_all(LazySets; ambiguities=false,
+                  # the unbound args should be resolved in the future
+                  unbound_args=(broken=true,),
+                  # known piracies
+                  piracies=(treat_as_own=[<=, rand, LazySets.activate_assertions,
+                                          LazySets.deactivate_assertions],))
+
+    # do not warn about ambiguities in dependencies
+    Aqua.test_ambiguities(LazySets)
+end
