e075fbf52b585b7c26af16631f2d3e9b4fa7a182
schillic <git@christianschilling.net>
2024-02-25 23:07:16 +0100

    add convenience functions to construct common intervals

--- src/Initialization/init_TaylorModels.jl
+++ src/Initialization/init_TaylorModels.jl
@@ -1,8 +1,11 @@
 using .TaylorModels: domain
+using .TaylorModels.TaylorSeries: numtype
+
+_eltype_TM(TMi) = numtype(polynomial(TMi))
 
 # check that a vector of Taylor models has the [-1, 1] domain
-function _has_normalized_domain(vTM)
-    return all(p -> all(==(IA.interval(-1, 1)), domain(p)), vTM)
+function _has_normalized_domain(vTM::Vector)
+    return all(TMi -> all(==(sym_itv(_eltype_TM(TMi))), domain(TMi)), vTM)
 end
 
 eval(load_taylormodels_convert_polynomial_zonotope())
