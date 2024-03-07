2769bfbfddd620b9011fe40488adc0b4e243553c
schillic <git@christianschilling.net>
2024-03-07 17:52:00 +0100

    zonotope overapproximation of zonotope w/ axis-aligned half-space

--- test/Approximations/overapproximate.jl
+++ test/Approximations/overapproximate.jl
@@ -513,13 +513,20 @@ for N in [Float64]
     cap = overapproximate(Z ∩ L, OctDirections)
     @test (L ∩ Z) ⊆ cap
 
-    # NOTE: ICP currently leads to unsatisfiable package requirements
     # overapproximate a nonlinear constraint with an HPolyhedron
-    #     dom = IntervalBox(interval(-2, 2), interval(-2, 2))
-    #     C = @constraint x^2 + y^2 <= 1
-    #     p = pave(C, dom, 0.01)
-    #     dirs = OctDirections(2)
-    #     H = overapproximate(p, dirs)
-    #     B2 = Ball2(N[0, 0], N(1))
-    #     @test B2 ⊆ H
+    dom = IntervalBox(interval(-2, 2), interval(-2, 2))
+    C = ICP.@constraint x^2 + y^2 <= 1
+    p = ICP.pave(C, dom, 0.01)
+    dirs = OctDirections(2)
+    H = overapproximate(p, dirs)
+    B2 = Ball2(N[0, 0], N(1))
+    @test B2 ⊆ H
+
+    # overapproximate the intersection of a zonotope with an axis-aligned
+    # half-space by a zonotope using ICP
+    Z = Zonotope(N[0, 2], N[1//2 1//2; 1 0])
+    H = HalfSpace(SingleEntryVector(1, 2, N(1)), N(0))
+    R = overapproximate(Z ∩ H, Zonotope)
+    R_exact = intersection(Z, H)
+    @test R ⊆ Z && R_exact ⊆ R
 end
