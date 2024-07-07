206da57c84e5dba991eafb3f969169cec81e5db5
schillic <git@christianschilling.net>
2024-07-07 23:21:13 +0200

    outsource binary operation to module

--- src/Sets/ZeroSet/ZeroSetModule.jl
+++ src/Sets/ZeroSet/ZeroSetModule.jl
@@ -7,7 +7,7 @@ using Random: AbstractRNG, GLOBAL_RNG
 using ReachabilityBase.Distribution: reseed!
 
 @reexport import ..API: dim, isoperationtype, rand, rectify, reflect, ∈,
-                        linear_map, scale, scale!, ρ, translate
+                        linear_map, scale, scale!, ρ, translate, minkowski_sum
 @reexport import ..LazySets: element
 @reexport using ..API
 
@@ -27,5 +27,7 @@ include("support_function.jl")
 include("translate.jl")
 
 include("element.jl")
+
+include("minkowski_sum.jl")
 
 end  # module
