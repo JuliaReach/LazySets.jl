583d986626f38e8f17eb2af0c938b66d49b5ec24
schillic <git@christianschilling.net>
2024-07-12 21:25:26 +0200

    add tests

--- test/Sets/Tetrahedron.jl
+++ test/Sets/Tetrahedron.jl
@@ -10,6 +10,10 @@ for N in [Float64, Float32, Rational{Int}]
     Tmat = Tetrahedron(vmat)
     @test Tmat == T
 
+    # constraints_list
+    c = constraints_list(T)
+    @test isequivalent(HPolytope(c), VPolytope(vertices))
+
     # support vector
     @test σ(ones(3), T) == N[0, 1, 1 / sqrt(2)]
 
@@ -23,3 +27,10 @@ for N in [Float64, Float32, Rational{Int}]
     T = Tetrahedron([N[0, 0, 0], N[0, 1, 0], N[1, 0, 0], N[1, 0, 1]])
     @test zeros(N, 3) ∈ T
 end
+
+for N in [Float64, Float32]
+    # rand
+    @test rand(Tetrahedron; N=N) isa Tetrahedron{N}
+end
+
+@test !isoperationtype(Tetrahedron)
