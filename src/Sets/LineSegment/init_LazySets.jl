cf42b45fc9222eb603154930d0b2dcb271061e79
schillic <git@christianschilling.net>
2024-07-30 10:56:02 +0200

    fix intersection of LineSegments

--- src/Sets/LineSegment/init_LazySets.jl
+++ src/Sets/LineSegment/init_LazySets.jl
@@ -1,3 +1,4 @@
+using .LazySets.Approximations: box_approximation
 using .LazySets.EmptySetModule: EmptySet
 using .LazySets.Line2DModule: Line2D
 using .LazySets.SingletonModule: Singleton
--- src/Sets/LineSegment/intersection.jl
+++ src/Sets/LineSegment/intersection.jl
@@ -34,24 +34,31 @@ function intersection(LS1::LineSegment, LS2::LineSegment)
     m = intersection(L1, L2)
     N = promote_type(eltype(LS1), eltype(LS2))
     if m == L1
-        # determine which segment is in both
-        p1 = max(min(LS1.p[1], LS1.q[1]), min(LS2.p[1], LS2.q[1]))
-        p2 = max(min(LS1.p[2], LS1.q[2]), min(LS2.p[2], LS2.q[2]))
-        q1 = min(max(LS1.p[1], LS1.q[1]), max(LS2.p[1], LS2.q[1]))
-        q2 = min(max(LS1.p[2], LS1.q[2]), max(LS2.p[2], LS2.q[2]))
-        if _isapprox(p1, q1) && _isapprox(p2, q2)
-            return Singleton([p1, p2])  # edges have a point in common
-
-        elseif _leq(p1, q1) && _leq(p2, q2)
-            return LineSegment([p1, p2], [q1, q2])
-
-        else
-            return EmptySet{N}(2)  # no intersection
+        # simple solution: compute the box approximation and find the right vertices (there are only 4)
+        B1 = box_approximation(LS1)
+        B2 = box_approximation(LS2)
+        B = intersection(B1, B2)
+        if isempty(B)
+            return B
         end
-
+        p = nothing
+        vlist = [LS1.p, LS1.q, LS2.p, LS2.q]
+        for v1 in vertices(B)
+            for v2 in vlist
+                if _isapprox(v1, v2)
+                    if isnothing(p)
+                        p = v1
+                    elseif _isapprox(v1, p)
+                        return Singleton(p)
+                    else
+                        return LineSegment(p, v1)
+                    end
+                end
+            end
+        end
+        error("this should not happen")
     elseif m isa Singleton && m.element ∈ LS1 && m.element ∈ LS2
         return m  # the intersection point between the lines is in the segments
-
     else
         return EmptySet{N}(2)  # no intersection
     end
