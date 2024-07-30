9ad333cabbe4201681617d6d493e24b89254c962
schillic <git@christianschilling.net>
2024-07-30 21:39:28 +0200

    fix case for empty intersection

--- src/Sets/LineSegment/intersection.jl
+++ src/Sets/LineSegment/intersection.jl
@@ -34,9 +34,23 @@ function intersection(LS1::LineSegment, LS2::LineSegment)
     m = intersection(L1, L2)
     N = promote_type(eltype(LS1), eltype(LS2))
     if m == L1
+        # line segments are on the same line
+
+        # check that the line segments intersect
+        @inbounds begin
+            # box approximation
+            l, r = LS1.p[1] < LS1.q[1] ? (LS1.p[1], LS1.q[1]) : (LS1.q[1], LS1.p[1])
+            b, t = LS1.p[2] < LS1.q[2] ? (LS1.p[2], LS1.q[2]) : (LS1.q[2], LS1.p[2])
+            # check that the other line segment has at least one point inside the box
+            if !(l <= LS2.p[1] <= r && b <= LS2.p[2] <= t) &&
+               !(l <= LS2.q[1] <= r && b <= LS2.q[2] <= t)
+                return EmptySet{N}(2)
+            end
+        end
+
         # find the middle two points of the four end points
         points = [LS1.p, LS1.q, LS2.p, LS2.q]
-        sorted_points = sort(points, by = p -> (p[1], p[2]))
+        sorted_points = sort(points; by=p -> (p[1], p[2]))
         @inbounds begin
             mid_point1 = sorted_points[2]
             mid_point2 = sorted_points[3]
