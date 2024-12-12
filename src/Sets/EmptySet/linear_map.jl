a20e15120f1fdc7ed442e2bf6ddd72545e021ce3
schillic <git@christianschilling.net>
2024-12-12 23:09:39 +0100

    EmptySet: promote_type in linear_map

--- src/Sets/EmptySet/linear_map.jl
+++ src/Sets/EmptySet/linear_map.jl
@@ -2,6 +2,6 @@ function linear_map(M::AbstractMatrix, ∅::EmptySet)
     @assert size(M, 2) == dim(∅) "cannot apply a $(size(M))-dimensional " *
                                  "matrix to a $(dim(∅))-dimensional set"
 
-    N = eltype(∅)
+    N = promote_type(eltype(M), eltype(∅))
     return EmptySet{N}(size(M, 1))
 end
