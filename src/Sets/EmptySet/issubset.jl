26c640ec229b9af4cef85f327071fcfa4a327645
schillic <git@christianschilling.net>
2024-12-13 23:47:20 +0100

    EmptySet: add '_issubset_emptyset2'

--- src/Sets/EmptySet/issubset.jl
+++ src/Sets/EmptySet/issubset.jl
@@ -7,3 +7,15 @@ function _issubset_emptyset(∅::EmptySet, X::LazySet, witness::Bool=false)
                              "but they are $(dim(∅)) and $(dim(X)), respectively"
     return _witness_result_empty(witness, true, ∅, X)
 end
+
+function _issubset_emptyset2(X::LazySet, ∅::EmptySet, witness::Bool=false)
+    @assert dim(∅) == dim(X) "the dimensions of the given sets should match, " *
+                             "but they are $(dim(∅)) and $(dim(X)), respectively"
+
+    result = isempty(X)
+    if !result && witness
+        return _witness_result_empty(witness, false, X, ∅, an_element(X))
+    else
+        return _witness_result_empty(witness, result, X, ∅)
+    end
+end
