fa56e22c7b09ccea0de944faad652214da26bfc3
schillic <git@christianschilling.net>
2024-12-15 15:50:15 +0100

    EmptySet: fix wrong assertion in cartesian_product

--- src/Sets/EmptySet/cartesian_product.jl
+++ src/Sets/EmptySet/cartesian_product.jl
@@ -3,8 +3,6 @@ function cartesian_product(∅₁::EmptySet, ∅₂::EmptySet)
 end
 
 function _cartesian_product_emptyset(∅::EmptySet, X::LazySet)
-    @assert dim(∅) == dim(X) "the dimensions of the given sets should match, " *
-                             "but they are $(dim(∅)) and $(dim(X)), respectively"
     N = promote_type(eltype(∅), eltype(X))
     return EmptySet{N}(dim(∅) + dim(X))
 end
