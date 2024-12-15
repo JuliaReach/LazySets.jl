7336907b9def3e38b0d38efcf963737b3d0b1da5
schillic <git@christianschilling.net>
2024-12-15 15:50:30 +0100

    EmptySet: add assertions

--- src/Sets/EmptySet/project.jl
+++ src/Sets/EmptySet/project.jl
@@ -1,3 +1,6 @@
-function project(::EmptySet{N}, block::AbstractVector{Int}; kwargs...) where {N}
+function project(∅::EmptySet{N}, block::AbstractVector{Int}; kwargs...) where {N}
+    @assert length(block) <= dim(∅) "incompatible dimensions $(length(block)) and $(dim(∅))"
+    @assert all(1 <= v <= dim(∅) for v in block) "invalid dimension in index vector"
+
     return EmptySet{N}(length(block))
 end
--- src/Sets/EmptySet/support_function.jl
+++ src/Sets/EmptySet/support_function.jl
@@ -1,3 +1,5 @@
-function ρ(::AbstractVector, ::EmptySet)
+function ρ(d::AbstractVector, ∅::EmptySet)
+    @assert length(d) == dim(∅) "incompatible dimensions $(length(d)) and $(dim(∅))"
+
     throw(ArgumentError("the support function of an empty set is undefined"))
 end
