a9537b7ae0e4a80a3704b7ffb36656cd9c71075d
schillic <git@christianschilling.net>
2024-12-29 22:59:03 +0100

    add assertions

--- src/Sets/Line/distance.jl
+++ src/Sets/Line/distance.jl
@@ -1,4 +1,6 @@
 @commutative function distance(x::AbstractVector, L::Line; p::Real=2)
+    @assert length(x) == dim(L) "incompatible dimensions $(length(x)) and $(dim(L))"
+
     d = L.d  # direction of the line
     t = dot(x - L.p, d) / dot(d, d)
     return distance(x, L.p + t * d; p=p)
