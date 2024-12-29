5b8743aee80dce477e1cea8e9440ec0554d4c855
schillic <git@christianschilling.net>
2024-12-29 22:45:11 +0100

    merge API's 'distance' with existing methods

--- src/Sets/HalfSpace/distance.jl
+++ src/Sets/HalfSpace/distance.jl
@@ -1,19 +1,8 @@
-"""
-    distance(x::AbstractVector, H::HalfSpace)
+@commutative function distance(x::AbstractVector, H::HalfSpace; p::Real=2)
+    if p != 2
+        throw(ArgumentError("`distance` is only implemented for Euclidean norm"))
+    end
 
-Compute the distance between point `x` and half-space `H` with respect to the
-Euclidean norm.
-
-### Input
-
-- `x` -- vector
-- `H` -- half-space
-
-### Output
-
-A scalar representing the distance between point `x` and half-space `H`.
-"""
-@commutative function distance(x::AbstractVector, H::HalfSpace)
     N = promote_type(eltype(x), eltype(H))
     a, b = _normalize_halfspace(H, N(2))
     return max(dot(x, a) - b, zero(N))
