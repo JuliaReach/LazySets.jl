99a467d1dd02e946d01b8f37962877dc5da73257
schillic <git@christianschilling.net>
2024-12-29 20:56:35 +0100

    EmptySet: add assertion to 'area'

--- src/Sets/EmptySet/area.jl
+++ src/Sets/EmptySet/area.jl
@@ -1,4 +1,7 @@
 function area(∅::EmptySet)
+    @assert dim(∅) == 2 "this function only applies to two-dimensional sets, " *
+                        "but the given set is $(dim(∅))-dimensional"
+
     N = eltype(∅)
     return zero(N)
 end
