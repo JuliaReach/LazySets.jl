7e292cf44044d64b44e4a661df81cf27bbf43059
schillic <git@christianschilling.net>
2025-01-21 20:38:11 +0100

    add 'p' kwarg to 'distance' methods

--- src/Sets/HalfSpace/distance.jl
+++ src/Sets/HalfSpace/distance.jl
@@ -1,6 +1,10 @@
-@commutative function distance(x::AbstractVector, H::HalfSpace)
+@commutative function distance(x::AbstractVector, H::HalfSpace; p::Real=2)
     @assert length(x) == dim(H) "a vector of length $(length(x)) is " *
                                 "incompatible with a set of dimension $(dim(H))"
+
+    if p != 2
+        throw(ArgumentError("this method is only implemented for the Euclidean norm"))
+    end
 
     N = promote_type(eltype(x), eltype(H))
     a, b = _normalize_halfspace(H, N(2))
