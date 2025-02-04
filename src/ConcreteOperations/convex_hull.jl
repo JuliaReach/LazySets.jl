ec669aa1074a49e5f0dbeb8e93e611fb4318d578
crisiumnih <fasfasmag@proton.me>
2025-02-05 04:29:11 +0530

    Fixed convex hull sorting by sanitizing

--- src/ConcreteOperations/convex_hull.jl
+++ src/ConcreteOperations/convex_hull.jl
@@ -197,7 +197,7 @@ function _two_points_2d!(points)
 end
 
 function _three_points_2d!(points::AbstractVector{<:AbstractVector{N}}) where {N}
-    # Algorithm: the function takes three points and uses the formula from here:
+    # Algorithmmonotone_chain!: the function takes three points and uses the formula from here:
     # https://stackoverflow.com/a/2122620
     # to decide if the points are ordered in counter-clockwise order. The result
     # is saved in the 'turn' flag and returned in ccw fashion acting according
@@ -458,7 +458,7 @@ function monotone_chain!(points::Vector{VN}; sort::Bool=true) where {N,VN<:Abstr
 
     if sort
         # sort the points lexicographically
-        sort!(points; by=x -> (x[1], x[2]))
+        sort!(points; by=x -> (x[1] == -0.0 ? 0.0 : x[1], x[2] == -0.0 ? 0.0 : x[2]))
     end
 
     # build lower hull
