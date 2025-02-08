6e9337b460229e71b82e8085d4d6199d9800fae5
schillic <git@christianschilling.net>
2025-02-08 09:47:40 +0100

    fix numeric type in 'cartesian_product' of Universe

--- src/Sets/Universe/cartesian_product.jl
+++ src/Sets/Universe/cartesian_product.jl
@@ -1,3 +1,4 @@
 function cartesian_product(U1::Universe, U2::Universe)
-    return Universe(dim(U1) + dim(U2))
+    N = promote_type(eltype(U1), eltype(U2))
+    return Universe{N}(dim(U1) + dim(U2))
 end
