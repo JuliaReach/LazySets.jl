0fc7662047c6bdb5a3d89416c716ed0ab8315926
schillic <git@christianschilling.net>
2025-02-11 07:55:36 +0100

    use 'cartesian_product' method from EmptySetModule

--- src/LazySets.jl
+++ src/LazySets.jl
@@ -121,11 +121,11 @@ include("Sets/Ellipsoid/EllipsoidModule.jl")
 
 include("Sets/EmptySet/EmptySetModule.jl")
 @reexport using ..EmptySetModule: EmptySet, ∅
-using ..EmptySetModule: _convex_hull_emptyset, _difference_emptyset, _difference_emptyset2,
-                        _distance_emptyset, _intersection_emptyset, _isdisjoint_emptyset,
-                        _issubset_emptyset, _issubset_emptyset2, _linear_combination_emptyset,
-                        _minkowski_difference_emptyset, _minkowski_difference_emptyset2,
-                        _minkowski_sum_emptyset
+using ..EmptySetModule: _cartesian_product_emptyset, _convex_hull_emptyset, _difference_emptyset,
+                        _difference_emptyset2, _distance_emptyset, _intersection_emptyset,
+                        _isdisjoint_emptyset, _issubset_emptyset, _issubset_emptyset2,
+                        _linear_combination_emptyset, _minkowski_difference_emptyset,
+                        _minkowski_difference_emptyset2, _minkowski_sum_emptyset
 
 include("Sets/HParallelotope/HParallelotopeModule.jl")
 @reexport using ..HParallelotopeModule: HParallelotope,
