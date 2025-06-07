a105cc07baac2b91d0f46455ffce0d04f35b6541
schillic <git@christianschilling.net>
2025-06-07 23:04:18 +0200

    reorder lines

--- src/Sets/ZonotopeMD/cartesian_product.jl
+++ src/Sets/ZonotopeMD/cartesian_product.jl
@@ -14,7 +14,7 @@ A new `ZonotopeMD` representing the Cartesian product `Z1 × Z2`.
 """
 function cartesian_product(Z1::ZonotopeMD, Z2::ZonotopeMD)
     c = vcat(Z1.center, Z2.center)
-    d = vcat(Z1.d, Z2.d)
     M = blockdiag(sparse(Z1.M), sparse(Z2.M))
+    d = vcat(Z1.d, Z2.d)
     return ZonotopeMD(c, M, d)
 end
