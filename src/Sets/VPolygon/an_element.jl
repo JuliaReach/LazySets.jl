3d36a659a08b8f30c36d4626127ac25fccd9aaab
schillic <git@christianschilling.net>
2025-06-21 09:55:12 +0200

    remove useless documentation

--- src/Sets/VPolygon/an_element.jl
+++ src/Sets/VPolygon/an_element.jl
@@ -1,12 +1,3 @@
-"""
-# Extended help
-
-    an_element(P::VPolygon)
-
-### Output
-
-The first vertex of the polygon in vertex representation.
-"""
 function an_element(P::VPolygon)
     @assert !isempty(P.vertices) "the polygon has no vertices"
     return P.vertices[1]
