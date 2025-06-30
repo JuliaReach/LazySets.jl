ab4d30f283dcaf5a4f48cde2b7ba2d2550d0d9a1
schillic <git@christianschilling.net>
2025-06-30 08:41:58 +0200

    tweak 'permute' and 'project' for Interval

--- src/Sets/Interval/permute.jl
+++ src/Sets/Interval/permute.jl
@@ -1,4 +1,4 @@
 function permute(X::Interval, p::AbstractVector{Int})
-    @assert length(p) == 1 && p[1] == 1 "invalid permutation vector $p"
+    @assert length(p) == 1 && (@inbounds p[1]) == 1 "invalid permutation vector $p for Interval"
     return X
 end
