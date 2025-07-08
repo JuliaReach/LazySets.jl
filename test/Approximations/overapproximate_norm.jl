29ec7ad895d76776acd5757e3e31230b80d33ab9
alecarraro <a.carraro@student.tudelft.nl>
2025-07-08 17:44:12 +0200

    Applied suggestions from code review and fixed indentation

--- test/Approximations/overapproximate_norm.jl
+++ test/Approximations/overapproximate_norm.jl
@@ -2,11 +2,11 @@ using LazySets, Test
 using LazySets.Approximations: overapproximate_norm
 
 let
-	for _ in 1:10
-		Z = rand(Zonotope; dim = 10, num_generators = 10)
-		@test overapproximate_norm(Z, 1) ≥ norm(Z, 1)
-	end
+    for _ in 1:5
+        Z = rand(Zonotope; dim=8, num_generators=5)
+        @test overapproximate_norm(Z, 1) ≥ norm(Z, 1)
+    end
 
-	Z = Zonotope([1., -1.], zeros(Float64, 2, 2))
-	@test norm(Z, 1) == 2
+    Z = Zonotope([1.0, -1.0], zeros(Float64, 2, 0))
+    @test overapproximate_norm(Z, 1) == 2
 end
