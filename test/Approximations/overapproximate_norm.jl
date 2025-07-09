37ed2e971ba4f61be718d9bbe3194aaee90b1e12
Alessandro Carraro <118743744+alecarraro@users.noreply.github.com>
2025-07-09 14:44:45 +0200

    Update test/Approximations/overapproximate_norm.jl
    Co-authored-by: Christian Schilling <git@christianschilling.net>

--- test/Approximations/overapproximate_norm.jl
+++ test/Approximations/overapproximate_norm.jl
@@ -1,12 +1,12 @@
 using LazySets, Test
 using LazySets.Approximations: overapproximate_norm
 
-let
+for N in [Float64, Float32, Rational{Int}]
     for _ in 1:5
-        Z = rand(Zonotope; dim=8, num_generators=5)
+        Z = rand(Zonotope; N=N, dim=8, num_generators=5)
         @test overapproximate_norm(Z, 1) ≥ norm(Z, 1)
     end
 
-    Z = Zonotope([1.0, -1.0], zeros(Float64, 2, 0))
+    Z = Zonotope(N[1, -1], zeros(N, 2, 0))
     @test overapproximate_norm(Z, 1) == 2
 end
