400cc01e08b9c19b6a99ec748d90e84fb659faf1
schillic <git@christianschilling.net>
2025-07-12 08:05:56 +0200

    add 'validate' to 'exact_sum'

--- src/ConcreteOperations/exact_sum.jl
+++ src/ConcreteOperations/exact_sum.jl
@@ -1,5 +1,5 @@
 # for non-parametric set types, the exact sum coincides with the Minkowski sum
-function exact_sum(X::LazySet, Y::LazySet)
+@validate function exact_sum(X::LazySet, Y::LazySet)
     @assert dim(X) == dim(Y) "the dimensions of the given sets should match, " *
                              "but they are $(dim(X)) and $(dim(Y)), respectively"
 
