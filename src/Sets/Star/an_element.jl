0707635e544d471559a5ee5787c01386419d4929
schillic <git@christianschilling.net>
2025-07-19 12:23:31 +0200

    let 'validate' check nonempty set for 'an_element'

--- src/Sets/Star/an_element.jl
+++ src/Sets/Star/an_element.jl
@@ -7,6 +7,6 @@
 
 We apply the affine map to the result of `an_element` on the predicate.
 """
-function an_element(X::Star)
+@validate function an_element(X::Star)
     return basis(X) * an_element(predicate(X)) + center(X)
 end
