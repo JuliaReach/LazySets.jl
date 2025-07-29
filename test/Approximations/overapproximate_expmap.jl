0a60e83d30b70c153fabaafc51ba33b4896117fb
Alessandro Carraro <118743744+alecarraro@users.noreply.github.com>
2025-07-29 21:55:22 +0200

    Update overapproximate_expmap.jl

--- test/Approximations/overapproximate_expmap.jl
+++ test/Approximations/overapproximate_expmap.jl
@@ -2,9 +2,8 @@
 using Test, LazySets, LinearAlgebra
 using IntervalMatrices: IntervalMatrix
 using RangeEnclosures
-using IntervalArithmetic: Interval
 
-for N in [Float32, Float64, Rational{Int}]
+for N in @tN([Float32, Float64, Rational{Int}])
     MZ = MatrixZonotope(N[1 1; -1 1], [N[1 0; 1 2]])
     P = SparsePolynomialZonotope(N[1, -1], N[1 1; 0 -1], hcat(N[0, 1]), [2 1; 0 1; 1 0], [1, 2, 3])
 
@@ -14,6 +13,10 @@ for N in [Float32, Float64, Rational{Int}]
     Pex = overapproximate(em, SparsePolynomialZonotope, 2)
     @test isa(Pex, SparsePolynomialZonotope)
     @test size(center(Pex), 1) == size(center(P), 1)
+end
+
+for N in @tN([Float32, Float64])
+    P = SparsePolynomialZonotope(N[1, -1], N[1 1; 0 -1], hcat(N[0, 1]), [2 1; 0 1; 1 0], [1, 2, 3])
 
     # expectation: exp(0)*P = I*P = P
     ZM = MatrixZonotope(zeros(N, 2, 2), Vector{Matrix{N}}())
@@ -24,10 +27,6 @@ for N in [Float32, Float64, Rational{Int}]
     @test genmat_dep(Pex) == genmat_dep(P)
     @test genmat_indep(Pex) == genmat_indep(P)
     @test expmat(Pex) == expmat(P)
-end
-
-for N in [Float32, Float64]
-    P = SparsePolynomialZonotope(N[1, -1], N[1 1; 0 -1], hcat(N[0, 1]), [2 1; 0 1; 1 0], [1, 2, 3])
 
     MZ = MatrixZonotope(N[1 -1; -1 2], [N[1.001 -0.999; -0.999 2.005]])
     mzexp =MatrixZonotopeExp(MZ)
