be30f34d684996f6b0dc8d1849c956a65ff6bea9
schillic <git@christianschilling.net>
2025-08-22 17:22:55 +0200

    revise tests for Zonotope

--- test/Sets/Zonotope.jl
+++ test/Sets/Zonotope.jl
@@ -6,25 +6,318 @@ if !isdefined(@__MODULE__, Symbol("@tN"))
     end
 end
 
+function isidentical(::Zonotope, ::Zonotope)
+    return false
+end
+
+function isidentical(Z1::Zonotope{N}, Z2::Zonotope{N}) where {N}
+    return Z1.center == Z2.center && Z1.generators == Z2.generators
+end
+
 for N in @tN([Float64, Float32, Rational{Int}])
+    # auxiliary sets
+    # TODO
+
+    # constructor
+    c = N[1, 2]
+    @test_throws AssertionError Zonotope(c, [N[0 0]; N[1 // 2 0]; N[0 1 // 2]; N[0 0]])
+    Z = Zonotope(c, N[1 3; 2 4])
+    Z0 = Zonotope(c, zeros(N, 2, 0))  # zonotope with no generators
+    Z1 = Zonotope(N[1], hcat(N[1]))  # 1D zonotope
+    Z3 = Zonotope(N[1, 1, 1], N[1 0 0; 0 2 0; 0 0 3])  # 3D zonotope, a hyperrectangle
     # constructor from list of generators
-    Z = Zonotope(N[1, 1], [N[1, 2], N[3, 4]])
-    @test Z isa Zonotope{N} && Z == Zonotope(N[1, 1], N[1 3; 2 4])
+    Z2 = Zonotope(c, [N[1, 2], N[3, 4]])
+    @test isidentical(Z2, Z)
 
-    # Test dimension assertion
-    @test_throws AssertionError Zonotope(N[1, 1], [N[0 0]; N[1 // 2 0]; N[0 1 // 2]; N[0 0]])
+    # convert
+    # from AbstractZonotope
+    Z2 = convert(Zonotope, HParallelotope(N[2 -1; 2 -2], N[1, 2, 1, 1]))
+    @test isidentical(Z2, Zonotope(N[-1 // 4, -1 // 2], N[1 -3//4; 1 -3//2]))
+    # from AbstractHyperrectangle
+    Z2 = convert(Zonotope, Hyperrectangle(c, N[4, 5]))
+    @test isidentical(Z2, Zonotope(c, N[4 0; 0 5]))
+    Z2 = convert(Zonotope, Hyperrectangle(c, N[0, 0]))  # flat hyperrectangle
+    @test isidentical(Z2, Zonotope(c, zeros(N, 2, 0)))
+    # TODO static hyperrectangle
+    # from CartesianProduct of AbstractHyperrectangles
+    # TODO
+    # from CartesianProductArray of AbstractHyperrectangles
+    # TODO
+    # from CartesianProduct of AbstractZonotopes
+    # TODO
+    # from CartesianProductArray of AbstractZonotopes
+    # TODO
+    # from LinearMap of AbstractZonotope
+    # TODO
+    # from LinearMap of CartesianProduct of AbstractHyperrectangles
+    # TODO
+    # from LinearMap of CartesianProductArray of AbstractHyperrectangles
+    # TODO
+    # from AbstractAffineMap of AbstractZonotope
+    # TODO
 
-    # 1D Zonotope
-    z = Zonotope(N[0], Matrix{N}(I, 1, 1))
-    # Test Dimension
-    @test dim(z) == 1
-    # support function
-    @test ρ(N[1], z) == ρ(N[-1], z) == N(1)
-    # Test Support Vector
-    d = N[1]
-    @test σ(d, z) == N[1]
-    d = N[-1]
-    @test σ(d, z) == N[-1]
+    # an_element
+    x = an_element(Z)
+    @test x isa Vector{N} && x ∈ Z
+
+    # area
+    @test_throws DimensionMismatch area(Z1)
+    res = area(Z)
+    @test res isa N && res == N(8)
+    @test_broken area(Z3)  # TODO this should work
+
+    # chebyshev_center_radius
+    @static if isdefined(@__MODULE__, :Polyhedra)
+        c, r = chebyshev_center_radius(Z)
+        @test c isa Vector{N} && c == Z.center
+        @test r isa N && n == N(TODO)
+    end
+
+    # complement
+    if isdefined(@__MODULE__, :Polyhedra) || N <: AbstractFloat
+        U = complement(Z3)
+        @test U isa UnionSetArray{N} && dim(U) == 3 &&
+              ispermutation(array(U),
+                            [HalfSpace(N[1, 0, 0], N(0)), HalfSpace(N[-1, 0, 0], N(-2)),
+                             HalfSpace(N[0, 1, 0], N(-1)), HalfSpace(N[0, -1, 0], N(-3)),
+                             HalfSpace(N[0, 0, 1], N(-2)), HalfSpace(N[0, 0, -1], N(-4))])
+    end
+
+    # concretize
+    Z2 = concretize(Z)
+    @test isidentical(Z, Z2)
+
+    # constrained_dimensions
+    @test constrained_dimensions(Z) == 1:2
+
+    if isdefined(@__MODULE__, :Polyhedra) || N <: AbstractFloat
+        # constraints_list
+        cs = constraints_list(Z3)
+        @test ispermutation(cs,
+                            [HalfSpace(N[1, 0, 0], N(2)), HalfSpace(N[-1, 0, 0], N(0)),
+                             HalfSpace(N[0, 1, 0], N(3)), HalfSpace(N[0, -1, 0], N(1)),
+                             HalfSpace(N[0, 0, 1], N(4)), HalfSpace(N[0, 0, -1], N(2))])
+        # sparse matrix (#1468)
+        Z2 = Zonotope(N[0], sparse(hcat(N[1])))
+        res = constraints_list(Z2)
+        @test res isa Vector{<:HalfSpace{N}} &&
+              ispermutation(res, [HalfSpace(N[1], N(1)), HalfSpace(N[-1], N(1))])
+
+        # constraints
+        cs2 = collect(constraints(Z3))
+        @test ispermutation(cs2, cs)
+    end
+
+    # convex_hull (unary)
+    Z2 = convex_hull(Z)
+    @test isidentical(Z, Z2)
+
+    # copy
+    Z2 = copy(Z)
+    @test isidentical(Z, Z2)
+
+    # diameter
+    @test_throws ArgumentError diameter(Z, N(1 // 2))
+    for res in (diameter(Z), diameter(Z, Inf))
+        @test res isa N && res == N(12)
+    end
+    @test_broken diameter(Z, 2)  # TODO this should work
+
+    # dim
+    @test dim(Z) == 2
+    @test dim(Z3) == 3
+
+    # eltype
+    @test eltype(Z) == N
+    @test eltype(typeof(Z)) == N
+
+    # extrema
+    res = extrema(Z)
+    @test res isa Tuple{Vector{N},Vector{N}} && res[1] == N[-3, -4] && res[2] == N[5, 8]
+    @test_throws DimensionMismatch extrema(Z, 3)
+    res = extrema(Z, 1)
+    @test res isa Tuple{N,N} && res[1] == N(-3) && res[2] == N(5)
+
+    # generators
+    @test collect(generators(Z)) == [N[1, 2], N[3, 4]]
+    # degenerate case
+    gens = collect(generators(Z0))
+    @test gens isa AbstractVector{<:AbstractVector{N}} && isempty(gens)
+
+    # genmat
+    @test genmat(Z) == Z.generators
+    # degenerate case
+    gens = genmat(Z0)
+    @test gens isa Matrix{N} && gens == Matrix{N}(undef, 2, 0)
+
+    # high
+    res = high(Z)
+    @test res isa Vector{N} && res == N[5, 8]
+    @test_throws DimensionMismatch high(Z, 3)
+    res = high(Z, 1)
+    @test res isa N && res == N(5)
+
+    # isbounded
+    @test isbounded(Z)
+
+    # isboundedtype
+    @test isboundedtype(typeof(Z))
+
+    # isconvextype
+    @test isconvextype(typeof(Z))
+
+    # isempty
+    @test !isempty(Z)
+    res, w = isempty(Z, true)
+    @test !res && w isa Vector{N} && w ∈ Z
+
+    # isoperation
+    @test !isoperation(Z)
+
+    # isoperationtype
+    @test !isoperationtype(typeof(Z))
+
+    # ispolyhedral
+    @test ispolyhedral(Z)
+
+    # isuniversal
+    @test !isuniversal(Z)
+    res, w = isuniversal(Z, true)
+    @test !res && w isa Vector{N} && w ∉ Z
+
+    # low
+    res = low(Z)
+    @test res isa Vector{N} && res == N[-3, -4]
+    @test_throws DimensionMismatch low(Z, 3)
+    res = low(Z, 1)
+    @test res isa N && res == N(-3)
+
+    # ngens
+    @test ngens(Z) == 2
+    # degenerate case
+    @test ngens(Z0) == 0
+
+    # norm
+    @test_throws ArgumentError norm(Z, N(1 // 2))
+    for res in (norm(Z), norm(Z, Inf))
+        @test res isa N && res == N(8)
+    end
+    res = norm(Z, 2)
+    @test res isa (N <: AbstractFloat ? N : Float64)
+    @test res == norm(N[5, 8], 2)
+
+    # polyhedron
+    @static if isdefined(@__MODULE__, :Polyhedra)
+        Y = polyhedron(X)
+        # TODO
+        @test Y isa Polyhedra.Interval{N} && ispermutation(Y.vrep.points.points, [[N(0)], [N(2)]])
+    end
+
+    # radius
+    @test_throws ArgumentError radius(Z, N(1 // 2))
+    for res in (radius(Z), radius(Z, Inf))
+        @test res isa N && res == N(6)
+    end
+    @test_broken radius(Z, 2)  # TODO this should work
+
+    # rectify
+    X = rectify(Z)
+    @test X isa UnionSetArray{N} && length(X) == 4
+    # no precise test here; the `rectify` implementation is tested elsewhere
+
+    # reflect
+    Z2 = reflect(Z)
+    @test isequivalent(Z2, Zonotope(N[-1, -2], genmat(Z)))
+
+    # singleton_list
+    res = singleton_list(Z)
+    @test res isa Vector{Singleton{N,Vector{N}}}
+    @test ispermutation(res,
+                        [Singleton(N[3, 4]), Singleton(N[5, 8]),
+                         Singleton(N[-1, 0]), Singleton(N[-3, -4])])
+
+    # tosimplehrep
+    A, b = tosimplehrep(Z)
+    @test A isa Matrix{N} && b isa Vector{N} && size(A) == (4, 2) && length(b) == 4
+    # no precise test here; the `tosimplehrep` implementation is tested elsewhere
+
+    # triangulate
+    @static if isdefined(@__MODULE__, :MiniQhull)
+        # TODO
+        Y = triangulate(Z)
+        @test Y isa UnionSetArray{N} && length(Y) == 2
+        Y = Y[1]
+        @test Y isa VPolytope{N}
+        res = vertices_list(Y)
+        @test ispermutation(res, [N[0], N[2]])
+    end
+
+    # triangulate_faces
+    @test_throws DimensionMismatch triangulate_faces(Z)
+    @static if isdefined(@__MODULE__, :MiniQhull)
+        # TODO
+        @test_throws ArgumentError triangulate_faces(Z3)
+    end
+
+    # vertices_list
+    res = vertices_list(Z)
+    @test res isa Vector{Vector{N}} && ispermutation(res, [N[3, 4], N[5, 8], N[-1, 0], N[-3, -4]])
+    # degenerate case (#1881)
+    res = vertices_list(Z0)
+    @test res isa Vector{Vector{N}} && res == [N[1, 2]]
+    # zero generators are ignored (#2147)
+    Z2 = Zonotope(zeros(N, 2), N[1 0; 0 0])
+    @test ispermutation(vertices_list(Z2), [N[1, 0], N[-1, 0]])
+    # redundant vertices are removed automatically (#1021)
+    Z2 = Zonotope(N[0, 0], N[1 0 1; 0 1 1])
+    vlistZ = vertices_list(Z2)
+    @test length(vlistZ) == 6
+    @test ispermutation(vlistZ, [N[-2, -2], N[0, -2], N[2, 0], N[2, 2], N[0, 2], N[-2, 0]])
+    c, G = Z2.center, Z2.generators
+    vlist2 = LazySets._vertices_list_2D(c, G; apply_convex_hull=true)
+    @test ispermutation(vlistZ, vlist2)
+    vlist2 = LazySets._vertices_list_2D(c, G; apply_convex_hull=false)
+    @test ispermutation(vlistZ, vlist2)
+    # option to not apply the convex-hull operation
+    vlistZ = LazySets._vertices_list_zonotope_iterative(Z2.center, Z2.generators;
+                                                        apply_convex_hull=false)
+    @test length(vlistZ) == 8
+    @test ispermutation(convex_hull(vlistZ),
+                        [N[-2, -2], N[0, -2], N[2, 0], N[2, 2], N[0, 2], N[-2, 0]])
+    # generators in positive resp. negative orthant (i.e., all numbers are positive resp. negative)
+    Z2 = Zonotope(N[0, 0], N[1 0 1; 0 1 1])
+    for i in 1:2
+        if i == 2
+            Z2.generators .*= -1
+        end
+        vlistZ = vertices_list(Z2)
+        if i == 1 || N <: AbstractFloat
+            @test ispermutation(vlistZ, [N[2, 0], N[2, 2], N[0, 2], N[-2, 0], N[-2, -2], N[0, -2]])
+        else
+            @test_broken ispermutation(vlistZ,  # TODO this should be fixed
+                                       [N[2, 0], N[2, 2], N[0, 2], N[-2, 0], N[-2, -2], N[0, -2]])
+        end
+    end
+    # 3D
+    @static if isdefined(@__MODULE__, :Polyhedra)
+        vlistZ = vertices_list(Z3)
+        @test ispermutation(vlistZ,
+                            [N[2, 3, 4], N[2, 3, -2], N[2, -1, 4], N[2, -1, -2],
+                             N[0, 3, 4], N[0, 3, -2], N[0, -1, 4], N[0, -1, -2]])
+    end
+
+    # vertices
+    res = collect(vertices(Z))
+    @test res isa Vector{Vector{N}} && ispermutation(res, vertices_list(Z))
+
+    # volume
+    @static if isdefined(@__MODULE__, :MiniQhull)
+        @test volume(Z) == N(8)  # TODO this should work without Polyhedra
+    end
+end
+
+for N in @tN([Float64, Float32, Rational{Int}])
+    # TODO revise this loop and merge with above loop
 
     # 2D Zonotope
     z = Zonotope(N[0, 0], Matrix{N}(I, 2, 2))
@@ -64,22 +357,6 @@ for N in @tN([Float64, Float32, Rational{Int}])
     @test size(z.generators) == (2, 5)
     zred = remove_zero_generators(z)
     @test size(zred.generators) == (2, 2)
-
-    # boundedness
-    @test isbounded(z)
-
-    # ispolyhedral
-    @test ispolyhedral(z)
-
-    # isempty
-    @test !isempty(z)
-
-    # isuniversal
-    answer, w = isuniversal(z, true)
-    @test !isuniversal(z) && !answer && w ∉ z
-
-    # an_element function
-    @test an_element(z) ∈ z
 
     # membership
     Z = Zonotope(N[1, 2], N[2 1; 1 2])
@@ -197,20 +474,6 @@ for N in @tN([Float64, Float32, Rational{Int}])
     Z = convert(Zonotope, A * B + b)
     @test Z == Zonotope(N[1, 1], N[1 0; 0 1])
 
-    # test conversion from hyperrectangular sets
-    Z = convert(Zonotope, Hyperrectangle(N[2, 3], N[4, 5]))
-    @test Z.center == N[2, 3] && diag(Z.generators) == N[4, 5]
-    convert(Zonotope, BallInf(N[5, 3], N(2)))
-    # flat hyperrectangle
-    Z = convert(Zonotope, Hyperrectangle(N[2, 3], N[0, 0]))
-    @test Z.center == N[2, 3] && isempty(Z.generators)
-    # conversion back to hyperrectangle
-    H = Hyperrectangle(N[2, 3], N[4, 5])
-    Z = convert(Zonotope, H)
-    @test convert(Hyperrectangle, Z) == H
-    Z = Zonotope(N[1, 1], N[2 1; 1 2])
-    @test_throws AssertionError convert(Hyperrectangle, Z)
-
     # convert the cartesian product of two hyperrectangles to a zonotope
     h1 = Hyperrectangle(N[1 / 2], N[1 / 2])
     h2 = Hyperrectangle(N[5 // 2, 9 // 2], N[1 / 2, 1 / 2])
@@ -309,44 +572,24 @@ for N in @tN([Float64, Float32, Rational{Int}])
     @test center(Z) == center(Z2)
     @test genmat(Z2) == N[0 1 0]'
 
-    # low/high
-    Z = Zonotope(N[1, 1], N[1 2 -3; -2 -3 4])
-    @test low(Z, 1) == -5 && low(Z, 2) == -8
-    @test high(Z, 1) == 7 && high(Z, 2) == 10
-
-    # reflect
-    Z = Zonotope(N[1, -2], N[2 0; -1//2 1//2])
-    @test reflect(Z) == Zonotope(N[-1, 2], N[2 0; -1//2 1//2])
-
     # permute
     Z = Zonotope(N[-1, -2], N[1 2 3; -4 -5 -6])
     @test permute(Z, [1, 2]) == Z
     @test permute(Z, [2, 1]) == Zonotope(N[-2, -1], N[-4 -5 -6; 1 2 3])
-
-    # norm
-    Z = Zonotope(N[1, 2], N[2 1 -2; 1 2 0])
-    @test norm(Z, 1) == 11
-    @test norm(scale(2.0, Z), 1) == 22.0
-
-    Z2 = Zonotope(N[1, -1], zeros(N, 2, 2))
-    @test norm(Z2, 1) == 2
-
-    Z3 = Zonotope(N[1, -1], N[-2 1; 0 -1])
-    @test norm(Z3, 1) == 6
 end
 
 for N in @tN([Float64, Float32])
     # rand
-    @test rand(Zonotope; N=N) isa Zonotope{N}
+    Z2 = rand(Zonotope; N=N)
+    @test Z2 isa Zonotope{N} && dim(Z2) == 2
+    Z2 = rand(Zonotope; N=N, dim=3)
+    @test Z2 isa Zonotope{N} && dim(Z2) == 3
+    Z2 = rand(Zonotope; N=N, num_generators=5)
+    @test Z2 isa Zonotope{N} && dim(Z2) == 2 && ngens(Z2) == 5
 end
 
 for N in [Float64]
-    # an_element function
-    g = zeros(N, 2, 5)
-    g[:, 3] = ones(N, 2)
-    g[1, 2] = N(2)
-    z = Zonotope(N[1, 2], g)
-    @test an_element(z) ∈ z
+    # TODO revise this loop
 
     # conversion to HPolytope
     # 1D
@@ -361,76 +604,6 @@ for N in [Float64]
     for d in BoxDiagDirections{N}(2)
         @test ρ(d, P) == ρ(d, Z)
     end
-    # sparse matrix (#1468)
-    constraints_list(Zonotope(N[0, 0], sparse(N[1 0; 0 1])))
-
-    @static if isdefined(@__MODULE__, :Polyhedra)
-        # constraints_list for generator matrix with a zero row
-        Z = Zonotope(N[0, 0], N[2 3; 0 0])
-        P = tovrep(HPolygon(constraints_list(Z)))
-        @test ispermutation(vertices_list(P), [N[5, 0], [-5, 0]])
-
-        # test that zero generators are ignored (#2147)
-        G = spzeros(N, 100, 100)
-        G[1, 1] = N(1)
-        Z = Zonotope(zeros(N, 100), G)
-        v1 = zeros(N, 100)
-        v1[1] = N(1)
-        v2 = zeros(N, 100)
-        v2[1] = N(-1)
-        @test ispermutation(vertices_list(Z), [v1, v2])
-    end
-
-    # vertices for singleton zonotope (#1881)
-    S = Singleton(N[0, 0])
-    Z = convert(Zonotope, S)
-    @test vertices_list(Z) == [element(S)]
-
-    # test that redundant vertices are removed by default (#1021)
-    Z = Zonotope([0.0, 0.0], [1.0 0.0 1.0; 0.0 1.0 1.0])
-    vlistZ = vertices_list(Z)
-    @test length(vlistZ) == 6
-    @test ispermutation(vlistZ, [N[-2, -2], N[0, -2], N[2, 0], N[2, 2], N[0, 2], N[-2, 0]])
-    c, G = Z.center, Z.generators
-    vlist2 = LazySets._vertices_list_2D(c, G; apply_convex_hull=true)
-    @test ispermutation(vlistZ, vlist2)
-    vlist3 = LazySets._vertices_list_2D(c, G; apply_convex_hull=false)
-    @test ispermutation(vlistZ, vlist3)
-
-    @static if isdefined(@__MODULE__, :Polyhedra)
-        # test 3d zonotope vertex enumeration
-        Z = Zonotope([0.0, 0.0, 0.0], [1.0 0.0 1.0; 0.0 1.0 1.0; 0.1 1.0 1.0])
-        vlistZ = vertices_list(Z)
-        @test length(vlistZ) == 8
-    end
-
-    # test 2d zonotope generators in positive orthant vertex enumeration
-    Z = Zonotope([0.0, 0.0], [1.0 0.0 1.0; 0.0 1.0 1.0])
-    vlistZ = vertices_list(Z)
-    @test length(vlistZ) == 6
-    # redundant vertices are removed automatically
-    Z = Zonotope(N[1, 3], N[0 0 1; 0 0 0])
-    vlistZ = vertices_list(Z)
-    @test length(vlistZ) == 2
-
-    # test 2d zonotope generators in negative orthant vertex enumeration
-    Z = Zonotope([0.0, 0.0], -[1.0 0.0 1.0; 0.0 1.0 1.0])
-    vlistZ = vertices_list(Z)
-    @test length(vlistZ) == 6
-
-    # option to not apply the convex hull operation
-    vlistZ = LazySets._vertices_list_zonotope_iterative(Z.center, Z.generators;
-                                                        apply_convex_hull=false)
-    @test length(vlistZ) == 8
-    @test ispermutation(convex_hull(vlistZ),
-                        [N[-2, -2], N[0, -2], N[2, 0], N[2, 2], N[0, 2], N[-2, 0]])
-
-    # constraints_list correctness check by subset check (requires LP solver)
-    B = BallInf(zeros(N, 3), N(1))
-    Z = convert(Zonotope, B)
-    clist = constraints_list(Z)
-    H = HPolytope(clist)
-    @test H ⊆ B && B ⊆ H
 
     gens = N[1 1; -1 1]
     Z1 = Zonotope(N[1, 1], gens)
@@ -484,6 +657,3 @@ for N in [Float64]
                    HalfSpace(N[0, -1], N(1))])
     @test isequivalent(intersection(H, Z), P)
 end
-
-# isoperationtype
-@test !isoperationtype(Zonotope)
