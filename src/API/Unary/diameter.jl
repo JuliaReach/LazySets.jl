5e08501ab7eb7c30d146dcd5d477016047b6a80c
schillic <git@christianschilling.net>
2025-10-26 12:09:08 +0100

    fix docs for 'diameter'

--- src/API/Unary/diameter.jl
+++ src/API/Unary/diameter.jl
@@ -14,8 +14,7 @@ A real number representing the diameter.
 
 ### Notes
 
-The diameter of a set is the maximum distance between any two elements of the
-set, or, equivalently, the diameter of the enclosing ball (of the given
+The diameter of a set is the diameter of the enclosing ball (of the given
 ``p``-norm) of minimal volume.
 """
 function diameter(::LazySet, ::Real=Inf) end
