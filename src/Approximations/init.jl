10eaeb57d8dfc5f6af1a6d6efd3ce905b2332d48
schillic <git@christianschilling.net>
2025-11-16 17:15:03 +0100

    remove method for 'IntervalBox'es

--- src/Approximations/init.jl
+++ src/Approximations/init.jl
@@ -1,7 +1,6 @@
 function __init__()
     @require Expokit = "a1e7a1ef-7a5d-5822-a38c-be74e1bb89f4" (nothing,)
     @require ExponentialUtilities = "d4d017d3-3776-5f7e-afef-a10c40355c18" (nothing,)
-    @require IntervalBoxes = "43d83c95-ebbb-40ec-8188-24586a1458ed" include("init_IntervalBoxes.jl")
     @require IntervalConstraintProgramming = "138f1668-1576-5ad7-91b9-7425abbf3153" include("init_IntervalConstraintProgramming.jl")
     @require IntervalMatrices = "5c1f47dc-42dd-5697-8aaa-4d102d140ba9" include("init_IntervalMatrices.jl")
     @require Ipopt = "b6b21f68-93f8-5de0-b562-5493be1d77c9" include("init_Ipopt.jl")
