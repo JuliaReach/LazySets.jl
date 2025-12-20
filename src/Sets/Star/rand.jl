23512676ad74e29ca7b9fed5d5a57afd2003486d
schillic <git@christianschilling.net>
2025-12-20 12:33:34 +0100

    let 'rand' of 'Star' create 'HPolyhedron' predicates

--- src/Sets/Star/rand.jl
+++ src/Sets/Star/rand.jl
@@ -7,7 +7,7 @@
 ### Algorithm
 
 A predicate `P` can be passed directly. If `P` is `nothing` (default), we
-generate a random `HPolytope` of dimension `dim`.
+generate a random `HPolyhedron` of dimension `dim`.
 
 All numbers are normally distributed with mean 0 and standard deviation 1.
 """
@@ -20,7 +20,7 @@ function rand(::Type{Star};
     rng = reseed!(rng, seed)
     c = randn(rng, N, dim)
     if isnothing(P)
-        P = rand(HPolytope; N=N, dim=dim, rng=rng, seed=seed)
+        P = rand(HPolyhedron; N=N, dim=dim, rng=rng, seed=seed)
     end
     V = randn(rng, N, dim, StarModule.dim(P))
     return Star(c, V, P)
