2d7fd5fb3031d7cb4e33b5f0ef8b908378c182fa
schillic <git@christianschilling.net>
2026-01-01 20:35:50 +0100

    fix code for Symbolics v7

--- src/Sets/HalfSpace/HalfSpace.jl
+++ src/Sets/HalfSpace/HalfSpace.jl
@@ -126,11 +126,11 @@ function load_Symbolics_HalfSpace()
             end
 
             # compute the linear coefficients by taking first-order derivatives
-            coeffs = [N(α.val) for α in Symbolics.gradient(sexpr, collect(vars))]
+            coeffs = [N(Symbolics.value(α)) for α in Symbolics.gradient(sexpr, collect(vars))]
 
             # get the constant term by expression substitution
             zeroed_vars = Dict(v => zero(N) for v in vars)
-            β = -N(Symbolics.substitute(sexpr, zeroed_vars))
+            β = -N(Symbolics.value(Symbolics.substitute(sexpr, zeroed_vars)))
 
             return HalfSpace(coeffs, β)
         end
