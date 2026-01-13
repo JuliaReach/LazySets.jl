29873444cafb9376978813988e17d7664026eee9
schillic <git@christianschilling.net>
2026-01-13 20:12:36 +0100

    make StaticArraysCore a soft dependency

--- src/Sets/BallInf/genmat.jl
+++ src/Sets/BallInf/genmat.jl
@@ -1,16 +1,16 @@
 function load_StaticArraysCore_genmat()
     return quote
-        using StaticArraysCore: SVector
+        using .StaticArraysCore: MMatrix, SMatrix, SVector
 
         function genmat(B::BallInf{N,SVector{L,N}}) where {L,N}
             if isflat(B)
-                return StaticArraysCore.SMatrix{L,0,N,0}()
+                return SMatrix{L,0,N,0}()
             else
-                gens = zeros(StaticArraysCore.MMatrix{L,L})
+                gens = zeros(MMatrix{L,L})
                 @inbounds for i in 1:L
                     gens[i, i] = B.radius
                 end
-                return StaticArraysCore.SMatrix(gens)
+                return SMatrix(gens)
             end
         end
     end
