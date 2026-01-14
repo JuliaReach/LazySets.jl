599585b21a720af30789645529632ff7378a02f9
schillic <git@christianschilling.net>
2026-01-14 21:10:39 +0100

    better default 'radius' in infinity norm

--- src/Interfaces/LazySet_functions.jl
+++ src/Interfaces/LazySet_functions.jl
@@ -554,20 +554,45 @@ end
 
 The default implementation handles the following cases:
 - `X` is 1D and `p` is any value (using the interval approximation)
-- `X` is any set and `p` is `Inf` (using `ballinf_approximation`)
+- `X` is any set and `p` is `Inf` (using `ballinf_approximation` but without allocations)
 - `X` is polytopic and `p` is `2` (using Welzl's algorithm)
 """
 @validate function radius(X::LazySet, p::Real=Inf)
-    if dim(X) == 1
-        l, h = extrema(X, 1)
-        return (h - l) / 2
+    n = dim(X)
+    if n == 1
+        return _radius_1D(X)
     elseif p == Inf
-        return radius(Approximations.ballinf_approximation(X), p)
+        return _radius_Inf(X; n)
     elseif p == 2
-        return radius(overapproximate(X, Ball2))
+        return _radius_2(X)
     else
         error("the radius for this value of p=$p is not implemented")
     end
+end
+
+function _radius_1D(X::LazySet)
+    l, h = extrema(X, 1)
+    return (h - l) / 2
+end
+
+# code is almost identical to `ballinf_approximation` but without the center
+function _radius_Inf(X::LazySet{N}; n=dim(X)) where {N}
+    r = zero(N)
+    @inbounds for i in 1:n
+        l, h = extrema(X, i)
+        rcur = (h - l) / 2
+        if (rcur > r)
+            r = rcur
+        elseif !_geq(rcur, zero(N))
+            # contradicting bounds => set is empty
+            throw(ArgumentError("set must be nonempty"))
+        end
+    end
+    return r
+end
+
+function _radius_2(X::LazySet)
+    return radius(overapproximate(X, Ball2))
 end
 
 """
