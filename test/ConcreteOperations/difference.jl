7960704f5fbac5a212efe8a57cc93af76bab45fd
schillic <git@christianschilling.net>
2026-02-02 20:48:17 +0100

    add 'difference' between polyhedra

--- test/ConcreteOperations/difference.jl
+++ test/ConcreteOperations/difference.jl
@@ -18,4 +18,19 @@ for N in @tN([Float64, Float32, Rational{Int}])
     for U in (HalfSpace(N[1], N(1)), HalfSpace(N[-1], N(-3)))
         @test difference(x, U) == x
     end
+
+    # polyhedron \ polyhedron
+    P = VPolygon([N[-1, 0], N[1, 0], N[0, 2]])
+    Q = VPolygon([N[0, 1], N[2, 2], N[1, 4]])
+    R = difference(P, Q)
+    # hard to test -> only sufficient test for result of implementation
+    @test eltype(R) == N
+    @test R == UnionSetArray([HPolytope(HalfSpace[HalfSpace(N[2, 1], N(2)),
+                                                  HalfSpace(N[-2, 1], N(2)),
+                                                  HalfSpace(N[0, -2], N(0)),
+                                                  HalfSpace(N[3, -1], N(-1))]),
+                              HPolytope(HalfSpace[HalfSpace(N[2, 1], N(2)),
+                                                  HalfSpace(N[-1, 2], N(2)),
+                                                  HalfSpace(N[-3, 1], N(1)),
+                                                  HalfSpace(N[0, -2], N(0))])])
 end
