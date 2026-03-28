87f54dccc07156500d0005d692ee7e6a65e40e4a
schillic <git@christianschilling.net>
2026-03-28 17:36:53 +0100

    add 'convert' from 'EmptySet' to 'HPolyhedron'

--- src/Convert/HPolyhedron.jl
+++ src/Convert/HPolyhedron.jl
@@ -2,3 +2,8 @@
 function convert(::Type{HPolyhedron}, P::HPolytope)
     return HPolyhedron(copy(constraints_list(P)))
 end
+
+function convert(::Type{HPolyhedron}, ∅::EmptySet)
+    clist = _infeasible_constraints_list(dim(∅); N=eltype(∅))
+    return HPolyhedron(clist)
+end
