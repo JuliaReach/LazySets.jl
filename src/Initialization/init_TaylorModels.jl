5d42d88488ef9e3c3a6bdb38523d77451a510862
schillic <git@christianschilling.net>
2026-06-06 08:52:36 +0200

    fix deprecations in TaylorSeries

--- src/Initialization/init_TaylorModels.jl
+++ src/Initialization/init_TaylorModels.jl
@@ -1,7 +1,6 @@
 using .TaylorModels: domain
-using .TaylorModels.TaylorSeries: numtype  # NOTE: this is an internal function
 
-_eltype_TM(TMi) = numtype(polynomial(TMi))
+_eltype_TM(TMi) = IA.numtype(polynomial(TMi))
 
 # check that a vector of Taylor models has the [-1, 1] domain
 function _has_normalized_domain(vTM::Vector)
