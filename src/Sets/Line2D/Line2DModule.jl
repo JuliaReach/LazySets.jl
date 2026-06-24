82ecc79784be8c258dc5f36ba6376621b65077d6
schillic <git@christianschilling.net>
2026-06-24 23:04:23 +0200

    use package extensions in Line2DModule

--- src/Sets/Line2D/Line2DModule.jl
+++ src/Sets/Line2D/Line2DModule.jl
@@ -1,22 +1,18 @@
 module Line2DModule
 
 using Reexport: @reexport
-using Requires: @require
-
-using ..LazySets: AbstractPolyhedron, AbstractLinearMapAlgorithm,
-                  _intersection_line2d, _linear_map_hrep, _witness_result_empty,
-                  @validate
+using ..LazySets: AbstractPolyhedron, _intersection_line2d,
+                  _witness_result_empty, @validate
 using LinearAlgebra: dot
 using Random: AbstractRNG, GLOBAL_RNG
 using ReachabilityBase.Arrays: nonzero_indices, right_turn
 using ReachabilityBase.Distribution: reseed!
 using ReachabilityBase.Comparison: isapproxzero, _isapprox
 
-@reexport import ..API: an_element, constraints_list, dim, isbounded, isempty,
-                        isoperationtype, isuniversal, rand, in, project, scale,
-                        scale!, σ, translate, intersection, isdisjoint
+@reexport import ..API: an_element, dim, isbounded, isempty, isoperationtype,
+                        rand, in, project, scale, scale!, translate,
+                        intersection, isdisjoint
 @reexport import ..LazySets: constrained_dimensions
-import ..LazySets: _linear_map_hrep_helper
 @reexport using ..API
 
 export Line2D
@@ -24,24 +20,22 @@ export Line2D
 include("Line2D.jl")
 
 include("an_element.jl")
-include("constraints_list.jl")
+# include("constraints_list.jl")
 include("dim.jl")
 include("isbounded.jl")
 include("isempty.jl")
 include("isoperationtype.jl")
-include("isuniversal.jl")
+# include("isuniversal.jl")
 include("rand.jl")
 include("in.jl")
-include("linear_map.jl")
+# include("linear_map.jl")
 include("project.jl")
 include("scale.jl")
-include("support_vector.jl")
+# include("support_vector.jl")
 include("translate.jl")
 include("intersection.jl")
 include("isdisjoint.jl")
 
 include("constrained_dimensions.jl")
-
-include("init.jl")
 
 end  # module
--- src/Sets/Line2D/init_LazySets.jl
+++
@@ -1,5 +0,0 @@
-using .LazySets.HalfSpaceModule: _non_element_halfspace
-using .LazySets.HyperplaneModule: _constraints_list_hyperplane,
-                                  _σ_hyperplane_halfspace
-using .LazySets.SingletonModule: Singleton
-using .LazySets.UniverseModule: Universe
