e70675d40c16406d3064a8a85c78141209fbb4d3
schillic <git@christianschilling.net>
2026-06-28 23:27:47 +0200

    use package extensions in StarModule

--- src/Sets/Star/isbounded.jl
+++ src/Sets/Star/isbounded.jl
@@ -1,13 +1,1 @@
-"""
-# Extended help
-
-    isbounded(X::Star; cond_tol::Number=DEFAULT_COND_TOL)
-
-### Algorithm
-
-See [`isbounded(::LazySets.AbstractAffineMap)`](@ref).
-"""
-function isbounded(X::Star; cond_tol::Number=DEFAULT_COND_TOL)
-    am = convert(STAR, X)
-    return isbounded(am; cond_tol=cond_tol)
-end
+# see ext/LazySets/LazySetsStarExt.jl
