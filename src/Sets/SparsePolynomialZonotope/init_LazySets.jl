b086168ca27e9f28bfa50c6ccfe4c71b1fbdb3b0
schillic <git@christianschilling.net>
2026-06-28 23:19:41 +0200

    use package extensions in SparsePolynomialZonotopeModule

--- src/Sets/SparsePolynomialZonotope/init_LazySets.jl
+++
@@ -1,3 +0,0 @@
-using .LazySets.SimpleSparsePolynomialZonotopeModule: SimpleSparsePolynomialZonotope
-using .LazySets.ZonotopeModule: Zonotope
-using .LazySets.Approximations: overapproximate
--- src/Sets/SparsePolynomialZonotope/reduce_order.jl
+++ src/Sets/SparsePolynomialZonotope/reduce_order.jl
@@ -16,8 +16,6 @@ function reduce_order(P::SparsePolynomialZonotope, r::Real,
         return P
     end
 
-    require(@__MODULE__, :LazySets; fun_name="reduce_order")
-
     n = dim(P)
     h = ngens_dep(P)
     q = ngens_indep(P)
@@ -36,31 +34,5 @@ function reduce_order(P::SparsePolynomialZonotope, r::Real,
     return _absorb_generators_spz(P, norms, threshold, method)
 end
 
-# absorb all generators whose norm is below a given threshold with a box
-function _absorb_generators_spz(P, norms, threshold, method)
-    h = ngens_dep(P)
-    q = ngens_indep(P)
-    c = center(P)
-    G = genmat_dep(P)
-    GI = genmat_indep(P)
-    E = expmat(P)
-    idx = indexvector(P)
-
-    # TODO is constructing an array of booleans the most efficient way?
-    K = [norms[i] ≤ threshold for i in 1:h]
-    Kbar = .!K
-
-    H = [norms[h + i] ≤ threshold for i in 1:q]
-    Hbar = .!H
-
-    PZ = SparsePolynomialZonotope(c, G[:, K], GI[:, H], E[:, K], idx)
-    Z = reduce_order(overapproximate(PZ, Zonotope), 1, method)
-
-    Ebar = E[:, Kbar]
-    N = [!iszero(e) for e in eachrow(Ebar)]
-
-    cz = center(Z)
-    Gz = genmat(Z)
-    return SparsePolynomialZonotope(cz, G[:, Kbar], hcat(GI[:, Hbar], Gz),
-                                    Ebar[N, :], idx[N])
-end
+# see ext/LazySets/LazySetsSparsePolynomialZonotopeExt.jl
+_absorb_generators_spz(P, norms, threshold, method) = error()
