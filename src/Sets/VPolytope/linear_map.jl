e60761fd01618c5764e0aa4b2a26051600da6e65
schillic <git@christianschilling.net>
2026-06-29 22:19:33 +0200

    use package extensions in VPolytopeModule

--- src/Sets/VPolytope/linear_map.jl
+++ src/Sets/VPolytope/linear_map.jl
@@ -17,12 +17,9 @@ a polytope in vertex representation. The output type is again a `VPolytope`.
     return _linear_map_vrep(M, P; apply_convex_hull=apply_convex_hull)
 end
 
-@inline function _linear_map_vrep(M::AbstractMatrix, P::VPolytope,
-                                  algo::LinearMapVRep=LinearMapVRep(nothing);  # ignored
-                                  apply_convex_hull::Bool=false)
-    vlist = broadcast(v -> M * v, P.vertices)
-    if apply_convex_hull
-        convex_hull!(vlist)
-    end
-    return VPolytope(vlist)
+# see ext/LazySets/LazySetsVPolytopeExt.jl
+function _linear_map_vrep(M::AbstractMatrix, P,
+                          algo::LinearMapVRep=LinearMapVRep(nothing);  # ignored
+                          apply_convex_hull::Bool=false)
+    return error()
 end
