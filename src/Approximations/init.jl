2f1083a6a792823c7733e6a0daaa5cfb8fa2c924
schillic <git@christianschilling.net>
2026-07-12 08:42:51 +0200

    use package extension for Expokit

--- src/Approximations/init.jl
+++ src/Approximations/init.jl
@@ -1,5 +1,4 @@
 function __init__()
-    @require Expokit = "a1e7a1ef-7a5d-5822-a38c-be74e1bb89f4" (nothing,)
     @require ExponentialUtilities = "d4d017d3-3776-5f7e-afef-a10c40355c18" (nothing,)
     @require IntervalMatrices = "5c1f47dc-42dd-5697-8aaa-4d102d140ba9" include("init_IntervalMatrices.jl")
     @require StaticArraysCore = "1e83bf80-4336-4d27-bf5d-d5a4f845583c" include("init_StaticArraysCore.jl")
--- src/Utils/matrix_exponential.jl
+++ src/Utils/matrix_exponential.jl
@@ -41,24 +41,3 @@ end  # quote / load_exponentialutilities
 function _expmv(::Val{:ExponentialUtilities}, t, A, b; kwargs...)
     return ExponentialUtilities.expv(t, A, b; kwargs...)
 end
-
-# =======
-# Expokit
-# =======
-
-function load_expokit()
-    return quote
-        if ismissing(exponential_backend[])
-            set_exponential_backend!(Expokit)
-        end
-    end
-end  # quote / load_expokit
-
-function _expmv(::Val{:Expokit}, t, A, b::Vector; kwargs...)
-    return Expokit.expmv(t, A, b; kwargs...)
-end
-
-function _expmv(backend::Val{:Expokit}, t, A, b; kwargs...)
-    b = Vector(b)  # Expokit requires a dense vector
-    return _expmv(backend, t, A, b; kwargs...)
-end
